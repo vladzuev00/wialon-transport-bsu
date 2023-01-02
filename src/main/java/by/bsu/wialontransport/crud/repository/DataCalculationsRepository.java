8c9859bbe131ff3c930505e77d259c90095d83cc
vladzuev <vladzuev.00@mail.ru>
2023-01-02 05:45:22 -0600

    add repository for DataCalculationsEntity

---
+++ src/main/java/by/bsu/wialontransport/crud/repository/DataCalculationsRepository.java
@@ -0,0 +1,8 @@
+package by.bsu.wialontransport.crud.repository;
+
+import by.bsu.wialontransport.crud.entity.DataCalculationsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DataCalculationsRepository extends JpaRepository<DataCalculationsEntity, Long> {
+
+}
