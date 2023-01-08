deef762980c6c1acad60e2e7a85892e1da9cd9e9
vladzuev <vladzuev.00@mail.ru>
2023-01-08 03:22:51 -0600

    add UserService

---
+++ src/main/java/by/bsu/wialontransport/crud/service/UserService.java
@@ -0,0 +1,16 @@
+package by.bsu.wialontransport.crud.service;
+
+import by.bsu.wialontransport.crud.dto.User;
+import by.bsu.wialontransport.crud.entity.UserEntity;
+import by.bsu.wialontransport.crud.mapper.UserMapper;
+import by.bsu.wialontransport.crud.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class UserService
+        extends AbstractCRUDService<Long, UserEntity, User, UserMapper, UserRepository> {
+
+    public UserService(UserMapper mapper, UserRepository repository) {
+        super(mapper, repository);
+    }
+}
