f7a0ce55a54263d81ace01750c345a507c4e99ed
vladzuev <vladzuev.00@mail.ru>
2023-01-28 14:08:43 +0300

    refactor RequestDataPackageDeserializer

--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/deserializer/RequestDataPackageDeserializer.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/deserializer/RequestDataPackageDeserializer.java
@@ -1,25 +1,26 @@
 package by.bsu.wialontransport.protocol.wialon.decoder.deserializer;
 
+import by.bsu.wialontransport.crud.dto.Data;
 import by.bsu.wialontransport.protocol.core.exception.AnswerableException;
-import by.bsu.wialontransport.protocol.wialon.decoder.deserializer.factory.DataFactory;
+import by.bsu.wialontransport.protocol.wialon.decoder.deserializer.parser.DataParser;
 import by.bsu.wialontransport.protocol.wialon.decoder.deserializer.parser.exception.NotValidDataException;
 import by.bsu.wialontransport.protocol.wialon.wialonpackage.data.RequestDataPackage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
+@RequiredArgsConstructor
 public final class RequestDataPackageDeserializer implements PackageDeserializer {
     private static final String RESPONSE_FAILURE_HANDLING = "#AD#0";
-    private final DataFactory dataFactory;
+
+    private final DataParser dataParser;
 
     @Override
     public RequestDataPackage deserialize(final String source) {
         try {
-            final String extendedDataString = PackageDeserializer.removePrefix(source);
-            //final ExtendedData extendedData = this.dataFactory.createAsExtendedData(extendedDataString);
-            //return new RequestDataPackage(extendedData);
-            return null;
+            final String dataString = PackageDeserializer.removePrefix(source);
+            final Data data = this.dataParser.parse(dataString);
+            return new RequestDataPackage(data);
         } catch (final NotValidDataException cause) {
             throw new AnswerableException(RESPONSE_FAILURE_HANDLING, cause);
         }
