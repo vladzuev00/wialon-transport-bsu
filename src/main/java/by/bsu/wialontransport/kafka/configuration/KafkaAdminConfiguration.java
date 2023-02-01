64c647221808ca8796c7afaec40abffd90ed320d
vladzuev <vladzuev.00@mail.ru>
2023-02-02 00:23:35 +0300

    add KafkaTopicConfiguration and KafkaAdminConfiguration

---
+++ src/main/java/by/bsu/wialontransport/kafka/configuration/KafkaAdminConfiguration.java
@@ -0,0 +1,20 @@
+package by.bsu.wialontransport.kafka.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Map;
+
+import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
+
+@Configuration
+public class KafkaAdminConfiguration {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(@Value("${spring.kafka.bootstrap-servers}") final String bootstrapAddress) {
+        final Map<String, Object> configurationByNames = Map.of(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configurationByNames);
+    }
+}
