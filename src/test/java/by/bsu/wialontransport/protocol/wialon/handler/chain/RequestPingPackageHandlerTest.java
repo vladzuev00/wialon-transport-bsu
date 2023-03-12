643c2eba2cecd3797eda29c12b8f2299161ef767
Vlad Zuev <vladzuev.00@mail.ru>
2023-03-12 20:49:59 +0300

    add RequestPingPackageHandlerTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/handler/chain/RequestPingPackageHandlerTest.java
@@ -0,0 +1,40 @@
+package by.bsu.wialontransport.protocol.wialon.handler.chain;
+
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.Package;
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.RequestPingPackage;
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.ResponsePingPackage;
+
+import io.netty.channel.ChannelHandlerContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public final class RequestPingPackageHandlerTest {
+    private final RequestPingPackageHandler handler;
+
+    @Captor
+    private ArgumentCaptor<ResponsePingPackage> responsePingPackageArgumentCaptor;
+
+    public RequestPingPackageHandlerTest() {
+        this.handler = new RequestPingPackageHandler();
+    }
+
+    @Test
+    public void packageShouldBeHandledIndependently() {
+        final Package givenRequestPackage = new RequestPingPackage();
+        final ChannelHandlerContext givenContext = mock(ChannelHandlerContext.class);
+
+        this.handler.handle(givenRequestPackage, givenContext);
+
+        verify(givenContext, times(1))
+                .writeAndFlush(this.responsePingPackageArgumentCaptor.capture());
+        assertNotNull(this.responsePingPackageArgumentCaptor.getValue());
+    }
+}
