9b9e3c49dde415ffeca7b079b455371b5faa40d0
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-08 20:45:11 +0300

    refactor AbstractKafkaDataProducer

--- src/main/java/by/bsu/wialontransport/kafka/producer/AbstractKafkaDataProducer.java
+++ src/main/java/by/bsu/wialontransport/kafka/producer/AbstractKafkaDataProducer.java
@@ -23,17 +23,16 @@ import static java.time.ZoneOffset.UTC;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.joining;
 
-//TODO: do inner classes for parameters and analog inputs serializers
 public abstract class AbstractKafkaDataProducer extends AbstractGenericRecordKafkaProducer<Long, TransportableData, Data> {
-    private static final String DELIMITER_SERIALIZED_PARAMETER_PROPERTIES = ":";
-    private static final String DELIMITER_SERIALIZED_PARAMETERS = ",";
-
-    private static final String DELIMITER_SERIALIZED_ANALOG_INPUTS = ",";
+    private final AnalogInputsSerializer analogInputsSerializer;
+    private final ParametersSerializer parametersSerializer;
 
     public AbstractKafkaDataProducer(final KafkaTemplate<Long, GenericRecord> kafkaTemplate,
                                      final String topicName,
                                      final Schema schema) {
         super(kafkaTemplate, topicName, schema);
+        this.analogInputsSerializer = new AnalogInputsSerializer();
+        this.parametersSerializer = new ParametersSerializer();
     }
 
     @Override
@@ -61,9 +60,9 @@ public abstract class AbstractKafkaDataProducer extends AbstractGenericRecordKaf
                 data.getReductionPrecision(),
                 data.getInputs(),
                 data.getOutputs(),
-                serializeAnalogInputs(data),
+                this.analogInputsSerializer.serialize(data),
                 data.getDriverKeyCode(),
-                serializeParameters(data),
+                this.parametersSerializer.serialize(data),
                 findTrackerId(data)
         );
     }
@@ -88,7 +87,6 @@ public abstract class AbstractKafkaDataProducer extends AbstractGenericRecordKaf
     private static char findLatitudeTypeValue(final Data data) {
         final Latitude latitude = data.getLatitude();
         final DataEntity.Latitude.Type latitudeType = latitude.getType();
-        System.out.println(latitudeType.getValue());
         return latitudeType.getValue();
     }
 
@@ -107,7 +105,6 @@ public abstract class AbstractKafkaDataProducer extends AbstractGenericRecordKaf
     private static char findLongitudeTypeValue(final Data data) {
         final Longitude longitude = data.getLongitude();
         final DataEntity.Longitude.Type longitudeType = longitude.getType();
-        System.out.println(longitudeType.getValue());
         return longitudeType.getValue();
     }
 
@@ -125,54 +122,63 @@ public abstract class AbstractKafkaDataProducer extends AbstractGenericRecordKaf
         return getterValue.applyAsInt(geographicCoordinate);
     }
 
-    private static String serializeAnalogInputs(final Data data) {
-        return stream(data.getAnalogInputs())
-                .mapToObj(Double::toString)
-                .collect(joining(DELIMITER_SERIALIZED_ANALOG_INPUTS));
-    }
-
-    private static String serializeParameters(final Data data) {
-        final Collection<Parameter> parameters = findParameters(data);
-        return serializeParameters(parameters);
-    }
-
-    private static Collection<Parameter> findParameters(final Data data) {
-        final Map<String, Parameter> parametersByNames = data.getParametersByNames();
-        return parametersByNames.values();
-    }
-
-    private static String serializeParameters(final Collection<Parameter> parameters) {
-        return parameters.stream()
-                .map(AbstractKafkaDataProducer::serializeParameter)
-                .collect(joining(DELIMITER_SERIALIZED_PARAMETERS));
-    }
-
-    private static String serializeParameter(final Parameter parameter) {
-        final StringBuilder builderSerializedParameter = new StringBuilder();
-        appendParameterIdWithPropertyDelimiterIfIdExists(builderSerializedParameter, parameter);
-        builderSerializedParameter.append(parameter.getName());
-        builderSerializedParameter.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
-        appendValueOfParameterType(builderSerializedParameter, parameter);
-        builderSerializedParameter.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
-        builderSerializedParameter.append(parameter.getValue());
-        return builderSerializedParameter.toString();
-    }
-
-    private static void appendParameterIdWithPropertyDelimiterIfIdExists(final StringBuilder stringBuilder,
-                                                                         final Parameter parameter) {
-        if (parameter.getId() != null) {
-            stringBuilder.append(parameter.getId());
-            stringBuilder.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
-        }
-    }
-
-    private static void appendValueOfParameterType(final StringBuilder stringBuilder, final Parameter parameter) {
-        final ParameterEntity.Type type = parameter.getType();
-        stringBuilder.append(type.getValue());
-    }
-
     private static Long findTrackerId(final Data data) {
         final Tracker tracker = data.getTracker();
         return tracker.getId();
+    }
+
+    private static final class AnalogInputsSerializer {
+        private static final String DELIMITER_SERIALIZED_ANALOG_INPUTS = ",";
+
+        public String serialize(final Data data) {
+            return stream(data.getAnalogInputs())
+                    .mapToObj(Double::toString)
+                    .collect(joining(DELIMITER_SERIALIZED_ANALOG_INPUTS));
+        }
+    }
+
+    private static final class ParametersSerializer {
+        private static final String DELIMITER_SERIALIZED_PARAMETER_PROPERTIES = ":";
+        private static final String DELIMITER_SERIALIZED_PARAMETERS = ",";
+
+        public String serialize(final Data data) {
+            final Collection<Parameter> parameters = findParameters(data);
+            return serializeParameters(parameters);
+        }
+
+        private static Collection<Parameter> findParameters(final Data data) {
+            final Map<String, Parameter> parametersByNames = data.getParametersByNames();
+            return parametersByNames.values();
+        }
+
+        private static String serializeParameters(final Collection<Parameter> parameters) {
+            return parameters.stream()
+                    .map(ParametersSerializer::serializeParameter)
+                    .collect(joining(DELIMITER_SERIALIZED_PARAMETERS));
+        }
+
+        private static String serializeParameter(final Parameter parameter) {
+            final StringBuilder builderSerializedParameter = new StringBuilder();
+            appendParameterIdWithPropertyDelimiterIfIdExists(builderSerializedParameter, parameter);
+            builderSerializedParameter.append(parameter.getName());
+            builderSerializedParameter.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
+            appendValueOfParameterType(builderSerializedParameter, parameter);
+            builderSerializedParameter.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
+            builderSerializedParameter.append(parameter.getValue());
+            return builderSerializedParameter.toString();
+        }
+
+        private static void appendParameterIdWithPropertyDelimiterIfIdExists(final StringBuilder stringBuilder,
+                                                                             final Parameter parameter) {
+            if (parameter.getId() != null) {
+                stringBuilder.append(parameter.getId());
+                stringBuilder.append(DELIMITER_SERIALIZED_PARAMETER_PROPERTIES);
+            }
+        }
+
+        private static void appendValueOfParameterType(final StringBuilder stringBuilder, final Parameter parameter) {
+            final ParameterEntity.Type type = parameter.getType();
+            stringBuilder.append(type.getValue());
+        }
     }
 }
---
+++ src/test/java/by/bsu/wialontransport/kafka/producer/serializer/AvroGenericRecordSerializerTest.java
@@ -0,0 +1,11 @@
+package by.bsu.wialontransport.kafka.producer.serializer;
+
+import org.junit.Test;
+
+public final class AvroGenericRecordSerializerTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
