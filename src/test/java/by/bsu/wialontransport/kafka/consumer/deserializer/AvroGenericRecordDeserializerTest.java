2dcf7be178990993ad7984b864e12644478bd971
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-08 20:32:33 +0300

    add AbstractKafkaGenericRecordConsumerTest

---
+++ src/test/java/by/bsu/wialontransport/kafka/consumer/deserializer/AvroGenericRecordDeserializerTest.java
@@ -0,0 +1,11 @@
+package by.bsu.wialontransport.kafka.consumer.deserializer;
+
+import org.junit.Test;
+
+public final class AvroGenericRecordDeserializerTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
