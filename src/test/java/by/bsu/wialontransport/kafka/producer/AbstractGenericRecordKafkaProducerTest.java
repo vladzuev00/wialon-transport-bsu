e96bb404352e3d6aa7f28751fc7f4715149d8a75
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-08 22:10:32 +0300

    add DOPParameterDictionaryTest

---
+++ src/test/java/by/bsu/wialontransport/kafka/producer/AbstractGenericRecordKafkaProducerTest.java
@@ -0,0 +1,11 @@
+package by.bsu.wialontransport.kafka.producer;
+
+import org.junit.Test;
+
+public final class AbstractGenericRecordKafkaProducerTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/by/bsu/wialontransport/kafka/producer/AbstractKafkaProducerTest.java
@@ -0,0 +1,11 @@
+package by.bsu.wialontransport.kafka.producer;
+
+import org.junit.Test;
+
+public final class AbstractKafkaProducerTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
