da1c4be2aa046b30cb87b7b7661b280d1c508854
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-12 01:00:11 +0300

    add AddressMapper

---
+++ src/main/java/by/bsu/wialontransport/crud/mapper/AddressMapper.java
@@ -0,0 +1,26 @@
+package by.bsu.wialontransport.crud.mapper;
+
+import by.bsu.wialontransport.crud.dto.Address;
+import by.bsu.wialontransport.crud.entity.AddressEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class AddressMapper extends AbstractMapper<AddressEntity, Address> {
+
+    public AddressMapper(final ModelMapper modelMapper) {
+        super(modelMapper, AddressEntity.class, Address.class);
+    }
+
+    @Override
+    protected Address createDto(final AddressEntity entity) {
+        return new Address(
+                entity.getId(),
+                entity.getBoundaries(),
+                entity.getCenterLatitude(),
+                entity.getCenterLongitude(),
+                entity.getCityName(),
+                entity.getCountryName()
+        );
+    }
+}
