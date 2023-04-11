323dabe873d03262a11c0fbf0b7b41dfc09003bf
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-12 01:06:59 +0300

    add AddressMapperTest

--- src/main/java/by/bsu/wialontransport/crud/dto/Address.java
+++ src/main/java/by/bsu/wialontransport/crud/dto/Address.java
@@ -1,9 +1,13 @@
 package by.bsu.wialontransport.crud.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
 import org.locationtech.jts.geom.Geometry;
 
 @Value
+@AllArgsConstructor
+@Builder
 public class Address implements AbstractDto<Long> {
     Long id;
     Geometry boundaries;
