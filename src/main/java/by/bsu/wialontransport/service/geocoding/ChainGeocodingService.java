b5cb0b916ca0f477994c8485823c316200398559
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-12 18:08:01 +0300

    refactor ChainGeocodingService and add tests for it

--- src/main/java/by/bsu/wialontransport/service/geocoding/ChainGeocodingService.java
+++ src/main/java/by/bsu/wialontransport/service/geocoding/ChainGeocodingService.java
@@ -2,12 +2,13 @@ package by.bsu.wialontransport.service.geocoding;
 
 import by.bsu.wialontransport.crud.dto.Address;
 import by.bsu.wialontransport.service.geocoding.component.GeocodingChainComponent;
-import by.bsu.wialontransport.service.geocoding.exception.GeocodingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.function.UnaryOperator.identity;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +21,6 @@ public final class ChainGeocodingService implements GeocodingService {
                 .map(service -> service.receive(latitude, longitude))
                 .filter(Optional::isPresent)
                 .findFirst()
-                .orElseThrow(() -> new GeocodingException("There are no geocoding services."));
+                .flatMap(identity());
     }
 }
