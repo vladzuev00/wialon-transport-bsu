44b47d8aaa1f8165ea0011e2f3594b196f5d61e3
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-28 00:58:28 +0300

    Revert "refactor"
    This reverts commit 8ceed883d9d8c6d32cfa4a9a983374207b65a372.

--- src/main/java/by/bsu/wialontransport/protocol/core/service/receivingdata/filter/DataFilter.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/service/receivingdata/filter/DataFilter.java
@@ -18,11 +18,10 @@ public final class DataFilter {
     }
 
     public boolean isValid(final Data data) {
-        return !this.filteringEnable
-                ||
-                this.propertyValidator.isValidDateTime(data)
-                        && this.propertyValidator.isValidAmountOfSatellites(data)
-                        && this.propertyValidator.isValidDOPParameters(data);
+        return this.filteringEnable
+                && this.propertyValidator.isValidDateTime(data)
+                && this.propertyValidator.isValidAmountOfSatellites(data)
+                && this.propertyValidator.isValidDOPParameters(data);
     }
 
     public boolean isValid(final Data current, final Data previous) {
@@ -30,14 +29,13 @@ public final class DataFilter {
     }
 
     public boolean isNeedToBeFixed(final Data current, final Data previous) {
-        return !this.filteringEnable
-                ||
-                this.propertyValidator.isValidDateTime(current)
-                        && isCorrectOrder(current, previous)
-                        && !(
+        return this.filteringEnable
+                && this.propertyValidator.isValidDateTime(current)
+                && isCorrectOrder(current, previous)
+                && !(
                         this.propertyValidator.isValidAmountOfSatellites(current)
                                 && this.propertyValidator.isValidDOPParameters(current)
-                );
+        );
     }
 
     private static boolean isCorrectOrder(final Data current, final Data previous) {
