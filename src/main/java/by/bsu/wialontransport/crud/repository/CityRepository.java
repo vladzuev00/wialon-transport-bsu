009cd8b6aced35687a0686bc80a00ee05421e6fe
Vlad Zuev <vladzuev.00@mail.ru>
2023-04-30 11:31:40 +0300

    add SearchingCitiesProcessRepository and CityRepository

---
+++ src/main/java/by/bsu/wialontransport/crud/repository/CityRepository.java
@@ -0,0 +1,8 @@
+package by.bsu.wialontransport.crud.repository;
+
+import by.bsu.wialontransport.crud.entity.CityEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+
+}
---
+++ src/main/java/by/bsu/wialontransport/crud/repository/SearchingCitiesProcessRepository.java
@@ -0,0 +1,8 @@
+package by.bsu.wialontransport.crud.repository;
+
+import by.bsu.wialontransport.crud.entity.SearchingCitiesProcessEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SearchingCitiesProcessRepository extends JpaRepository<SearchingCitiesProcessEntity, Long> {
+
+}
