d0f85ccb54c24071f54a456831d0df93ea1cc0c4
Vlad Zuev <vladzuev.00@mail.ru>
2023-05-18 11:46:18 +0300

    add TransportableSavedData

--- src/main/java/by/bsu/wialontransport/kafka/transportable/TransportableData.java
+++ src/main/java/by/bsu/wialontransport/kafka/transportable/TransportableData.java
@@ -1,44 +1,43 @@
 package by.bsu.wialontransport.kafka.transportable;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 import lombok.experimental.FieldNameConstants;
 import org.apache.avro.reflect.Nullable;
 
-@Value
-@Builder
 @AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+@Builder
 @FieldNameConstants
 public class TransportableData implements Transportable<Long> {
 
     @Nullable
-    Long id;
+    private final Long id;
 
-    long epochSeconds;
-    int latitudeDegrees;
-    int latitudeMinutes;
-    int latitudeMinuteShare;
-    char latitudeTypeValue;
-    int longitudeDegrees;
-    int longitudeMinutes;
-    int longitudeMinuteShare;
-    char longitudeTypeValue;
-    int speed;
-    int course;
-    int altitude;
-    int amountOfSatellites;
-    double reductionPrecision;
-    int inputs;
-    int outputs;
-    String serializedAnalogInputs;
-    String driverKeyCode;
-    String serializedParameters;
-    Long trackerId;
+    private final long epochSeconds;
+    private final int latitudeDegrees;
+    private final int latitudeMinutes;
+    private final int latitudeMinuteShare;
+    private final char latitudeTypeValue;
+    private final int longitudeDegrees;
+    private final int longitudeMinutes;
+    private final int longitudeMinuteShare;
+    private final char longitudeTypeValue;
+    private final int speed;
+    private final int course;
+    private final int altitude;
+    private final int amountOfSatellites;
+    private final double reductionPrecision;
+    private final int inputs;
+    private final int outputs;
+    private final String serializedAnalogInputs;
+    private final String driverKeyCode;
+    private final String serializedParameters;
+    private final Long trackerId;
 
-    //TODO: test
     @Override
-    public Long findKey() {
+    public final Long findKey() {
         return this.trackerId;
     }
 }
