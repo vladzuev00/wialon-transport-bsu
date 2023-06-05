f4234358d0ddb95030f9bd9953f5b5b340a07299
Vlad Zuev <vladzuev.00@mail.ru>
2023-06-05 21:18:46 +0300

    refactor

---
+++ src/test/java/by/bsu/wialontransport/controller/useraction/UserActionControllerTest.java
@@ -0,0 +1,12 @@
+package by.bsu.wialontransport.controller.useraction;
+
+import org.junit.Test;
+
+public class UserActionControllerTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+
+}
