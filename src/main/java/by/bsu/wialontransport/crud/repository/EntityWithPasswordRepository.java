289dc96da86f69e871d750a22fdaaa91bf805af4
Vlad Zuev <vladzuev.00@mail.ru>
2023-06-07 14:40:44 +0300

    refactor EntityWithPasswordRepository

--- src/main/java/by/bsu/wialontransport/crud/repository/EntityWithPasswordRepository.java
+++ src/main/java/by/bsu/wialontransport/crud/repository/EntityWithPasswordRepository.java
@@ -1,9 +1,11 @@
 package by.bsu.wialontransport.crud.repository;
 
-import by.bsu.wialontransport.crud.entity.AbstractEntity;
+import by.bsu.wialontransport.crud.entity.AbstractEntityWithPassword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-public interface EntityWithPasswordRepository<IdType, EntityType extends AbstractEntity<IdType>>
+@NoRepositoryBean
+public interface EntityWithPasswordRepository<IdType, EntityType extends AbstractEntityWithPassword<IdType>>
         extends JpaRepository<EntityType, IdType> {
     void updatePassword(final IdType id, final String encryptedPassword);
 }
--- src/test/java/by/bsu/wialontransport/crud/service/UserServiceTest.java
+++ src/test/java/by/bsu/wialontransport/crud/service/UserServiceTest.java
@@ -2,7 +2,6 @@ package by.bsu.wialontransport.crud.service;
 
 import by.bsu.wialontransport.base.AbstractContextTest;
 import by.bsu.wialontransport.crud.dto.User;
-import by.bsu.wialontransport.crud.entity.UserEntity.Role;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -50,36 +49,6 @@ public final class UserServiceTest extends AbstractContextTest {
     public void userShouldNotExistByEmail() {
         final boolean exists = this.service.isExistByEmail("notexist@mail.ru");
         assertFalse(exists);
-    }
-
-    @Test
-    public void userShouldBeSavedWithEncryptedPassword() {
-        final String givenEmail = "vladzuev.01@mail.ru";
-        final String givenPassword = "password";
-        final Role givenRole = USER;
-        final User givenUser = User.builder()
-                .email("vladzuev.01@mail.ru")
-                .password(givenPassword)
-                .role(givenRole)
-                .build();
-
-        final User actualSavedUser = this.service.saveWithEncryptingPassword(givenUser);
-        final User actualSavedUserFromDataBase = this.service.findById(actualSavedUser.getId()).orElseThrow();
-        final User expected = User.builder()
-                .email(givenEmail)
-                .password(givenPassword)
-                .role(givenRole)
-                .build();
-
-        checkEqualsExceptId(expected, actualSavedUser);
-        checkEqualsExceptId(expected, actualSavedUserFromDataBase);
-        assertEquals(actualSavedUser, actualSavedUserFromDataBase);
-    }
-
-    private void checkEqualsExceptId(final User expected, final User actual) {
-        assertEquals(expected.getEmail(), actual.getEmail());
-        assertTrue(this.passwordEncoder.matches(expected.getPassword(), actual.getPassword()));
-        assertSame(expected.getRole(), actual.getRole());
     }
 
 }
