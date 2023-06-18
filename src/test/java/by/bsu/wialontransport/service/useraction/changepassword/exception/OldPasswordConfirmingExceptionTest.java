7f5009ed06c7e48e6c2362dfc7b1eec3c9c44583
Vlad Zuev <vladzuev.00@mail.ru>
2023-06-18 11:45:09 +0300

    add OldPasswordConfirmingExceptionTest

---
+++ src/test/java/by/bsu/wialontransport/service/useraction/changepassword/exception/OldPasswordConfirmingExceptionTest.java
@@ -0,0 +1,27 @@
+package by.bsu.wialontransport.service.useraction.changepassword.exception;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class OldPasswordConfirmingExceptionTest {
+    private static final String ATTRIBUTE_NAME_OF_ERROR = "oldPasswordConfirmingError";
+    private static final String ATTRIBUTE_VALUE_OF_ERROR = "Old password isn't correct";
+
+    @Test
+    public void errorAttributeNameShouldBeFound() {
+        final PasswordChangingException givenException = new OldPasswordConfirmingException();
+
+        final String actual = givenException.findErrorAttributeName();
+        assertEquals(ATTRIBUTE_NAME_OF_ERROR, actual);
+    }
+
+    @Test
+    public void errorAttributeValueShouldBeFound() {
+        final PasswordChangingException givenException = new OldPasswordConfirmingException();
+
+        final String actual = givenException.findErrorAttributeValue();
+        assertEquals(ATTRIBUTE_VALUE_OF_ERROR, actual);
+    }
+
+}
