67e00bd5895e68412b91caba8cb0cee3bacb33fd
Vlad Zuev <vladzuev.00@mail.ru>
2023-07-03 09:48:21 +0300

    add DataService::findDataWithTrackerAndAddressOfUser

--- src/main/java/by/bsu/wialontransport/crud/service/DataService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/DataService.java
@@ -1,12 +1,15 @@
 package by.bsu.wialontransport.crud.service;
 
 import by.bsu.wialontransport.crud.dto.Data;
+import by.bsu.wialontransport.crud.dto.User;
 import by.bsu.wialontransport.crud.entity.DataEntity;
 import by.bsu.wialontransport.crud.mapper.DataMapper;
 import by.bsu.wialontransport.crud.repository.DataRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +23,15 @@ public class DataService extends AbstractCRUDService<Long, DataEntity, Data, Dat
     public Optional<Data> findTrackerLastDataByTrackerId(final Long trackerId) {
         final Optional<DataEntity> optionalEntity = super.repository.findTrackerLastDataByTrackerId(trackerId);
         return optionalEntity.map(super.mapper::mapToDto);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Data> findDataWithTrackerAndAddressOfUser(final User user,
+                                                          final LocalDate startDate,
+                                                          final LocalDate endDate) {
+        final List<DataEntity> foundEntities = super.repository.findDataWithTrackerAndAddressOfUserWithGivenId(
+                user.getId(), startDate, endDate
+        );
+        return super.mapper.mapToDto(foundEntities);
     }
 }
