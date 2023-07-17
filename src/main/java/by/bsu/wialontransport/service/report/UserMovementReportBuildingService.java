981c516e1666a48afb770c08aa0e227d9c66fc37
Vlad Zuev <vladzuev.00@mail.ru>
2023-07-17 17:32:00 +0300

    refactor

--- src/main/java/by/bsu/wialontransport/service/report/UserMovementReportBuildingService.java
+++ src/main/java/by/bsu/wialontransport/service/report/UserMovementReportBuildingService.java
@@ -11,20 +11,26 @@ import by.bsu.wialontransport.service.report.model.ReportTable;
 import by.bsu.wialontransport.service.report.model.UserMovementReportBuildingContext;
 import by.bsu.wialontransport.service.report.tablebuilder.DistributedUserMovementTableBuilder;
 import by.bsu.wialontransport.service.report.tabledrawer.DistributedReportTableDrawer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.springframework.stereotype.Service;
+import org.vandeseer.easytable.settings.HorizontalAlignment;
 import org.vandeseer.easytable.structure.Row;
 import org.vandeseer.easytable.structure.Table;
+import org.vandeseer.easytable.structure.cell.TextCell;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +38,8 @@ import java.util.Map.Entry;
 
 import static by.bsu.wialontransport.util.CellFactoryUtil.createTextCell;
 import static by.bsu.wialontransport.util.PDFUtil.addPage;
+import static java.awt.Color.BLUE;
+import static java.awt.Color.WHITE;
 import static java.lang.String.format;
 import static java.time.format.DateTimeFormatter.ofPattern;
 import static java.util.Arrays.stream;
@@ -151,25 +159,6 @@ public final class UserMovementReportBuildingService {
                 .build();
     }
 
-    private static List<Row> createUserTrackersTableRows(final UserMovementReportBuildingContext context) {
-        return context.getDataGroupedBySortedByImeiTrackers()
-                .entrySet()
-                .stream()
-                .map(dataByTracker -> createUserTrackersTableRow(dataByTracker.getKey(), dataByTracker.getValue()))
-                .toList();
-    }
-
-    private static Row createUserTrackersTableRow(final Tracker tracker, final List<Data> trackerData) {
-        final String trackerImei = tracker.getImei();
-        final String trackerPhoneNumber = tracker.getPhoneNumber();
-        final int countOfPoints = trackerData.size();
-        return Row.builder()
-                .add(createTextCell(trackerImei))
-                .add(createTextCell(trackerPhoneNumber))
-                .add(createTextCell(countOfPoints))
-                .build();
-    }
-
     private static final float USER_MOVEMENT_TABLE_COLUMN_WIDTH_OF_DATETIME = 110;
     private static final float USER_MOVEMENT_TABLE_COLUMN_WIDTH_OF_LATITUDE = 110;
     private static final float USER_MOVEMENT_TABLE_COLUMN_WIDTH_OF_LONGITUDE = 110;
@@ -214,26 +203,157 @@ public final class UserMovementReportBuildingService {
         return tableBuilder.build();
     }
 
-    private static Row createUserMovementTableRow(final Data data) {
-        final LocalDateTime dateTime = data.findDateTime();
-        final double latitudeAsDouble = data.findLatitudeAsDouble();
-        final double longitudeAsDouble = data.findLongitudeAsDouble();
-        final String cityName = data.findCityName();
-        final String countryName = data.findCountryName();
-        return Row.builder()
-                .add(createTextCell(dateTime))
-                .add(createTextCell(latitudeAsDouble))
-                .add(createTextCell(longitudeAsDouble))
-                .add(createTextCell(cityName))
-                .add(createTextCell(countryName))
-                .build();
-    }
-
     private static byte[] transformToByteArray(final PDDocument document)
             throws IOException {
         try (final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             document.save(outputStream);
             return outputStream.toByteArray();
+        }
+    }
+
+    @Value
+    @AllArgsConstructor
+    @Builder
+    private static class TableNameRowMetaData {
+        Color backgroundColor;
+        Color textColor;
+        PDFont font;
+        Integer fontSize;
+        HorizontalAlignment horizontalAlignment;
+        float minHeight;
+        String tableName;
+    }
+
+    @RequiredArgsConstructor
+    private static abstract class AbstractPDFTableAppender {
+        private final DistributedReportTableDrawer tableDrawer;
+        private final float[] columnWidths;
+
+        public final void appendTableToContextDocument(final UserMovementReportBuildingContext context) {
+            final ReportTable table = this.buildTable(context);
+            this.tableDrawer.draw(table, context.getDocument());
+        }
+
+        protected abstract List<Row> createContentRows(final UserMovementReportBuildingContext context);
+
+        private ReportTable buildTable(final UserMovementReportBuildingContext context) {
+            return ReportTable.builder()
+                    .rows(this.createTableRows(context))
+                    .columnWidths(this.columnWidths)
+                    .font(context.getFont())
+                    .fontSize(context.getFontSize())
+                    .borderColor(context.getBorderColor())
+                    .build();
+        }
+
+        private List<Row> createTableRows(final UserMovementReportBuildingContext context) {
+            final List<Row> tableRows = new ArrayList<>();
+            tableRows.add(this.createNameRow());
+            tableRows.addAll(this.createContentRows(context));
+            return tableRows;
+        }
+
+        private Row createNameRow(final TableNameRowMetaData metaData) {
+            return Row.builder()
+                    .add(
+                            TextCell.builder()
+                                    .backgroundColor(metaData.getBackgroundColor())
+                                    .textColor(metaData.getTextColor())
+                                    .font(metaData.getFont())
+                                    .fontSize(metaData.getFontSize())
+                                    .horizontalAlignment(metaData.getHorizontalAlignment())
+                                    .colSpan(this.columnWidths.length)
+                                    .minHeight(metaData.getMinHeight())
+                                    .text(metaData.getTableName())
+                                    .build()
+                    )
+                    .build();
+        }
+    }
+
+    private static final class UserTrackersTableAppender extends AbstractPDFTableAppender {
+        private static final float TABLE_COLUMN_WIDTH_OF_IMEI = 150;
+        private static final float TABLE_COLUMN_WIDTH_OF_PHONE_NUMBER = 150;
+        private static final float TABLE_COLUMN_WIDTH_OF_COUNT_OF_POINTS = 150;
+        private static final float[] TABLE_COLUMNS_WIDTHS = {
+                TABLE_COLUMN_WIDTH_OF_IMEI,
+                TABLE_COLUMN_WIDTH_OF_PHONE_NUMBER,
+                TABLE_COLUMN_WIDTH_OF_COUNT_OF_POINTS
+        };
+
+        public UserTrackersTableAppender(final DistributedReportTableDrawer tableDrawer) {
+            super(tableDrawer, TABLE_COLUMNS_WIDTHS);
+        }
+
+        @Override
+        protected List<Row> createContentRows(UserMovementReportBuildingContext context) {
+            return context.getPointCountsByAllTrackers()
+                    .entrySet()
+                    .stream()
+                    .map(UserTrackersTableAppender::createUserTrackersTableRow)
+                    .toList();
+        }
+
+        private static Row createUserTrackersTableRow(final Entry<Tracker, Integer> pointCountsByTracker) {
+            final Tracker tracker = pointCountsByTracker.getKey();
+            final String trackerImei = tracker.getImei();
+            final String trackerPhoneNumber = tracker.getPhoneNumber();
+            final int countOfPoints = pointCountsByTracker.getValue();
+            return Row.builder()
+                    .add(createTextCell(trackerImei))
+                    .add(createTextCell(trackerPhoneNumber))
+                    .add(createTextCell(countOfPoints))
+                    .build();
+        }
+
+        private static TableNameRowMetaData createNameRowMetaData() {
+            return TableNameRowMetaData.builder()
+                    .backgroundColor(BLUE)
+                    .textColor(WHITE)
+                    .font()
+                    .build();
+        }
+    }
+
+    private static final class UserMovementTableAppender extends AbstractPDFTableAppender {
+        private static final float TABLE_COLUMN_WIDTH_OF_TRACKER_IMEI = 100;
+        private static final float TABLE_COLUMN_WIDTH_OF_DATETIME = 100;
+        private static final float TABLE_COLUMN_WIDTH_OF_LATITUDE = 100;
+        private static final float TABLE_COLUMN_WIDTH_OF_LONGITUDE = 100;
+        private static final float TABLE_COLUMN_WIDTH_OF_CITY = 100;
+        private static final float TABLE_COLUMN_WIDTH_OF_COUNTRY = 100;
+        private static final float[] TABLE_COLUMNS_WIDTHS = {
+                TABLE_COLUMN_WIDTH_OF_TRACKER_IMEI,
+                TABLE_COLUMN_WIDTH_OF_DATETIME,
+                TABLE_COLUMN_WIDTH_OF_LATITUDE,
+                TABLE_COLUMN_WIDTH_OF_LONGITUDE,
+                TABLE_COLUMN_WIDTH_OF_CITY,
+                TABLE_COLUMN_WIDTH_OF_COUNTRY
+        };
+
+        public UserMovementTableAppender(final DistributedReportTableDrawer tableDrawer) {
+            super(tableDrawer, TABLE_COLUMNS_WIDTHS);
+        }
+
+        @Override
+        protected List<Row> createContentRows(UserMovementReportBuildingContext context) {
+
+            return null;
+        }
+
+        private static Row createUserMovementTableRow(final Data data) {
+            final LocalDateTime dateTime = data.findDateTime();
+            final double latitudeAsDouble = data.findLatitudeAsDouble();
+            final double longitudeAsDouble = data.findLongitudeAsDouble();
+            final String cityName = data.findCityName();
+            final String countryName = data.findCountryName();
+            return Row.builder()
+                    .add(createTextCell(dateTime))
+                    .add(createTextCell(latitudeAsDouble))
+                    .add(createTextCell(longitudeAsDouble))
+                    .add(createTextCell(cityName))
+                    .add(createTextCell(countryName))
+                    .build();
         }
     }
 
