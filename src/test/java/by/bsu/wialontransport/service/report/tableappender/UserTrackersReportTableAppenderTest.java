71478b03bfe8e42f3f27d4bbf00aa468541e38ea
Vlad Zuev <vladzuev.00@mail.ru>
2023-07-19 21:06:09 +0300

    refactor UserTrackersReportTableAppenderTest

--- src/test/java/by/bsu/wialontransport/service/report/tableappender/UserTrackersReportTableAppenderTest.java
+++ src/test/java/by/bsu/wialontransport/service/report/tableappender/UserTrackersReportTableAppenderTest.java
@@ -11,8 +11,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static by.bsu.wialontransport.util.CellFactoryUtil.createTextCell;
 import static by.bsu.wialontransport.util.CollectionUtil.createLinkedHashMap;
+import static java.util.Arrays.stream;
 import static org.junit.Assert.assertEquals;
 
 public final class UserTrackersReportTableAppenderTest {
@@ -28,19 +28,21 @@ public final class UserTrackersReportTableAppenderTest {
         final UserMovementReportBuildingContext givenContext = createContext(givenPointCountsByAllTrackers);
 
         final List<Row> actual = this.reportTableAppender.createContentRows(givenContext);
-        final List<String> actualRowsContents = findColumnContents(actual);
-        final List<String> expectedRowsContents = List.of(
+        final List<String> actualCellsContents = findCellsContents(actual);
+        final List<String> expectedCellsContents = List.of(
                 "11112222333344445555", "447736955", "10",
                 "11112222333344445556", "447736956", "7",
                 "11112222333344445554", "447736957", "15"
         );
-        assertEquals(expectedRowsContents, actualRowsContents);
+        assertEquals(expectedCellsContents, actualCellsContents);
     }
 
     @Test
     public void headerRowCellsShouldBeCreated() {
         final AbstractCell[] actual = this.reportTableAppender.createHeaderRowCells();
-        throw new RuntimeException();
+        final List<String> actualCellsContents = findCellsContents(actual);
+        final List<String> expectedCellsContents = List.of("Imei", "Phone number", "Count of points");
+        assertEquals(expectedCellsContents, actualCellsContents);
     }
 
     private static Tracker createTracker(final String imei, final String phoneNumber) {
@@ -56,26 +58,26 @@ public final class UserTrackersReportTableAppenderTest {
                 .build();
     }
 
-    private static List<String> findColumnContents(final List<Row> rows) {
+    private static List<String> findCellsContents(final List<Row> rows) {
         return rows.stream()
                 .flatMap(UserTrackersReportTableAppenderTest::findColumnContentsAsStream)
                 .toList();
     }
 
     private static Stream<String> findColumnContentsAsStream(final Row row) {
-        return row.getCells()
-                .stream()
-                .map(cell -> (TextCell) cell)
-                .map(TextCell::getText);
+        final List<AbstractCell> cells = row.getCells();
+        final Stream<AbstractCell> cellStream = cells.stream();
+        return findContentsAsStream(cellStream);
     }
 
-    private static Row createContentRow(final String trackerImei,
-                                        final String trackerPhoneNumber,
-                                        final Integer pointCounts) {
-        return Row.builder()
-                .add(createTextCell(trackerImei))
-                .add(createTextCell(trackerPhoneNumber))
-                .add(createTextCell(pointCounts))
-                .build();
+    private static List<String> findCellsContents(final AbstractCell[] cells) {
+        final Stream<AbstractCell> cellStream = stream(cells);
+        return findContentsAsStream(cellStream).toList();
+    }
+
+    private static Stream<String> findContentsAsStream(final Stream<AbstractCell> cellStream) {
+        return cellStream
+                .map(cell -> (TextCell) cell)
+                .map(TextCell::getText);
     }
 }
