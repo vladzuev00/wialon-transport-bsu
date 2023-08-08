c23e046c9db4a99ad40cfd03bc671a1966c7e1df
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-08 23:24:23 +0300

    refactor City and CityTest

--- src/main/java/by/bsu/wialontransport/crud/dto/City.java
+++ src/main/java/by/bsu/wialontransport/crud/dto/City.java
@@ -19,11 +19,7 @@ public class City implements AbstractDto<Long> {
     public static City copyWithAddressAndProcess(final City source,
                                                  final Address address,
                                                  final SearchingCitiesProcess process) {
-        return new City(
-                source.getId(),
-                address,
-                process
-        );
+        return new City(source.getId(), address, process);
     }
 
     public static City createWithAddress(final Address address) {
--- src/test/java/by/bsu/wialontransport/crud/dto/CityTest.java
+++ src/test/java/by/bsu/wialontransport/crud/dto/CityTest.java
@@ -1,12 +1,20 @@
 package by.bsu.wialontransport.crud.dto;
 
+import by.bsu.wialontransport.base.AbstractContextTest;
 import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static by.bsu.wialontransport.crud.dto.City.copyWithAddressAndProcess;
 import static by.bsu.wialontransport.crud.dto.City.createWithAddress;
+import static by.bsu.wialontransport.util.GeometryTestUtil.createPolygon;
 import static org.junit.Assert.assertEquals;
 
-public final class CityTest {
+public final class CityTest extends AbstractContextTest {
+
+    @Autowired
+    private GeometryFactory geometryFactory;
 
     @Test
     public void cityShouldBeCopiedWithGivenAddressAndProcess() {
@@ -28,9 +36,28 @@ public final class CityTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void geometryOfCityShouldBeFound() {
+        final Geometry givenGeometry = createPolygon(
+                this.geometryFactory,
+                1.1, 2.2, 3.3, 4.4, 5.5, 6.6
+        );
+        final Address givenAddress = createAddress(givenGeometry);
+        final City givenCity = createCity(givenAddress);
+
+        final Geometry actual = givenCity.findGeometry();
+        assertEquals(givenGeometry, actual);
+    }
+
     private static City createCity(final Long id) {
         return City.builder()
                 .id(id)
+                .build();
+    }
+
+    private static City createCity(final Address address) {
+        return City.builder()
+                .address(address)
                 .build();
     }
 
@@ -40,15 +67,15 @@ public final class CityTest {
                 .build();
     }
 
-    private static SearchingCitiesProcess createProcess(final Long id) {
-        return SearchingCitiesProcess.builder()
-                .id(id)
+    private static Address createAddress(final Geometry geometry) {
+        return Address.builder()
+                .geometry(geometry)
                 .build();
     }
 
-    private static City createCity(final Address address) {
-        return City.builder()
-                .address(address)
+    private static SearchingCitiesProcess createProcess(final Long id) {
+        return SearchingCitiesProcess.builder()
+                .id(id)
                 .build();
     }
 }
