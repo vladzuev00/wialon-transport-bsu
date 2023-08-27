a3ba946b5be69a2eef9bf8cb3733210755f5c00f
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-27 17:28:19 +0300

    add TrackerOdometerEntity

---
+++ src/main/java/by/bsu/wialontransport/crud/entity/TrackerOdometerEntity.java
@@ -0,0 +1,29 @@
+package by.bsu.wialontransport.crud.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "cities")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class TrackerOdometerEntity extends AbstractEntity<Long> {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "urban")
+    private double urban;
+
+    @Column(name = "country")
+    private double country;
+}
