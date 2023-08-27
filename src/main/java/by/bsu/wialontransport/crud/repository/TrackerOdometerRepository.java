307813c3136b4b102b2e6385919214c6d4e60292
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-27 17:33:02 +0300

    add TrackerOdometerRepository

---
+++ src/main/java/by/bsu/wialontransport/crud/repository/TrackerOdometerRepository.java
@@ -0,0 +1,8 @@
+package by.bsu.wialontransport.crud.repository;
+
+import by.bsu.wialontransport.crud.entity.TrackerOdometerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrackerOdometerRepository extends JpaRepository<TrackerOdometerEntity, Long> {
+
+}
