3062a09dd732f3cf30695aa84fe658105c3ae9a4
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-03 23:14:23 +0300

    add TrackerMileageService::increaseMileage

--- src/main/java/by/bsu/wialontransport/crud/service/TrackerMileageService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/TrackerMileageService.java
@@ -1,9 +1,11 @@
 package by.bsu.wialontransport.crud.service;
 
+import by.bsu.wialontransport.crud.dto.Tracker;
 import by.bsu.wialontransport.crud.dto.TrackerMileage;
 import by.bsu.wialontransport.crud.entity.TrackerMileageEntity;
 import by.bsu.wialontransport.crud.mapper.TrackerMileageMapper;
 import by.bsu.wialontransport.crud.repository.TrackerMileageRepository;
+import by.bsu.wialontransport.model.Mileage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,10 @@ public class TrackerMileageService
         super(mapper, repository);
     }
 
-
-
+    public void increaseMileage(final Tracker tracker, final Mileage mileageDelta) {
+        final Long trackerId = tracker.getId();
+        final double urbanDelta = mileageDelta.getUrban();
+        final double countryDelta = mileageDelta.getCountry();
+        this.repository.increaseMileage(trackerId, urbanDelta, countryDelta);
+    }
 }
