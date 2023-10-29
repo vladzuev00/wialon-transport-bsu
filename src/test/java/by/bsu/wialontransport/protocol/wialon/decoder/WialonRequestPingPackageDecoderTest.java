c98c3eea74443f07abad6b546d78401c68b7c816
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-30 00:41:04 +0300

    add WialonRequestPingPackageDecoderTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoderTest.java
@@ -0,0 +1,18 @@
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.WialonRequestPingPackage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public final class WialonRequestPingPackageDecoderTest {
+    private final WialonRequestPingPackageDecoder decoder = new WialonRequestPingPackageDecoder();
+
+    @Test
+    public void messageShouldBeDecoded() {
+        final String givenMessage = "";
+
+        final WialonRequestPingPackage actual = this.decoder.decodeMessage(givenMessage);
+        assertNotNull(actual);
+    }
+}
