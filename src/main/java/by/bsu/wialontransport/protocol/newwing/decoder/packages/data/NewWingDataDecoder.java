6fc77254de875f2ca4fc1aac770dcdb16ef83e91
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-02 00:52:04 +0300

    add NewWingDataIterator

---
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataDecoder.java
@@ -0,0 +1,17 @@
+package by.bsu.wialontransport.protocol.newwing.decoder.packages.data;
+
+import by.bsu.wialontransport.protocol.newwing.model.NewWingData;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class NewWingDataDecoder {
+
+    public NewWingData decodeNext(final ByteBuf buffer) {
+//        final short hours = buffer.readShortLE();
+//        final short minutes = buffer.readUnsignedByte();
+//        final short seconds = buffer.readUnsignedByte();
+//        final
+        return null;
+    }
+}
