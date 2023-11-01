e375204adf7b28b7eafeb121514d7631eeb8b8e4
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-01 23:49:27 +0300

    refactor PackageBufferDecoder

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageBufferDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageBufferDecoder.java
@@ -3,6 +3,11 @@ package by.bsu.wialontransport.protocol.core.decoder.packages;
 import by.bsu.wialontransport.protocol.protocolpackage.Package;
 import io.netty.buffer.ByteBuf;
 
-public abstract class PackageBufferDecoder<P extends Package> extends PackageDecoder<ByteBuf, P> {
+public abstract class PackageBufferDecoder<PREFIX, PACKAGE extends Package>
+        extends PackageDecoder<PREFIX, ByteBuf, PACKAGE> {
+
+    public PackageBufferDecoder(final PREFIX packagePrefix) {
+        super(packagePrefix);
+    }
 
 }
