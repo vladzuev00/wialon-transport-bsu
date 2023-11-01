329a0ab3eefc8021b0699491a18ffd45e4d6ee06
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-02 00:42:05 +0300

    add DataNewWingPackage

---
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/model/packages/DataNewWingPackage.java
@@ -0,0 +1,16 @@
+package by.bsu.wialontransport.protocol.newwing.model.packages;
+
+import by.bsu.wialontransport.protocol.newwing.model.NewWingData;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public final class DataNewWingPackage extends NewWingPackage {
+    private final List<NewWingData> data;
+
+    public DataNewWingPackage(final int checksum, final List<NewWingData> data) {
+        super(checksum);
+        this.data = data;
+    }
+}
