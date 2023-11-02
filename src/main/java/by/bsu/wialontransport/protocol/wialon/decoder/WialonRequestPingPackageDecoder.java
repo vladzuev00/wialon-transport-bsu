5fafb5bfb9b077b4ccc526511c2b0cc3e25ae86a
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-03 00:31:10 +0300

    refactor WialonRequestPingPackageDecoder and WialonRequestPingPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoder.java
@@ -1,17 +1,17 @@
-//package by.bsu.wialontransport.protocol.wialon.decoder;
-//
-//import by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.WialonRequestPingPackage;
-//
-//import static by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.WialonRequestPingPackage.PREFIX;
-//
-//public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder<WialonRequestPingPackage> {
-//
-//    public WialonRequestPingPackageDecoder() {
-//        super(PREFIX);
-//    }
-//
-//    @Override
-//    protected WialonRequestPingPackage decodeMessage(final String message) {
-//        return new WialonRequestPingPackage();
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.WialonRequestPingPackage;
+
+import static by.bsu.wialontransport.protocol.wialon.wialonpackage.ping.WialonRequestPingPackage.PREFIX;
+
+public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder<WialonRequestPingPackage> {
+
+    public WialonRequestPingPackageDecoder() {
+        super(PREFIX);
+    }
+
+    @Override
+    protected WialonRequestPingPackage decodeMessage(final String message) {
+        return new WialonRequestPingPackage();
+    }
+}
