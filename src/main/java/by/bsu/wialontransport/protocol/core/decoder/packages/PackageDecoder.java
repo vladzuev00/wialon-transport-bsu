6be053e41f039d972f6ad052a1f04970467f616f
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-02 23:15:08 +0300

    refactor WialonPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageDecoder.java
@@ -1,11 +1,13 @@
 package by.bsu.wialontransport.protocol.core.decoder.packages;
 
 import by.bsu.wialontransport.protocol.protocolpackage.Package;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Objects;
 
 @RequiredArgsConstructor
+@Getter
 public abstract class PackageDecoder<PREFIX, SOURCE, PACKAGE extends Package> {
     private final PREFIX packagePrefix;
 
--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonPackageDecoder.java
@@ -1,31 +1,32 @@
-//package by.bsu.wialontransport.protocol.wialon.decoder;
-//
-//import by.bsu.wialontransport.protocol.core.decoder.packages.PackageStringDecoder;
-//import by.bsu.wialontransport.protocol.wialon.wialonpackage.WialonPackage;
-//import lombok.RequiredArgsConstructor;
-//
-//import static by.bsu.wialontransport.protocol.wialon.wialonpackage.WialonPackage.POSTFIX;
-//
-//@RequiredArgsConstructor
-//public abstract class WialonPackageDecoder<P extends WialonPackage> extends PackageStringDecoder<P> {
-//    private final String packagePrefix;
-//
-//    @Override
-//    public final boolean isAbleToDecode(final String source) {
-//        return source.startsWith(this.packagePrefix);
-//    }
-//
-//    @Override
-//    public final P decode(final String source) {
-//        final String message = this.extractMessage(source);
-//        return this.decodeMessage(message);
-//    }
-//
-//    protected abstract P decodeMessage(final String message);
-//
-//    private String extractMessage(final String source) {
-//        final int startMessageIndex = this.packagePrefix.length();
-//        final int endMessageNextIndex = source.length() - POSTFIX.length();
-//        return source.substring(startMessageIndex, endMessageNextIndex);
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+import by.bsu.wialontransport.protocol.core.decoder.packages.PackageStringDecoder;
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.WialonPackage;
+
+import static by.bsu.wialontransport.protocol.wialon.wialonpackage.WialonPackage.POSTFIX;
+
+public abstract class WialonPackageDecoder<PACKAGE extends WialonPackage> extends PackageStringDecoder<PACKAGE> {
+
+    public WialonPackageDecoder(final String packagePrefix) {
+        super(packagePrefix);
+    }
+
+    @Override
+    public final PACKAGE decode(final String source) {
+        final String message = this.extractMessage(source);
+        return this.decodeMessage(message);
+    }
+
+    protected abstract PACKAGE decodeMessage(final String message);
+
+    private String extractMessage(final String source) {
+        final int startMessageIndex = this.findPackagePrefixLength();
+        final int endMessageNextIndex = source.length() - POSTFIX.length();
+        return source.substring(startMessageIndex, endMessageNextIndex);
+    }
+
+    private int findPackagePrefixLength() {
+        final String packagePrefix = super.getPackagePrefix();
+        return packagePrefix.length();
+    }
+}
