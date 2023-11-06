7a446d6abfac28d99f78c0a8c45eb2f5aa2eee9e
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-06 13:05:12 +0300

    add NewWingProtocolDecoderTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingProtocolDecoderTest.java
@@ -0,0 +1,32 @@
+package by.bsu.wialontransport.protocol.newwing.decoder;
+
+import io.netty.buffer.ByteBuf;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class NewWingProtocolDecoderTest {
+    private static final int EXPECTED_PACKAGE_PREFIX_LENGTH = 6;
+    private static final Charset EXPECTED_PACKAGE_PREFIX_CHARSET = UTF_8;
+
+    private final NewWingProtocolDecoder decoder = new NewWingProtocolDecoder(null);
+
+    @Test
+    public void packagePrefixShouldBeExtracted() {
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
+
+        final String givenPackagePrefix = "TEST";
+        when(givenBuffer.readCharSequence(eq(EXPECTED_PACKAGE_PREFIX_LENGTH), same(EXPECTED_PACKAGE_PREFIX_CHARSET)))
+                .thenReturn(givenPackagePrefix);
+
+        final String actual = this.decoder.extractPackagePrefix(givenBuffer);
+        assertSame(givenPackagePrefix, actual);
+    }
+}
