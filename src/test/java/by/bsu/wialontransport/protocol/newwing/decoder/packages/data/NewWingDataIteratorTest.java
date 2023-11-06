7341cbfedc3a6294e9ad6fe7ebbe06e5efa70b80
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-06 14:09:43 +0300

    add NewWingDataIteratorTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataIteratorTest.java
@@ -0,0 +1,58 @@
+package by.bsu.wialontransport.protocol.newwing.decoder.packages.data;
+
+import by.bsu.wialontransport.protocol.newwing.model.NewWingData;
+import io.netty.buffer.ByteBuf;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static by.bsu.wialontransport.util.CollectionUtil.convertToList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class NewWingDataIteratorTest {
+
+    @Test
+    public void bufferShouldBeIterated() {
+        final NewWingDataDecoder givenDataDecoder = mock(NewWingDataDecoder.class);
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
+        final int givenDataCount = 3;
+        final NewWingDataIterator givenIterator = new NewWingDataIterator(givenDataDecoder, givenBuffer, givenDataCount);
+
+        final NewWingData firstGivenData = createData(2, 9, 20);
+        final NewWingData secondGivenData = createData(3, 10, 21);
+        final NewWingData thirdGivenData = createData(4, 11, 22);
+        when(givenDataDecoder.decodeNext(same(givenBuffer)))
+                .thenReturn(firstGivenData)
+                .thenReturn(secondGivenData)
+                .thenReturn(thirdGivenData);
+
+        final List<NewWingData> actual = convertToList(givenIterator);
+        final List<NewWingData> expected = List.of(firstGivenData, secondGivenData, thirdGivenData);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void bufferShouldNotBeIteratedBecauseOfNoSuchElementException() {
+        final NewWingDataDecoder givenDataDecoder = mock(NewWingDataDecoder.class);
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
+        final int givenDataCount = 3;
+        final NewWingDataIterator givenIterator = new NewWingDataIterator(givenDataDecoder, givenBuffer, givenDataCount);
+
+        givenIterator.next();
+        givenIterator.next();
+        givenIterator.next();
+        givenIterator.next();
+    }
+
+    private static NewWingData createData(final int day, final int month, final int year) {
+        return NewWingData.builder()
+                .day((byte) day)
+                .month((byte) month)
+                .year((byte) year)
+                .build();
+    }
+}
