29e3314c18a0a7c57409a95cd9f4ca5a1fceaf62
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-06 15:44:24 +0300

    add PackageHandler

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/PackageHandler.java
@@ -0,0 +1,21 @@
+package by.bsu.wialontransport.protocol.core.handler.packages;
+
+import by.bsu.wialontransport.protocol.protocolpackage.Package;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class PackageHandler<PACKAGE extends Package> {
+    private final Class<PACKAGE> handledPackageType;
+
+    public final boolean isAbleToHandle(final Package requestPackage) {
+        return this.handledPackageType.isInstance(requestPackage);
+    }
+
+    public final void handle(final Package requestPackage, final ChannelHandlerContext context) {
+        final PACKAGE concreteRequestPackage = this.handledPackageType.cast(requestPackage);
+        this.handleConcretePackage(concreteRequestPackage, context);
+    }
+
+    protected abstract void handleConcretePackage(final PACKAGE requestPackage, final ChannelHandlerContext context);
+}
