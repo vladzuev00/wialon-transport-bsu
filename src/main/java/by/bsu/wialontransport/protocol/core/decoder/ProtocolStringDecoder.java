b49428364efcd1a4560a2c7a8358a379fb2c7cd3
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-06 11:08:09 +0300

    refactor ProtocolStringDecoder and ProtocolStringDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/ProtocolStringDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/ProtocolStringDecoder.java
@@ -5,19 +5,60 @@ import io.netty.buffer.ByteBuf;
 
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.regex.Pattern.compile;
 
 public abstract class ProtocolStringDecoder<DECODER extends PackageStringDecoder<?>>
         extends ProtocolDecoder<String, String, DECODER> {
     private static final Charset CHARSET_TO_DECODE_BUFFER = UTF_8;
 
-    public ProtocolStringDecoder(final List<DECODER> packageDecoders) {
+    private final Pattern packagePrefixPattern;
+
+    public ProtocolStringDecoder(final List<DECODER> packageDecoders, final String packagePrefixRegex) {
         super(packageDecoders);
+        this.packagePrefixPattern = compile(packagePrefixRegex);
     }
 
     @Override
-    public final String createSource(final ByteBuf buffer) {
+    protected final String createSource(final ByteBuf buffer) {
         return buffer.toString(CHARSET_TO_DECODE_BUFFER);
+    }
+
+    @Override
+    protected final String extractPackagePrefix(final String source) {
+        return this.packagePrefixPattern.matcher(source)
+                .results()
+                .map(MatchResult::group)
+                .findFirst()
+                .orElseThrow(
+                        () -> new PackagePrefixExtractingException(
+                                "Impossible to extract package's prefix from '%s'".formatted(source)
+                        )
+                );
+    }
+
+    static final class PackagePrefixExtractingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public PackagePrefixExtractingException() {
+
+        }
+
+        public PackagePrefixExtractingException(final String description) {
+            super(description);
+        }
+
+        @SuppressWarnings("unused")
+        public PackagePrefixExtractingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public PackagePrefixExtractingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
     }
 }
---
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonProtocolDecoderTest.java
@@ -0,0 +1,7 @@
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+public final class WialonProtocolDecoderTest {
+    private final WialonProtocolDecoder decoder = new WialonProtocolDecoder(null);
+
+
+}
