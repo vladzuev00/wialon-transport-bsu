08c0a1565ac6142573cd87056a5cc76f27f9f98a
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-08 23:21:55 +0300

    refactor TrackerLoginService

--- src/main/java/by/bsu/wialontransport/protocol/core/service/login/TrackerLoginService.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/service/login/TrackerLoginService.java
@@ -8,6 +8,7 @@ import by.bsu.wialontransport.protocol.core.connectionmanager.ConnectionManager;
 import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
 import by.bsu.wialontransport.protocol.core.model.packages.LoginPackage;
 import by.bsu.wialontransport.protocol.core.model.packages.Package;
+import by.bsu.wialontransport.protocol.core.service.login.responseprovider.LoginResponseProvider;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.RequiredArgsConstructor;
 
@@ -15,60 +16,65 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 @RequiredArgsConstructor
-public abstract class TrackerLoginService<PACKAGE extends LoginPackage> {
+public abstract class TrackerLoginService<PACKAGE extends LoginPackage, RESPONSE_PROVIDER extends LoginResponseProvider> {
     private final ContextAttributeManager contextAttributeManager;
     private final TrackerService trackerService;
     private final ConnectionManager connectionManager;
     private final DataService dataService;
 
-    public final void login(final PACKAGE loginPackage, final ChannelHandlerContext context) {
+    public final void login(final PACKAGE loginPackage,
+                            final RESPONSE_PROVIDER responseProvider,
+                            final ChannelHandlerContext context) {
         final String trackerImei = loginPackage.getImei();
         this.contextAttributeManager.putTrackerImei(context, trackerImei);
         final Optional<Tracker> optionalTracker = this.trackerService.findByImei(trackerImei);
         optionalTracker.ifPresentOrElse(
-                tracker -> this.loginExistingTracker(tracker, loginPackage, context),
-                () -> this.sendNoSuchImeiResponse(context)
+                tracker -> this.loginExistingTracker(tracker, loginPackage, responseProvider, context),
+                () -> this.sendNoSuchImeiResponse(responseProvider, context)
         );
     }
 
-    protected abstract Package createNoSuchImeiResponse();
-
-    protected abstract Package createSuccessResponse();
-
-    protected abstract Optional<Package> checkPackageAndCreateResponseIfLoginFailed(final Tracker tracker,
-                                                                                    final PACKAGE loginPackage);
+    protected abstract Optional<Package> checkPackageAndTakeResponseIfLoginFailed(
+            final Tracker tracker,
+            final PACKAGE loginPackage,
+            final RESPONSE_PROVIDER responseProvider
+    );
 
     private void loginExistingTracker(final Tracker tracker,
                                       final PACKAGE loginPackage,
+                                      final RESPONSE_PROVIDER responseProvider,
                                       final ChannelHandlerContext context) {
-        final Optional<Package> optionalFailedResponse = this.checkPackageAndCreateResponseIfLoginFailed(
+        final Optional<Package> optionalFailedResponse = this.checkPackageAndTakeResponseIfLoginFailed(
                 tracker,
-                loginPackage
+                loginPackage,
+                responseProvider
         );
         optionalFailedResponse.ifPresentOrElse(
                 context::writeAndFlush,
-                () -> this.handleSuccessLogin(tracker, context)
+                () -> this.handleSuccessLogin(tracker, responseProvider, context)
         );
     }
 
-    private void sendNoSuchImeiResponse(final ChannelHandlerContext context) {
-        sendResponse(context, this::createNoSuchImeiResponse);
+    private void sendNoSuchImeiResponse(final RESPONSE_PROVIDER responseProvider, final ChannelHandlerContext context) {
+        sendResponse(responseProvider::getNoSuchImeiResponse, context);
     }
 
-    private void sendSuccessResponse(final ChannelHandlerContext context) {
-        sendResponse(context, this::createSuccessResponse);
+    private void sendSuccessResponse(final RESPONSE_PROVIDER responseProvider, final ChannelHandlerContext context) {
+        sendResponse(responseProvider::getSuccessResponse, context);
     }
 
-    private static void sendResponse(final ChannelHandlerContext context, final Supplier<Package> responseSupplier) {
+    private static void sendResponse(final Supplier<Package> responseSupplier, final ChannelHandlerContext context) {
         final Package response = responseSupplier.get();
         context.writeAndFlush(response);
     }
 
-    private void handleSuccessLogin(final Tracker tracker, final ChannelHandlerContext context) {
+    private void handleSuccessLogin(final Tracker tracker,
+                                    final RESPONSE_PROVIDER responseProvider,
+                                    final ChannelHandlerContext context) {
         this.contextAttributeManager.putTracker(context, tracker);
         this.connectionManager.add(context);
         this.putLastDataInContextIfExist(tracker, context);
-        this.sendSuccessResponse(context);
+        this.sendSuccessResponse(responseProvider, context);
     }
 
     private void putLastDataInContextIfExist(final Tracker tracker, final ChannelHandlerContext context) {
