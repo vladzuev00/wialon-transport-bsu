9dcb68f8728adfc6dd7659b9e028454899b7b581
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-21 01:14:59 +0300

    add ResponseNewWingPackage

---
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/model/packages/response/ResponseNewWingPackage.java
@@ -0,0 +1,8 @@
+package by.bsu.wialontransport.protocol.newwing.model.packages.response;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class ResponseNewWingPackage {
+    private final String value;
+}
