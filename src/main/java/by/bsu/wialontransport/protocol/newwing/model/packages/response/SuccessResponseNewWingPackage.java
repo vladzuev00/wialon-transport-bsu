0a926d06733f6feab3eceef6046f758f0d504fa4
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-21 01:15:53 +0300

    add SuccessResponseNewWingPackage

---
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/model/packages/response/SuccessResponseNewWingPackage.java
@@ -0,0 +1,10 @@
+package by.bsu.wialontransport.protocol.newwing.model.packages.response;
+
+public final class SuccessResponseNewWingPackage extends ResponseNewWingPackage {
+    private static final String VALUE = "TTTTT";
+
+    public SuccessResponseNewWingPackage() {
+        super(VALUE);
+    }
+
+}
