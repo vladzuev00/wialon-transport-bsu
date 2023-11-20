d0853a0f724821836a0ea409dda718144b5cee5a
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-21 01:18:37 +0300

    add NewWingLoginResponseProvider

--- src/main/java/by/bsu/wialontransport/protocol/newwing/model/packages/response/ResponseNewWingPackage.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/model/packages/response/ResponseNewWingPackage.java
@@ -1,10 +1,11 @@
 package by.bsu.wialontransport.protocol.newwing.model.packages.response;
 
+import by.bsu.wialontransport.protocol.core.model.packages.Package;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
-public abstract class ResponseNewWingPackage {
+public abstract class ResponseNewWingPackage implements Package {
     private final String value;
 }
