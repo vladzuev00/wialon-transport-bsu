9215a6a31aca0750516533a43d3fbc3bc4e9e902
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-04 01:02:18 +0300

    refactor AddressMapperTest

--- src/test/java/by/bsu/wialontransport/crud/mapper/AddressMapperTest.java
+++ src/test/java/by/bsu/wialontransport/crud/mapper/AddressMapperTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static by.bsu.wialontransport.util.GeometryTestUtil.createPoint;
 import static by.bsu.wialontransport.util.GeometryTestUtil.createPolygon;
 
+import static by.bsu.wialontransport.util.entity.AddressEntityUtil.checkEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -41,7 +42,6 @@ public final class AddressMapperTest extends AbstractContextTest {
                 .countryName("country")
                 .geometry(createPolygon(this.geometryFactory, 1, 2, 3, 4, 5, 6))
                 .build();
-
         assertNotNull(actual);
         checkEquals(expected, actual);
     }
@@ -67,13 +67,5 @@ public final class AddressMapperTest extends AbstractContextTest {
                 .geometry(createPolygon(this.geometryFactory, 2, 3, 4, 5, 6, 7))
                 .build();
         assertEquals(expected, actual);
-    }
-
-    private static void checkEquals(final AddressEntity expected, final AddressEntity actual) {
-        assertEquals(expected.getId(), actual.getId());
-        assertEquals(expected.getBoundingBox(), actual.getBoundingBox());
-        assertEquals(expected.getCenter(), actual.getCenter());
-        assertEquals(expected.getCityName(), actual.getCityName());
-        assertEquals(expected.getCountryName(), actual.getCountryName());
     }
 }
