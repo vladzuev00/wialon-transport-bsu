491ce1ca99e6c62fff61eaac0e731c79d8246797
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-03 21:50:43 +0300

    refactor AbstractCRUDService

--- src/main/java/by/bsu/wialontransport/crud/service/AbstractCRUDService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/AbstractCRUDService.java
@@ -8,43 +8,41 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 import java.util.List;
 
-public abstract class AbstractCRUDService<
-        IdType,
-        EntityType extends Entity<IdType>,
-        DtoType extends Dto<IdType>,
-        MapperType extends Mapper<EntityType, DtoType>,
-        RepositoryType extends JpaRepository<EntityType, IdType>
-        >
-        extends AbstractRUDService<IdType, EntityType, DtoType, MapperType, RepositoryType> {
+import static by.bsu.wialontransport.util.CollectionUtil.mapToList;
 
-    public AbstractCRUDService(final MapperType mapper, final RepositoryType repository) {
+public abstract class AbstractCRUDService<
+        ID,
+        ENTITY extends Entity<ID>,
+        DTO extends Dto<ID>,
+        MAPPER extends Mapper<ENTITY, DTO>,
+        REPOSITORY extends JpaRepository<ENTITY, ID>
+        >
+        extends AbstractRUDService<ID, ENTITY, DTO, MAPPER, REPOSITORY> {
+
+    public AbstractCRUDService(final MAPPER mapper, final REPOSITORY repository) {
         super(mapper, repository);
     }
 
-    public DtoType save(final DtoType saved) {
-        final EntityType entityToBeSaved = this.mapToConfiguredEntity(saved);
-        final EntityType savedEntity = super.repository.save(entityToBeSaved);
+    public DTO save(final DTO dto) {
+        final ENTITY entityToBeSaved = this.mapToConfiguredEntity(dto);
+        final ENTITY savedEntity = super.repository.save(entityToBeSaved);
         return super.mapper.mapToDto(savedEntity);
     }
 
-    public List<DtoType> saveAll(final Collection<DtoType> saved) {
-        final List<EntityType> entitiesToBeSaved = this.mapToConfiguredEntities(saved);
-        final List<EntityType> savedEntities = super.repository.saveAll(entitiesToBeSaved);
+    public List<DTO> saveAll(final Collection<DTO> dtos) {
+        final List<ENTITY> entitiesToBeSaved = this.mapToConfiguredEntities(dtos);
+        final List<ENTITY> savedEntities = super.repository.saveAll(entitiesToBeSaved);
         return super.mapper.mapToDtos(savedEntities);
     }
 
-    protected EntityType configureBeforeSave(final EntityType source) {
-        return source;
-    }
+    protected abstract ENTITY configureBeforeSave(final ENTITY source);
 
-    private EntityType mapToConfiguredEntity(final DtoType source) {
-        final EntityType sourceEntity = super.mapper.mapToEntity(source);
+    private ENTITY mapToConfiguredEntity(final DTO source) {
+        final ENTITY sourceEntity = super.mapper.mapToEntity(source);
         return this.configureBeforeSave(sourceEntity);
     }
 
-    private List<EntityType> mapToConfiguredEntities(final Collection<DtoType> source) {
-        return source.stream()
-                .map(this::mapToConfiguredEntity)
-                .toList();
+    private List<ENTITY> mapToConfiguredEntities(final Collection<DTO> sources) {
+        return mapToList(sources, this::mapToConfiguredEntity);
     }
 }
