0ac698624af5b1812736f3ec07de28b25faeb534
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-03 19:43:36 +0300

    refactor SearchingCitiesProcessMapper

--- src/main/java/by/bsu/wialontransport/crud/mapper/SearchingCitiesProcessMapper.java
+++ src/main/java/by/bsu/wialontransport/crud/mapper/SearchingCitiesProcessMapper.java
@@ -6,22 +6,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public final class SearchingCitiesProcessMapper
-        extends Mapper<SearchingCitiesProcessEntity, SearchingCitiesProcess> {
+public final class SearchingCitiesProcessMapper extends Mapper<SearchingCitiesProcessEntity, SearchingCitiesProcess> {
 
     public SearchingCitiesProcessMapper(final ModelMapper modelMapper) {
         super(modelMapper, SearchingCitiesProcessEntity.class, SearchingCitiesProcess.class);
     }
 
     @Override
-    protected SearchingCitiesProcess createDto(final SearchingCitiesProcessEntity entity) {
+    protected SearchingCitiesProcess createDto(final SearchingCitiesProcessEntity source) {
         return new SearchingCitiesProcess(
-                entity.getId(),
-                entity.getBounds(),
-                entity.getSearchStep(),
-                entity.getTotalPoints(),
-                entity.getHandledPoints(),
-                entity.getStatus()
+                source.getId(),
+                source.getBounds(),
+                source.getSearchStep(),
+                source.getTotalPoints(),
+                source.getHandledPoints(),
+                source.getStatus()
         );
+    }
+
+    @Override
+    protected void mapSpecificFields(final SearchingCitiesProcess source,
+                                     final SearchingCitiesProcessEntity destination) {
+
     }
 }
