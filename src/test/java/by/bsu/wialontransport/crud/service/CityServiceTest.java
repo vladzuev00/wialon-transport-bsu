b5318709b1e3ee513e9f9c8229964115c5475ec8
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-19 01:00:55 +0300

    refactor CityServiceTest

--- src/test/java/by/bsu/wialontransport/crud/service/CityServiceTest.java
+++ src/test/java/by/bsu/wialontransport/crud/service/CityServiceTest.java
@@ -20,20 +20,11 @@ public final class CityServiceTest extends AbstractContextTest {
     private GeometryFactory geometryFactory;
 
     @Test
-    @Sql(statements = "INSERT INTO addresses"
-            + "(id, bounding_box, center, city_name, country_name, geometry) "
-            + "VALUES(255, ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))', 4326), "
-            + "ST_SetSRID(ST_POINT(53.050286, 24.873635), 4326), 'city', 'country', "
-            + "ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 1 2))', 4326)"
-            + ")")
-    @Sql(statements = "INSERT INTO searching_cities_processes("
-            + "id, bounds, search_step, total_points, handled_points, status) "
-            + "VALUES(256, ST_GeomFromText('POLYGON((1 1, 1 4, 4 4, 4 1, 1 1))', 4326), 0.5, 1000, 100, 'HANDLING')")
-    @Sql(statements = "INSERT INTO cities(id, address_id, searching_cities_process_id) VALUES(257, 255, 256)")
+    @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldExistByGeometry() {
         final Geometry givenGeometry = createPolygon(
                 this.geometryFactory,
-                1, 2, 3, 4, 5, 6
+                3, 1, 4, 1, 4, 2, 3, 2
         );
 
         final boolean exists = this.cityService.isExistByGeometry(givenGeometry);
@@ -41,15 +32,7 @@ public final class CityServiceTest extends AbstractContextTest {
     }
 
     @Test
-    @Sql(statements = "INSERT INTO addresses"
-            + "(id, bounding_box, center, city_name, country_name, geometry) "
-            + "VALUES(255, ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))', 4326), "
-            + "ST_SetSRID(ST_POINT(53.050286, 24.873635), 4326), 'city', 'country', "
-            + "ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 1 2))', 4326)"
-            + ")")
-    @Sql(statements = "INSERT INTO searching_cities_processes("
-            + "id, bounds, search_step, total_points, handled_points, status) "
-            + "VALUES(256, ST_GeomFromText('POLYGON((1 1, 1 4, 4 4, 4 1, 1 1))', 4326), 0.5, 1000, 100, 'HANDLING')")
+    @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldNotExistByGeometry() {
         final Geometry givenGeometry = createPolygon(
                 this.geometryFactory,
