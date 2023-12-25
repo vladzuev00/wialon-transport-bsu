b2435e172eeb92b04ba154f87461f43b4975a770
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2023-12-25 16:38:49 +0300

    refactor AddressService

--- src/main/java/by/bsu/wialontransport/crud/service/AddressService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/AddressService.java
@@ -27,7 +27,7 @@ public class AddressService extends CRUDService<Long, AddressEntity, Address, Ad
 
     @Transactional(readOnly = true)
     public Optional<Address> findByGpsCoordinates(final Coordinate coordinate) {
-        return super.findUnique(
+        return findUnique(
                 repository -> repository.findByGpsCoordinates(
                         coordinate.getLatitude(),
                         coordinate.getLongitude()
@@ -37,17 +37,17 @@ public class AddressService extends CRUDService<Long, AddressEntity, Address, Ad
 
     @Transactional(readOnly = true)
     public Optional<Address> findByGeometry(final Geometry geometry) {
-        return super.findUnique(repository -> repository.findByGeometry(geometry));
+        return findUnique(repository -> repository.findByGeometry(geometry));
     }
 
     @Transactional(readOnly = true)
     public boolean isExistByGeometry(final Geometry geometry) {
-        return super.findBoolean(repository -> repository.isExistByGeometry(geometry));
+        return findBoolean(repository -> repository.isExistByGeometry(geometry));
     }
 
     @Transactional(readOnly = true)
     public Set<PreparedGeometry> findCitiesPreparedGeometriesIntersectedByLineString(final LineString lineString) {
-        return super.findEntityStreamAndCollect(
+        return findEntityStreamAndCollect(
                 repository -> repository.findCityAddressesIntersectedByLineString(lineString),
                 mapping(AddressService::extractPreparedGeometry, toUnmodifiableSet())
         );
