60faca6a25a7effa0c92f5eae4dcf04e91488913
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2023-12-25 16:13:28 +0300

    refactor CityRepositoryTest

--- src/test/java/by/bsu/wialontransport/crud/repository/CityRepositoryTest.java
+++ src/test/java/by/bsu/wialontransport/crud/repository/CityRepositoryTest.java
@@ -26,17 +26,17 @@ public final class CityRepositoryTest extends AbstractContextTest {
     @Test
     @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldBeFoundById() {
-        super.startQueryCount();
-        final CityEntity actual = this.repository.findById(258L).orElseThrow();
-        super.checkQueryCount(1);
+        startQueryCount();
+        final CityEntity actual = repository.findById(258L).orElseThrow();
+        checkQueryCount(1);
 
         assertFalse(isAddressLoaded(actual));
         assertFalse(isSearchingCitiesProcessLoaded(actual));
 
         final CityEntity expected = CityEntity.builder()
                 .id(258L)
-                .address(super.entityManager.getReference(AddressEntity.class, 257L))
-                .searchingCitiesProcess(super.entityManager.getReference(SearchingCitiesProcessEntity.class, 254L))
+                .address(entityManager.getReference(AddressEntity.class, 257L))
+                .searchingCitiesProcess(entityManager.getReference(SearchingCitiesProcessEntity.class, 254L))
                 .build();
         checkEquals(expected, actual);
     }
@@ -45,24 +45,24 @@ public final class CityRepositoryTest extends AbstractContextTest {
     @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldBeSaved() {
         final CityEntity givenCity = CityEntity.builder()
-                .address(super.entityManager.getReference(AddressEntity.class, 255L))
-                .searchingCitiesProcess(super.entityManager.getReference(SearchingCitiesProcessEntity.class, 254L))
+                .address(entityManager.getReference(AddressEntity.class, 255L))
+                .searchingCitiesProcess(entityManager.getReference(SearchingCitiesProcessEntity.class, 254L))
                 .build();
 
-        super.startQueryCount();
-        this.repository.save(givenCity);
-        super.checkQueryCount(2);
+        startQueryCount();
+        repository.save(givenCity);
+        checkQueryCount(2);
     }
 
     @Test
     @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldExistByGeometry() {
         final Geometry givenGeometry = createPolygon(
-                this.geometryFactory,
+                geometryFactory,
                 3, 1, 4, 1, 4, 2, 3, 2
         );
 
-        final boolean exists = this.repository.isExistByGeometry(givenGeometry);
+        final boolean exists = repository.isExistByGeometry(givenGeometry);
         assertTrue(exists);
     }
 
@@ -70,11 +70,11 @@ public final class CityRepositoryTest extends AbstractContextTest {
     @Sql("classpath:sql/cities/insert-cities.sql")
     public void cityShouldNotExistByGeometry() {
         final Geometry givenGeometry = createPolygon(
-                this.geometryFactory,
+                geometryFactory,
                 1, 2, 3, 4, 5, 6
         );
 
-        final boolean exists = this.repository.isExistByGeometry(givenGeometry);
+        final boolean exists = repository.isExistByGeometry(givenGeometry);
         assertFalse(exists);
     }
 }
