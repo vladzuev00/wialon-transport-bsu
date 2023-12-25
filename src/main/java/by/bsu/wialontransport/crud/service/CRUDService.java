e1f4d5c8176cfad6851e0cc3dfefb7f98dec08bd
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-25 23:26:02 +0300

    refactor DataServiceTest

--- src/main/java/by/bsu/wialontransport/crud/service/CRUDService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/CRUDService.java
@@ -3,6 +3,7 @@ package by.bsu.wialontransport.crud.service;
 import by.bsu.wialontransport.crud.dto.Dto;
 import by.bsu.wialontransport.crud.entity.Entity;
 import by.bsu.wialontransport.crud.mapper.Mapper;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,74 +35,78 @@ public abstract class CRUDService<
 
     @Transactional(readOnly = true)
     public Optional<DTO> findById(final ID id) {
-        return this.findUnique(repository -> repository.findById(id));
+        return findUnique(repository -> repository.findById(id));
     }
 
     @Transactional(readOnly = true)
     public List<DTO> findByIds(final Iterable<ID> ids) {
-        return mapToList(this.repository.findAllById(ids), this.mapper::mapToDto);
+        return mapToList(repository.findAllById(ids), mapper::mapToDto);
     }
 
     @Transactional(readOnly = true)
     public boolean isExist(final ID id) {
-        return this.repository.existsById(id);
+        return repository.existsById(id);
     }
 
     public DTO update(final DTO dto) {
-        this.checkIdDefined(dto);
-        final ENTITY initialEntity = this.mapper.mapToEntity(dto);
-        final ENTITY updatedEntity = this.repository.save(initialEntity);
-        return this.mapper.mapToDto(updatedEntity);
+        checkIdDefined(dto);
+        final ENTITY initialEntity = mapper.mapToEntity(dto);
+        final ENTITY updatedEntity = repository.save(initialEntity);
+        return mapper.mapToDto(updatedEntity);
     }
 
     public void delete(final ID id) {
-        this.checkIdDefined(id);
-        this.repository.deleteById(id);
+        checkIdDefined(id);
+        repository.deleteById(id);
     }
 
     public DTO save(final DTO dto) {
-        final ENTITY entityToBeSaved = this.mapToConfiguredEntity(dto);
-        final ENTITY savedEntity = this.repository.save(entityToBeSaved);
-        return this.mapper.mapToDto(savedEntity);
+        final ENTITY entityToBeSaved = mapToConfiguredEntity(dto);
+        final ENTITY savedEntity = repository.save(entityToBeSaved);
+        return mapper.mapToDto(savedEntity);
     }
 
     public List<DTO> saveAll(final Collection<DTO> dtos) {
-        final List<ENTITY> entitiesToBeSaved = this.mapToConfiguredEntities(dtos);
-        final List<ENTITY> savedEntities = this.repository.saveAll(entitiesToBeSaved);
-        return mapToList(savedEntities, this.mapper::mapToDto);
+        final List<ENTITY> entitiesToBeSaved = mapToConfiguredEntities(dtos);
+        final List<ENTITY> savedEntities = repository.saveAll(entitiesToBeSaved);
+        return mapToList(savedEntities, mapper::mapToDto);
     }
 
     protected abstract void configureBeforeSave(final ENTITY entity);
 
     protected final Optional<DTO> findUnique(final Function<REPOSITORY, Optional<ENTITY>> operation) {
-        return operation.apply(this.repository).map(this.mapper::mapToDto);
+        return operation.apply(repository).map(mapper::mapToDto);
     }
 
     protected final boolean findBoolean(final Predicate<REPOSITORY> operation) {
-        return operation.test(this.repository);
+        return operation.test(repository);
     }
 
     protected final int findInt(final ToIntFunction<REPOSITORY> operation) {
-        return operation.applyAsInt(this.repository);
+        return operation.applyAsInt(repository);
     }
 
     protected <D> D findEntityStreamAndCollect(final Function<REPOSITORY, Stream<ENTITY>> operation,
                                                final Collector<ENTITY, ?, D> collector) {
-        try (final Stream<ENTITY> stream = operation.apply(this.repository)) {
+        try (final Stream<ENTITY> stream = operation.apply(repository)) {
             return stream.collect(collector);
         }
     }
 
     protected Stream<DTO> findDtoStream(final Function<REPOSITORY, Stream<ENTITY>> operation) {
-        return operation.apply(this.repository).map(this.mapper::mapToDto);
+        return operation.apply(repository).map(mapper::mapToDto);
+    }
+
+    protected Page<DTO> findDtoPage(final Function<REPOSITORY, Page<ENTITY>> operation) {
+        return operation.apply(repository).map(mapper::mapToDto);
     }
 
     private void checkIdDefined(final DTO dto) {
-        this.checkIdDefined(dto.getId());
+        checkIdDefined(dto.getId());
     }
 
     private void checkIdDefined(final ID id) {
-        if (this.isIdNotDefined(id)) {
+        if (isIdNotDefined(id)) {
             throw new IllegalArgumentException("Entity should have defined id");
         }
     }
@@ -117,8 +122,8 @@ public abstract class CRUDService<
     }
 
     private ENTITY mapToConfiguredEntity(final DTO dto) {
-        final ENTITY entity = this.mapper.mapToEntity(dto);
-        this.configureBeforeSave(entity);
+        final ENTITY entity = mapper.mapToEntity(dto);
+        configureBeforeSave(entity);
         return entity;
     }
 
