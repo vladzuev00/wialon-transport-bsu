5100c3a4079d2ed1ccda89fb1932adecdf06278b
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2023-12-25 16:49:36 +0300

    refactor TrackerService

--- src/main/java/by/bsu/wialontransport/crud/service/TrackerService.java
+++ src/main/java/by/bsu/wialontransport/crud/service/TrackerService.java
@@ -5,15 +5,15 @@ import by.bsu.wialontransport.crud.dto.User;
 import by.bsu.wialontransport.crud.entity.TrackerEntity;
 import by.bsu.wialontransport.crud.mapper.TrackerMapper;
 import by.bsu.wialontransport.crud.repository.TrackerRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
+
+import static org.springframework.data.domain.Pageable.unpaged;
 
 @Service
 public class TrackerService extends CRUDEncryptingPasswordService<
@@ -32,35 +32,26 @@ public class TrackerService extends CRUDEncryptingPasswordService<
 
     @Transactional(readOnly = true)
     public Optional<Tracker> findByImei(final String imei) {
-        return super.findUnique(repository -> repository.findByImei(imei));
+        return findUnique(repository -> repository.findByImei(imei));
     }
 
     @Transactional(readOnly = true)
-    public Stream<Tracker> findByUser(final User user) {
-//        return super.findDtoStream(repository -> repository.findByUserId(user.getId(), unpaged()));
-        return null;
+    public Page<Tracker> findByUser(final User user) {
+        return findByUser(user, unpaged());
     }
 
     @Transactional(readOnly = true)
-    public Stream<Tracker> findByUser(final User user, final Pageable pageable) {
-//        return super.findDtoStream(repository -> repository.findByUserId(user.getId(), pageable));
-        return null;
-    }
-
-    @Transactional(readOnly = true)
-    public List<Tracker> findByUser(final User user, final Pageable pageable, final Comparator<Tracker> comparator) {
-        return this.findByUser(user, pageable)
-                .sorted(comparator)
-                .toList();
+    public Page<Tracker> findByUser(final User user, final Pageable pageable) {
+        return findDtoPage(repository -> repository.findByUserId(user.getId(), pageable));
     }
 
     @Transactional(readOnly = true)
     public Optional<Tracker> findByIdWithUser(final Long id) {
-        return super.findUnique(repository -> repository.findByIdWithUser(id));
+        return findUnique(repository -> repository.findByIdWithUser(id));
     }
 
     @Transactional(readOnly = true)
     public Optional<Tracker> findByPhoneNumber(final String phoneNumber) {
-        return super.findUnique(repository -> repository.findByPhoneNumber(phoneNumber));
+        return findUnique(repository -> repository.findByPhoneNumber(phoneNumber));
     }
 }
