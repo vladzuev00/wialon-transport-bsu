ad213398a0eb93fb815a26edcbdf1f72e0165163
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2023-12-25 15:57:06 +0300

    refactor SearchingCitiesProcessMapperTest

--- src/test/java/by/bsu/wialontransport/crud/mapper/SearchingCitiesProcessMapperTest.java
+++ src/test/java/by/bsu/wialontransport/crud/mapper/SearchingCitiesProcessMapperTest.java
@@ -25,17 +25,17 @@ public final class SearchingCitiesProcessMapperTest extends AbstractContextTest 
     public void entityShouldBeMappedToDto() {
         final SearchingCitiesProcessEntity givenEntity = SearchingCitiesProcessEntity.builder()
                 .id(255L)
-                .bounds(createPolygon(this.geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
+                .bounds(createPolygon(geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
                 .searchStep(0.5)
                 .totalPoints(1000)
                 .handledPoints(100)
                 .status(HANDLING)
                 .build();
 
-        final SearchingCitiesProcess actual = this.mapper.mapToDto(givenEntity);
+        final SearchingCitiesProcess actual = mapper.mapToDto(givenEntity);
         final SearchingCitiesProcess expected = SearchingCitiesProcess.builder()
                 .id(255L)
-                .bounds(createPolygon(this.geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
+                .bounds(createPolygon(geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
                 .searchStep(0.5)
                 .totalPoints(1000)
                 .handledPoints(100)
@@ -48,17 +48,17 @@ public final class SearchingCitiesProcessMapperTest extends AbstractContextTest 
     public void dtoShouldBeMappedToEntity() {
         final SearchingCitiesProcess givenDto = SearchingCitiesProcess.builder()
                 .id(255L)
-                .bounds(createPolygon(this.geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
+                .bounds(createPolygon(geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
                 .searchStep(0.5)
                 .totalPoints(1000)
                 .handledPoints(100)
                 .status(HANDLING)
                 .build();
 
-        final SearchingCitiesProcessEntity actual = this.mapper.mapToEntity(givenDto);
+        final SearchingCitiesProcessEntity actual = mapper.mapToEntity(givenDto);
         final SearchingCitiesProcessEntity expected = SearchingCitiesProcessEntity.builder()
                 .id(255L)
-                .bounds(createPolygon(this.geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
+                .bounds(createPolygon(geometryFactory, 1, 1, 1, 4, 4, 4, 4, 1))
                 .searchStep(0.5)
                 .totalPoints(1000)
                 .handledPoints(100)
