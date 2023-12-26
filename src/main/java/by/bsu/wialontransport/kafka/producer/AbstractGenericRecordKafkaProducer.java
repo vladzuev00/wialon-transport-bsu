3962e860c1fb8e0056f246e5099cca2ed7955661
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-27 02:35:43 +0300

    refactor AbstractGenericRecordKafkaProducer

--- src/main/java/by/bsu/wialontransport/kafka/producer/AbstractGenericRecordKafkaProducer.java
+++ src/main/java/by/bsu/wialontransport/kafka/producer/AbstractGenericRecordKafkaProducer.java
@@ -1,6 +1,5 @@
 package by.bsu.wialontransport.kafka.producer;
 
-import by.bsu.wialontransport.kafka.producer.exception.KafkaProducerMappingToGenericRecordException;
 import by.bsu.wialontransport.kafka.transportable.Transportable;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumReader;
@@ -11,6 +10,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public abstract class AbstractGenericRecordKafkaProducer<K, T extends Transportable<K>, S>
         extends AbstractKafkaProducer<K, GenericRecord, T, S> {
@@ -26,16 +26,45 @@ public abstract class AbstractGenericRecordKafkaProducer<K, T extends Transporta
     @Override
     protected final GenericRecord mapToValue(final T mapped) {
         try (final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            final ReflectDatumWriter<T> datumWriter = new ReflectDatumWriter<>(this.schema);
-            final BinaryEncoder encoder = EncoderFactory.get()
-                    .binaryEncoder(outputStream, null);
+            final ReflectDatumWriter<T> datumWriter = new ReflectDatumWriter<>(schema);
+            final BinaryEncoder encoder = createEncoder(outputStream);
             datumWriter.write(mapped, encoder);
             encoder.flush();
-            final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(this.schema);
-            final BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
+            final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+            final BinaryDecoder decoder = createDecoder(outputStream);
             return datumReader.read(null, decoder);
         } catch (final IOException cause) {
             throw new KafkaProducerMappingToGenericRecordException(cause);
+        }
+    }
+
+    private static BinaryEncoder createEncoder(final OutputStream outputStream) {
+        return EncoderFactory.get().binaryEncoder(outputStream, null);
+    }
+
+    private static BinaryDecoder createDecoder(final ByteArrayOutputStream outputStream) {
+        return DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
+    }
+
+    static final class KafkaProducerMappingToGenericRecordException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public KafkaProducerMappingToGenericRecordException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public KafkaProducerMappingToGenericRecordException(final String description) {
+            super(description);
+        }
+
+        public KafkaProducerMappingToGenericRecordException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public KafkaProducerMappingToGenericRecordException(final String description, final Exception cause) {
+            super(description, cause);
         }
     }
 }
