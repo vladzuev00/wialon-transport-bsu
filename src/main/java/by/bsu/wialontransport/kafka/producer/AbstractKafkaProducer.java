859f9ebd49876a238ab4512fbd970bed6ccd6202
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-27 02:31:46 +0300

    refactor AbstractKafkaProducer

--- src/main/java/by/bsu/wialontransport/kafka/producer/AbstractKafkaProducer.java
+++ src/main/java/by/bsu/wialontransport/kafka/producer/AbstractKafkaProducer.java
@@ -1,29 +1,20 @@
 package by.bsu.wialontransport.kafka.producer;
 
 import by.bsu.wialontransport.kafka.transportable.Transportable;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 
-/**
- * @param <K>      - topic's key
- * @param <V>      - topic's value
- * @param <T>      - value which was mapped to value before it will be sent to kafka's topic
- * @param <S> - value which was mapped to TRANSPORTABLE before it will be sent to kafka's topic
- */
+@RequiredArgsConstructor
 public abstract class AbstractKafkaProducer<K, V, T extends Transportable<K>, S> {
     private final KafkaTemplate<K, V> kafkaTemplate;
     private final String topicName;
 
-    public AbstractKafkaProducer(final KafkaTemplate<K, V> kafkaTemplate, final String topicName) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.topicName = topicName;
-    }
-
     public final void send(final S source) {
-        final T transportable = this.mapToTransportable(source);
+        final T transportable = mapToTransportable(source);
         final K key = transportable.findTopicKey();
-        final V value = this.mapToValue(transportable);
-        this.sendValue(key, value);
+        final V value = mapToValue(transportable);
+        sendValue(key, value);
     }
 
     protected abstract T mapToTransportable(final S source);
@@ -31,7 +22,7 @@ public abstract class AbstractKafkaProducer<K, V, T extends Transportable<K>, S>
     protected abstract V mapToValue(final T mapped);
 
     private void sendValue(final K key, final V value) {
-        final ProducerRecord<K, V> producerRecord = new ProducerRecord<>(this.topicName, key, value);
-        this.kafkaTemplate.send(producerRecord);
+        final ProducerRecord<K, V> producerRecord = new ProducerRecord<>(topicName, key, value);
+        kafkaTemplate.send(producerRecord);
     }
 }
