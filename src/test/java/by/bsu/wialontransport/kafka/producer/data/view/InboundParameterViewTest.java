2872ac7754e28872324fb7cf7b93f23c8ef748d7
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-29 11:20:11 +0300

    add InboundParameterViewTest

---
+++ src/test/java/by/bsu/wialontransport/kafka/producer/data/view/InboundParameterViewTest.java
@@ -0,0 +1,54 @@
+package by.bsu.wialontransport.kafka.producer.data.view;
+
+import by.bsu.wialontransport.base.AbstractContextTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static by.bsu.wialontransport.crud.entity.ParameterEntity.Type.INTEGER;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
+public final class InboundParameterViewTest extends AbstractContextTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void viewShouldBeConvertedToJson()
+            throws Exception {
+        final InboundParameterView givenView = InboundParameterView.builder()
+                .name("parameter-name")
+                .type(INTEGER)
+                .value("1")
+                .build();
+
+        final String actual = objectMapper.writeValueAsString(givenView);
+        final String expected = """
+                {
+                  "name": "parameter-name",
+                  "type": "INTEGER",
+                  "value": "1"
+                }""";
+        assertEquals(expected, actual, true);
+    }
+
+    @Test
+    public void jsonShouldBeConvertedToView()
+            throws Exception {
+        final String givenJson = """
+                {
+                  "name": "parameter-name",
+                  "type": "INTEGER",
+                  "value": "1"
+                }""";
+
+        final InboundParameterView actual = objectMapper.readValue(givenJson, InboundParameterView.class);
+        final InboundParameterView expected = InboundParameterView.builder()
+                .name("parameter-name")
+                .type(INTEGER)
+                .value("1")
+                .build();
+        assertEquals(expected, actual);
+    }
+}
