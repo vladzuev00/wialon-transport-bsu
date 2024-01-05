29e3d55f19b4377ad6d46b3edd801a40e74a182f
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-05 17:50:13 +0300

    start refactor SearchingCitiesProcessControllerTest

--- src/test/java/by/bsu/wialontransport/controller/searchingcities/SearchingCitiesProcessControllerTest.java
+++ src/test/java/by/bsu/wialontransport/controller/searchingcities/SearchingCitiesProcessControllerTest.java
@@ -1,82 +1,94 @@
-//package by.bsu.wialontransport.controller.searchingcities;
-//
-//import by.bsu.wialontransport.base.AbstractContextTest;
-//import by.bsu.wialontransport.crud.dto.SearchingCitiesProcess;
-//import by.bsu.wialontransport.crud.entity.SearchingCitiesProcessEntity.Status;
-//import by.bsu.wialontransport.crud.service.SearchingCitiesProcessService;
-//import by.bsu.wialontransport.model.AreaCoordinate;
-//import by.bsu.wialontransport.model.Coordinate;
-//import by.bsu.wialontransport.service.searchingcities.StartingSearchingCitiesProcessService;
-//import org.junit.Test;
-//import org.locationtech.jts.geom.GeometryFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.http.HttpEntity;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.util.UriComponentsBuilder;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static by.bsu.wialontransport.crud.entity.SearchingCitiesProcessEntity.Status.HANDLING;
-//import static by.bsu.wialontransport.util.GeometryTestUtil.createPolygon;
-//import static java.util.Optional.empty;
-//import static org.junit.Assert.*;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-//import static org.springframework.http.HttpStatus.*;
-//import static org.springframework.http.MediaType.APPLICATION_JSON;
-//import static org.springframework.web.util.UriComponentsBuilder.fromUriString;
-//
-//@SpringBootTest(webEnvironment = RANDOM_PORT)
-//public final class SearchingCitiesProcessControllerTest extends AbstractContextTest {
-//    private static final String CONTROLLER_URL = "/searchCities";
-//
-//    private static final String PARAM_NAME_STATUS = "status";
-//    private static final String PARAM_NAME_PAGE_NUMBER = "pageNumber";
-//    private static final String PARAM_NAME_PAGE_SIZE = "pageSize";
-//
-//    @MockBean
-//    private SearchingCitiesProcessService mockedProcessService;
-//
-//    @MockBean
-//    private StartingSearchingCitiesProcessService mockedStartingProcessService;
-//
-//    @Autowired
-//    private GeometryFactory geometryFactory;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Test
-//    public void processShouldBeFoundById() {
-//        final Long givenId = 255L;
-//        final SearchingCitiesProcess givenProcess = SearchingCitiesProcess.builder()
-//                .id(givenId)
-//                .bounds(createPolygon(this.geometryFactory, 1, 1, 1, 2, 2, 2, 2, 1))
-//                .searchStep(0.5)
-//                .totalPoints(100)
-//                .handledPoints(0)
-//                .status(HANDLING)
-//                .build();
-//
-//        when(this.mockedProcessService.findById(givenId)).thenReturn(Optional.of(givenProcess));
-//
-//        final String url = createUrlToFindProcessById(givenId);
-//        final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(url, String.class);
-//
-//        assertSame(OK, responseEntity.getStatusCode());
-//
-//        final String actual = responseEntity.getBody();
-//        final String expected = "{\"id\":255,\"bounds\":{\"type\":\"Polygon\","
-//                + "\"coordinates\":[[[1.0,1.0],[1.0,2.0],[2.0,2.0],[2.0,1.0],[1.0,1.0]]]},"
-//                + "\"searchStep\":0.5,\"totalPoints\":100,\"handledPoints\":0,\"status\":\"HANDLING\"}";
-//        assertEquals(expected, actual);
-//    }
-//
+package by.bsu.wialontransport.controller.searchingcities;
+
+import by.bsu.wialontransport.base.AbstractContextTest;
+import by.bsu.wialontransport.controller.searchingcities.mapper.SearchingCitiesProcessControllerMapper;
+import by.bsu.wialontransport.controller.searchingcities.validator.StartSearchingCitiesRequestValidator;
+import by.bsu.wialontransport.crud.dto.SearchingCitiesProcess;
+import by.bsu.wialontransport.crud.entity.SearchingCitiesProcessEntity.Status;
+import by.bsu.wialontransport.crud.service.SearchingCitiesProcessService;
+import by.bsu.wialontransport.service.searchingcities.StartingSearchingCitiesProcessService;
+import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
+
+import static by.bsu.wialontransport.crud.entity.SearchingCitiesProcessEntity.Status.HANDLING;
+import static by.bsu.wialontransport.util.GeometryTestUtil.createPolygon;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.util.UriComponentsBuilder.fromUriString;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public final class SearchingCitiesProcessControllerTest extends AbstractContextTest {
+    private static final String CONTROLLER_URL = "/searchCities";
+
+    private static final String PARAM_NAME_STATUS = "status";
+    private static final String PARAM_NAME_PAGE_NUMBER = "pageNumber";
+    private static final String PARAM_NAME_PAGE_SIZE = "pageSize";
+
+    @MockBean
+    private StartSearchingCitiesRequestValidator mockedRequestValidator;
+
+    @MockBean
+    private SearchingCitiesProcessService mockedProcessService;
+
+    @MockBean
+    private SearchingCitiesProcessControllerMapper mockedMapper;
+
+    @MockBean
+    private StartingSearchingCitiesProcessService mockedStartingProcessService;
+
+    @Autowired
+    private GeometryFactory geometryFactory;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void processShouldBeFoundById() {
+        final Long givenId = 255L;
+        final Geometry givenBounds = createPolygon(
+                geometryFactory,
+                1, 1, 1, 2, 2, 2, 2, 1
+        );
+        final double givenSearchStep = 0.5;
+        final long givenTotalPoints = 100;
+        final long givenHandledPoints = 0;
+        final Status givenStatus = HANDLING;
+
+        final SearchingCitiesProcess givenProcess = SearchingCitiesProcess.builder()
+                .id(givenId)
+                .bounds(givenBounds)
+                .searchStep(givenSearchStep)
+                .totalPoints(givenTotalPoints)
+                .handledPoints(givenHandledPoints)
+                .status(givenStatus)
+                .build();
+        when(mockedProcessService.findById(eq(givenId))).thenReturn(Optional.of(givenProcess));
+
+        final String url = createUrlToFindProcessById(givenId);
+        final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(url, String.class);
+
+        assertSame(OK, responseEntity.getStatusCode());
+
+        final String actual = responseEntity.getBody();
+        final String expected = "{\"id\":255,\"bounds\":{\"type\":\"Polygon\","
+                + "\"coordinates\":[[[1.0,1.0],[1.0,2.0],[2.0,2.0],[2.0,1.0],[1.0,1.0]]]},"
+                + "\"searchStep\":0.5,\"totalPoints\":100,\"handledPoints\":0,\"status\":\"HANDLING\"}";
+        assertEquals(expected, actual);
+    }
+
 //    @Test
 //    public void processShouldNotBeFoundById() {
 //        final Long givenId = 255L;
@@ -352,37 +364,37 @@
 //        assertNotNull(actual);
 //        assertTrue(actual.matches(expectedRegex));
 //    }
-//
-//    private static String createUrlToFindProcessById(final Long id) {
-//        return CONTROLLER_URL + "/" + id;
-//    }
-//
-//    private static String createUrlToFindProcessesByStatus(final Status status,
-//                                                           final Integer pageNumber,
-//                                                           final Integer pageSize) {
-//        final String statusName = status != null ? status.name() : null;
-//        return createUrlToFindProcessesByStatus(statusName, pageNumber, pageSize);
-//    }
-//
-//    private static String createUrlToFindProcessesByStatus(final String statusName,
-//                                                           final Integer pageNumber,
-//                                                           final Integer pageSize) {
-//        final UriComponentsBuilder builder = fromUriString(CONTROLLER_URL);
-//        if (statusName != null) {
-//            builder.queryParam(PARAM_NAME_STATUS, statusName);
-//        }
-//        if (pageNumber != null) {
-//            builder.queryParam(PARAM_NAME_PAGE_NUMBER, pageNumber);
-//        }
-//        if (pageSize != null) {
-//            builder.queryParam(PARAM_NAME_PAGE_SIZE, pageSize);
-//        }
-//        return builder
-//                .build()
-//                .toUriString();
-//    }
-//
-//    private static String createUrlToStartProcess() {
-//        return CONTROLLER_URL;
-//    }
-//}
+
+    private static String createUrlToFindProcessById(final Long id) {
+        return CONTROLLER_URL + "/" + id;
+    }
+
+    private static String createUrlToFindProcessesByStatus(final Status status,
+                                                           final Integer pageNumber,
+                                                           final Integer pageSize) {
+        final String statusName = status != null ? status.name() : null;
+        return createUrlToFindProcessesByStatus(statusName, pageNumber, pageSize);
+    }
+
+    private static String createUrlToFindProcessesByStatus(final String statusName,
+                                                           final Integer pageNumber,
+                                                           final Integer pageSize) {
+        final UriComponentsBuilder builder = fromUriString(CONTROLLER_URL);
+        if (statusName != null) {
+            builder.queryParam(PARAM_NAME_STATUS, statusName);
+        }
+        if (pageNumber != null) {
+            builder.queryParam(PARAM_NAME_PAGE_NUMBER, pageNumber);
+        }
+        if (pageSize != null) {
+            builder.queryParam(PARAM_NAME_PAGE_SIZE, pageSize);
+        }
+        return builder
+                .build()
+                .toUriString();
+    }
+
+    private static String createUrlToStartProcess() {
+        return CONTROLLER_URL;
+    }
+}
