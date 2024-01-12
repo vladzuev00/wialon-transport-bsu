1d70d43e6d601157bd27b66863a8d79f99711d6a
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-12 10:32:07 +0300

    refactor NewWingProtocolHandler

--- src/main/java/by/bsu/wialontransport/protocol/newwing/handler/NewWingProtocolHandler.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/handler/NewWingProtocolHandler.java
@@ -3,16 +3,23 @@ package by.bsu.wialontransport.protocol.newwing.handler;
 import by.bsu.wialontransport.protocol.core.connectionmanager.ConnectionManager;
 import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
 import by.bsu.wialontransport.protocol.core.handler.ProtocolHandler;
-import by.bsu.wialontransport.protocol.core.handler.packages.PackageHandler;
+import by.bsu.wialontransport.protocol.newwing.handler.packages.NewWingDataPackageHandler;
+import by.bsu.wialontransport.protocol.newwing.handler.packages.NewWingEventCountPackageHandler;
+import by.bsu.wialontransport.protocol.newwing.handler.packages.NewWingLoginPackageHandler;
 
 import java.util.List;
 
 public final class NewWingProtocolHandler extends ProtocolHandler {
 
-    public NewWingProtocolHandler(final List<PackageHandler<?>> packageHandlers,
+    public NewWingProtocolHandler(final NewWingLoginPackageHandler loginPackageHandler,
+                                  final NewWingEventCountPackageHandler eventCountPackageHandler,
+                                  final NewWingDataPackageHandler dataPackageHandler,
                                   final ContextAttributeManager contextAttributeManager,
                                   final ConnectionManager connectionManager) {
-        super(packageHandlers, contextAttributeManager, connectionManager);
+        super(
+                List.of(loginPackageHandler, eventCountPackageHandler, dataPackageHandler),
+                contextAttributeManager,
+                connectionManager);
     }
 
 }
