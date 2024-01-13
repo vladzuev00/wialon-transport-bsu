511cd80bea186425d7e921d25c0e66c04a7b709d
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-13 17:36:12 +0300

    refactor ParameterEntity

--- src/main/java/by/bsu/wialontransport/crud/entity/ParameterEntity.java
+++ src/main/java/by/bsu/wialontransport/crud/entity/ParameterEntity.java
@@ -6,7 +6,6 @@ import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 
-import static java.util.Arrays.stream;
 import static javax.persistence.EnumType.STRING;
 import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.SEQUENCE;
@@ -47,23 +46,7 @@ public class ParameterEntity extends Entity<Long> {
     @ToString.Exclude
     private DataEntity data;
 
-    @RequiredArgsConstructor
     public enum Type {
-        INTEGER((byte) 1), DOUBLE((byte) 2), STRING((byte) 3);
-
-        @Getter
-        private final byte value;
-
-        //TODO: test
-        public static Type findByValue(final byte value) {
-            return stream(values())
-                    .filter(type -> type.value == value)
-                    .findFirst()
-                    .orElseThrow(
-                            () -> new IllegalArgumentException(
-                                    "There is no type with value '%d'".formatted(value)
-                            )
-                    );
-        }
+        INTEGER, DOUBLE, STRING
     }
 }
