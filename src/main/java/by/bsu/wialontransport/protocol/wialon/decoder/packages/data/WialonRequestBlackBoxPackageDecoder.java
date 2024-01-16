e457efbcbee858d3c7d480342e96edc9c0079355
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-16 13:23:03 +0300

    refactor WialonRequestBlackBoxPackageDecoder and WialonRequestBlackBoxPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/data/WialonRequestBlackBoxPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/data/WialonRequestBlackBoxPackageDecoder.java
@@ -1,28 +1,24 @@
 package by.bsu.wialontransport.protocol.wialon.decoder.packages.data;
 
-import by.bsu.wialontransport.crud.dto.Data;
 import by.bsu.wialontransport.protocol.wialon.decoder.packages.data.parser.WialonMessageParser;
 import by.bsu.wialontransport.protocol.wialon.model.WialonData;
 import by.bsu.wialontransport.protocol.wialon.wialonpackage.data.request.WialonRequestBlackBoxPackage;
 import by.bsu.wialontransport.protocol.wialon.wialonpackage.data.response.WialonResponseBlackBoxPackage;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import static by.bsu.wialontransport.protocol.wialon.wialonpackage.data.request.WialonRequestBlackBoxPackage.PREFIX;
-import static java.util.Arrays.stream;
 
 public final class WialonRequestBlackBoxPackageDecoder
         extends AbstractWialonRequestDataPackageDecoder<WialonRequestBlackBoxPackage, WialonResponseBlackBoxPackage> {
-    private static final String REGEX_SUB_MESSAGES_DELIMITER = "\\|";
 
-    public WialonRequestBlackBoxPackageDecoder(final WialonMessageParser wialonMessageParser) {
-        super(PREFIX, wialonMessageParser);
+    public WialonRequestBlackBoxPackageDecoder(final WialonMessageParser messageParser) {
+        super(PREFIX, messageParser);
     }
 
     @Override
-    protected WialonRequestBlackBoxPackage createPackage(List<WialonData> data) {
-        return null;
+    protected WialonRequestBlackBoxPackage createPackage(final List<WialonData> data) {
+        return new WialonRequestBlackBoxPackage(data);
     }
 
     @Override
