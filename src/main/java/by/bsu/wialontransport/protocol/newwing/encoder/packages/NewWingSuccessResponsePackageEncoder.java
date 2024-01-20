42861611a9fc20b2476ae23e46ddf4c9b0876227
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-21 01:24:36 +0300

    add NewWingSuccessResponsePackageEncoder

---
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/encoder/packages/NewWingSuccessResponsePackageEncoder.java
@@ -0,0 +1,11 @@
+package by.bsu.wialontransport.protocol.newwing.encoder.packages;
+
+import by.bsu.wialontransport.protocol.newwing.model.packages.response.NewWingSuccessResponsePackage;
+
+public final class NewWingSuccessResponsePackageEncoder extends ResponseNewWingPackageEncoder<NewWingSuccessResponsePackage> {
+
+    public NewWingSuccessResponsePackageEncoder() {
+        super(NewWingSuccessResponsePackage.class);
+    }
+
+}
