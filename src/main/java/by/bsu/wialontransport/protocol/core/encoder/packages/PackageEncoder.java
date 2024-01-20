f26ba2f40395d9befd3a5d4a52d568c0f5158295
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-20 14:29:25 +0300

    add PackageEncoder

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/encoder/packages/PackageEncoder.java
@@ -0,0 +1,20 @@
+package by.bsu.wialontransport.protocol.core.encoder.packages;
+
+import by.bsu.wialontransport.protocol.core.model.packages.Package;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class PackageEncoder<PACKAGE extends Package> {
+    private final Class<PACKAGE> encodedPackageType;
+
+    public final boolean isAbleToEncode(final Package response) {
+        return encodedPackageType.isInstance(response);
+    }
+
+    public final String encode(final Package response) {
+        final PACKAGE castedResponse = encodedPackageType.cast(response);
+        return encodeInternal(castedResponse);
+    }
+
+    protected abstract String encodeInternal(final PACKAGE response);
+}
