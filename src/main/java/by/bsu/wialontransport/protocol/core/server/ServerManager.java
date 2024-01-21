c49bc4c963d33d4241698b1c99e081b565f726c7
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-21 04:02:49 +0300

    add ServerManager

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/server/ServerManager.java
@@ -0,0 +1,26 @@
+package by.bsu.wialontransport.protocol.core.server;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public final class ServerManager {
+    private final List<ProtocolServer> servers;
+
+    @PostConstruct
+    public void runServers() {
+        servers.stream()
+                .map(server -> new Thread(server::run))
+                .forEach(Thread::start);
+    }
+
+    @PreDestroy
+    public void stopServers() {
+        servers.forEach(ProtocolServer::stop);
+    }
+}
