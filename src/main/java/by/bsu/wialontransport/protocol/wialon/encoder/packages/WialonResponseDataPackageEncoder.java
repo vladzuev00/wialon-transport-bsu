6d8ae762cd8275e90c9308db9e89e08f79c3c65b
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-25 20:44:27 +0300

    refactor WialonResponseDataPackageEncoder

---
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/encoder/packages/WialonResponseDataPackageEncoder.java
@@ -0,0 +1,17 @@
+package by.bsu.wialontransport.protocol.wialon.encoder.packages;
+
+import by.bsu.wialontransport.protocol.wialon.wialonpackage.data.response.WialonResponseDataPackage;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class WialonResponseDataPackageEncoder extends WialonPackageEncoder<WialonResponseDataPackage> {
+
+    public WialonResponseDataPackageEncoder() {
+        super(WialonResponseDataPackage.class);
+    }
+
+    @Override
+    protected String encodeMessage(final WialonResponseDataPackage response) {
+        return Byte.toString(response.getStatus().getValue());
+    }
+}
