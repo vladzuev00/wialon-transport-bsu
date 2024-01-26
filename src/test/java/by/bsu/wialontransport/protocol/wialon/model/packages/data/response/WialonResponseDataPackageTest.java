dc02318da252b36e9f2b4cc1b7409309bd44c9f2
Vlad Zuev <vladzuev.00@mail.ru>
2024-01-26 22:01:50 +0300

    add WialonResponseDataPackageTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/model/packages/data/response/WialonResponseDataPackageTest.java
@@ -0,0 +1,18 @@
+package by.bsu.wialontransport.protocol.wialon.model.packages.data.response;
+
+import org.junit.Test;
+
+import static by.bsu.wialontransport.protocol.wialon.model.packages.data.response.WialonResponseDataPackage.PREFIX;
+import static by.bsu.wialontransport.protocol.wialon.model.packages.data.response.WialonResponseDataPackage.Status.PACKAGE_FIX_SUCCESS;
+import static org.junit.Assert.assertSame;
+
+public final class WialonResponseDataPackageTest {
+
+    @Test
+    public void prefixShouldBeFound() {
+        final WialonResponseDataPackage givenPackage = new WialonResponseDataPackage(PACKAGE_FIX_SUCCESS);
+
+        final String actual = givenPackage.findPrefix();
+        assertSame(PREFIX, actual);
+    }
+}
