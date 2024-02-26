8478f27b7093174b7216d8978e0f35c51eb65bc3
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 19:03:49 +0300

    refactor

--- src/main/java/by/bsu/wialontransport/config/property/DataDefaultPropertyConfig.java
+++ src/main/java/by/bsu/wialontransport/config/property/DataDefaultPropertyConfig.java
@@ -2,21 +2,46 @@ package by.bsu.wialontransport.config.property;
 
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Setter
+import javax.validation.constraints.*;
+
+@RequiredArgsConstructor
 @Getter
 @EqualsAndHashCode
 @ToString
+@Builder
 @ConfigurationProperties("tracker-server.data-default-property")
+@ConstructorBinding
 public class DataDefaultPropertyConfig {
 
+    @NotNull
+    private final Integer course;
 
-    private  Integer course;
-    private  Integer altitude;
-    private  Double speed;
-    private  Integer amountOfSatellites;
-    private  Double hdop;
-    private  Integer inputs;
-    private  Integer outputs;
-    private  String driverKeyCode;
+    @NotNull
+    private final Integer altitude;
+
+    @NotNull
+    @PositiveOrZero
+    private final Double speed;
+
+    @NotNull
+    @Positive
+    private final Integer amountOfSatellites;
+
+    @NotNull
+    @Min(1)
+    @Max(10)
+    private final Double hdop;
+
+    @NotNull
+    @PositiveOrZero
+    private final Integer inputs;
+
+    @NotNull
+    @PositiveOrZero
+    private final Integer outputs;
+
+    @NotBlank
+    private final String driverKeyCode;
 }
---
+++ src/test/java/by/bsu/wialontransport/config/property/DataDefaultPropertyConfigTest.java
@@ -0,0 +1,319 @@
+package by.bsu.wialontransport.config.property;
+
+import by.bsu.wialontransport.base.AbstractSpringBootTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public final class DataDefaultPropertyConfigTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private DataDefaultPropertyConfig config;
+
+    @Autowired
+    private Validator validator;
+
+    @Test
+    public void configShouldBeCreated() {
+        final DataDefaultPropertyConfig expected = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+        assertEquals(expected, config);
+    }
+
+    @Test
+    public void configShouldBeValid() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfCourseIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfAltitudeIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfSpeedIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfSpeedIsNegative() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(-0.0000001)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть больше или равно 0", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfAmountOfSatellitesIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfAmountOfSatellitesIsNotPositive() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(0)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть больше 0", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfHdopIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfHdopIsLessThanMinimalAllowable() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(0.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть не меньше 1", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfHdopIsBiggerThanMaximalAllowable() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(11.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть не больше 10", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfInputsIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfInputsIsNegative() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(-1)
+                .outputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть больше или равно 0", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfOutputsIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно равняться null", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfOutputsIsNegative() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(-1)
+                .driverKeyCode("not defined")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("должно быть больше или равно 0", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfDriverKeyCodeIsNull() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно быть пустым", findFirstMessage(violations));
+    }
+
+    @Test
+    public void configShouldNotBeValidBecauseOfDriverKeyCodeIsBlank() {
+        final DataDefaultPropertyConfig givenConfig = DataDefaultPropertyConfig.builder()
+                .course(0)
+                .altitude(0)
+                .speed(0.)
+                .amountOfSatellites(3)
+                .hdop(5.)
+                .inputs(0)
+                .outputs(0)
+                .driverKeyCode("      \n\t")
+                .build();
+
+        final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations = validator.validate(givenConfig);
+        assertEquals(1, violations.size());
+        assertEquals("не должно быть пустым", findFirstMessage(violations));
+    }
+
+    private static String findFirstMessage(final Set<ConstraintViolation<DataDefaultPropertyConfig>> violations) {
+        return violations.iterator().next().getMessage();
+    }
+}
