a4a40f757269e42f371353cd6be0b0ac2d1ba277
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-08 10:45:26 +0300

    refactor PasswordValidator and add PasswordValidatorTest

--- src/main/java/by/bsu/wialontransport/validation/validator/byregex/PasswordValidator.java
+++ src/main/java/by/bsu/wialontransport/validation/validator/byregex/PasswordValidator.java
@@ -2,7 +2,6 @@ package by.bsu.wialontransport.validation.validator.byregex;
 
 import by.bsu.wialontransport.validation.annotation.Password;
 
-//TODO: test
 public final class PasswordValidator extends ValidatorByRegex<Password> {
     private static final String REGEX = "[a-zA-Z0-9-]{3,128}";
 
