5ada0b13fcc07e30887d581077f60eb72ca9e4fb
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-11 22:36:16 +0300

    add UserView

---
+++ src/main/java/by/bsu/wialontransport/controller/model/user/UserView.java
@@ -0,0 +1,18 @@
+package by.bsu.wialontransport.controller.model.user;
+
+import by.bsu.wialontransport.crud.dto.User;
+import by.bsu.wialontransport.crud.entity.UserEntity.Role;
+import lombok.Value;
+
+@Value
+public class UserView {
+    Long id;
+    String email;
+    Role role;
+
+    public UserView(final User user) {
+        id = user.getId();
+        email = user.getEmail();
+        role = user.getRole();
+    }
+}
