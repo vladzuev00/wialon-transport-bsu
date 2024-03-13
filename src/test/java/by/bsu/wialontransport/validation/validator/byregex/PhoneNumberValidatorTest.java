9e9b5696e8b3df1b41aaca999ad4582ed22e1a6a
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-13 10:53:48 +0300

    add PhoneNumberValidatorTest

---
+++ src/test/java/by/bsu/wialontransport/validation/validator/byregex/PhoneNumberValidatorTest.java
@@ -0,0 +1,39 @@
+package by.bsu.wialontransport.validation.validator.byregex;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.validation.ConstraintValidatorContext;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+public final class PhoneNumberValidatorTest {
+    private final PhoneNumberValidator validator = new PhoneNumberValidator();
+
+    @ParameterizedTest
+    @MethodSource("providePhoneNumberAndExpectedValidationResult")
+    public void phoneNumberShouldBeValidated(final String givenPhoneNumber, final boolean expected) {
+        final ConstraintValidatorContext givenContext = mock(ConstraintValidatorContext.class);
+
+        final boolean actual = validator.isValid(givenPhoneNumber, givenContext);
+        assertEquals(expected, actual);
+
+        verifyNoInteractions(givenContext);
+    }
+
+    private static Stream<Arguments> providePhoneNumberAndExpectedValidationResult() {
+        return Stream.of(
+                Arguments.of("447336934", true),
+                Arguments.of("44733693", false),
+                Arguments.of("4473369344", false),
+                Arguments.of("4473a6934", false),
+                Arguments.of("     ", false),
+                Arguments.of("", false),
+                Arguments.of(null, false)
+        );
+    }
+}
