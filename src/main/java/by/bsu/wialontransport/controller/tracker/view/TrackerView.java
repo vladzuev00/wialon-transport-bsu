09d93ae40e9748165282f332f8c55c8c6e0095a3
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-17 13:37:08 +0300

    refactor TrackerView

--- src/main/java/by/bsu/wialontransport/controller/tracker/view/TrackerView.java
+++ src/main/java/by/bsu/wialontransport/controller/tracker/view/TrackerView.java
@@ -1,6 +1,5 @@
 package by.bsu.wialontransport.controller.tracker.view;
 
-import by.bsu.wialontransport.crud.dto.Tracker;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
@@ -10,10 +9,4 @@ public class TrackerView {
     Long id;
     String imei;
     String phoneNumber;
-
-    public TrackerView(final Tracker tracker) {
-        id = tracker.getId();
-        imei = tracker.getImei();
-        phoneNumber = tracker.getPhoneNumber();
-    }
 }
