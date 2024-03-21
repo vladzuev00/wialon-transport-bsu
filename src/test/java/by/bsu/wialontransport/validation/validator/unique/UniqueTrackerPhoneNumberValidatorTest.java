70934aae62feb234da0c013acf6e446034744228
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-21 18:52:37 +0300

    add UniqueTrackerPhoneNumberValidatorTest

---
+++ src/test/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerPhoneNumberValidatorTest.java
@@ -0,0 +1,36 @@
+package by.bsu.wialontransport.validation.validator.unique;
+
+import by.bsu.wialontransport.crud.service.TrackerService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class UniqueTrackerPhoneNumberValidatorTest {
+    private final UniqueTrackerPhoneNumberValidator validator = new UniqueTrackerPhoneNumberValidator(null);
+
+    @Test
+    public void phoneNumberShouldExist() {
+        final String givenPhoneNumber = "447336934";
+        final TrackerService givenService = mock(TrackerService.class);
+
+        when(givenService.isExistByPhoneNumber(same(givenPhoneNumber))).thenReturn(true);
+
+        final boolean exists = validator.isExist(givenService, givenPhoneNumber);
+        assertTrue(exists);
+    }
+
+    @Test
+    public void phoneNumberShouldNotExist() {
+        final String givenPhoneNumber = "447336934";
+        final TrackerService givenService = mock(TrackerService.class);
+
+        when(givenService.isExistByPhoneNumber(same(givenPhoneNumber))).thenReturn(false);
+
+        final boolean exists = validator.isExist(givenService, givenPhoneNumber);
+        assertFalse(exists);
+    }
+}
