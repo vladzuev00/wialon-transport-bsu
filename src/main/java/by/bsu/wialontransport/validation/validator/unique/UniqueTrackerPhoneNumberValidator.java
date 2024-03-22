02164e935c4ed8ff6ff374ce89145c2aea212fb2
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-23 00:43:11 +0300

    refactor UniqueTrackerPhoneNumberValidator

--- src/main/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerPhoneNumberValidator.java
+++ src/main/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerPhoneNumberValidator.java
@@ -1,18 +1,28 @@
 package by.bsu.wialontransport.validation.validator.unique;
 
+import by.bsu.wialontransport.controller.tracker.view.TrackerView;
+import by.bsu.wialontransport.crud.dto.Tracker;
 import by.bsu.wialontransport.crud.service.TrackerService;
 import by.bsu.wialontransport.validation.annotation.UniqueTrackerPhoneNumber;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
-public final class UniqueTrackerPhoneNumberValidator extends UniquePropertyValidator<UniqueTrackerPhoneNumber, String, TrackerService> {
+public final class UniqueTrackerPhoneNumberValidator extends UniquePropertyValidator<UniqueTrackerPhoneNumber, Tracker, TrackerView, String, TrackerService> {
 
     public UniqueTrackerPhoneNumberValidator(final TrackerService service) {
         super(service);
     }
 
+
     @Override
-    protected boolean isExist(final TrackerService service, final String value) {
-        return service.isExistByPhoneNumber(value);
+    protected String getProperty(final TrackerView view) {
+        return view.getPhoneNumber();
+    }
+
+    @Override
+    protected Optional<Tracker> findByProperty(final String phoneNumber, final TrackerService service) {
+        return service.findByPhoneNumber(phoneNumber);
     }
 }
