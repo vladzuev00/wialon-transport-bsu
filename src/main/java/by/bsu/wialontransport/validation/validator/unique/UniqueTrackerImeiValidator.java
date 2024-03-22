c4898dd725830c11a7d96a8a138264b115fed457
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-23 00:40:50 +0300

    refactor UniqueTrackerImeiValidator

--- src/main/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerImeiValidator.java
+++ src/main/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerImeiValidator.java
@@ -1,13 +1,27 @@
 package by.bsu.wialontransport.validation.validator.unique;
 
+import by.bsu.wialontransport.controller.tracker.view.TrackerView;
+import by.bsu.wialontransport.crud.dto.Tracker;
 import by.bsu.wialontransport.crud.service.TrackerService;
 import by.bsu.wialontransport.validation.annotation.UniqueTrackerImei;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
-public final class UniqueTrackerImeiValidator extends UniquePropertyValidator<UniqueTrackerImei, Long,  String, TrackerService> {
+public final class UniqueTrackerImeiValidator extends UniquePropertyValidator<UniqueTrackerImei, Tracker, TrackerView, String, TrackerService> {
 
     public UniqueTrackerImeiValidator(final TrackerService service) {
         super(service);
+    }
+
+    @Override
+    protected String getProperty(final TrackerView view) {
+        return view.getImei();
+    }
+
+    @Override
+    protected Optional<Tracker> findByProperty(final String imei, final TrackerService service) {
+        return service.findByImei(imei);
     }
 }
