f038dce8bb5bebf5056c2fcef152ba38b9228511
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-23 12:23:24 +0300

    refactor UniqueTrackerImeiValidatorTest

--- src/test/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerImeiValidatorTest.java
+++ src/test/java/by/bsu/wialontransport/validation/validator/unique/UniqueTrackerImeiValidatorTest.java
@@ -1,9 +1,14 @@
 package by.bsu.wialontransport.validation.validator.unique;
 
+import by.bsu.wialontransport.controller.tracker.view.TrackerView;
+import by.bsu.wialontransport.crud.dto.Tracker;
 import by.bsu.wialontransport.crud.service.TrackerService;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
@@ -13,26 +18,50 @@ public final class UniqueTrackerImeiValidatorTest {
     private final UniqueTrackerImeiValidator validator = new UniqueTrackerImeiValidator(null);
 
     @Test
-    public void imeiShouldExist() {
-//        final String givenImei = "11112222333344445555";
-//        final TrackerService givenService = mock(TrackerService.class);
-//
-//        when(givenService.isExistByImei(same(givenImei))).thenReturn(true);
-//
-//        final boolean exists = validator.isExist(givenService, givenImei);
-//        assertTrue(exists);
-        throw new RuntimeException();
+    public void propertyShouldBeGot() {
+        final String givenImei = "11112222333344445555";
+        final TrackerView givenView = createView(givenImei);
+
+        final String actual = validator.getProperty(givenView);
+        assertSame(givenImei, actual);
     }
 
     @Test
-    public void imeiShouldNotExist() {
-//        final String givenImei = "11112222333344445555";
-//        final TrackerService givenService = mock(TrackerService.class);
-//
-//        when(givenService.isExistByImei(same(givenImei))).thenReturn(false);
-//
-//        final boolean exists = validator.isExist(givenService, givenImei);
-//        assertFalse(exists);
-        throw new RuntimeException();
+    public void trackerShouldBeFoundByImei() {
+        final String givenImei = "11112222333344445555";
+        final TrackerService givenService = mock(TrackerService.class);
+
+        final Tracker givenTracker = createTracker(255L);
+        when(givenService.findByImei(same(givenImei))).thenReturn(Optional.of(givenTracker));
+
+        final Optional<Tracker> optionalActual = validator.findByProperty(givenImei, givenService);
+        assertTrue(optionalActual.isPresent());
+        final Tracker actual = optionalActual.get();
+        assertSame(givenTracker, actual);
+    }
+
+    @Test
+    public void trackerShouldNotBeFoundByImei() {
+        final String givenImei = "11112222333344445555";
+        final TrackerService givenService = mock(TrackerService.class);
+
+        when(givenService.findByImei(same(givenImei))).thenReturn(empty());
+
+        final Optional<Tracker> optionalActual = validator.findByProperty(givenImei, givenService);
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static TrackerView createView(final String imei) {
+        final TrackerView givenView = mock(TrackerView.class);
+        when(givenView.getImei()).thenReturn(imei);
+        return givenView;
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static Tracker createTracker(final Long id) {
+        return Tracker.builder()
+                .id(id)
+                .build();
     }
 }
