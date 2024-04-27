6dcf70aa169c7dc0dcbad8378a4b88ed48441280
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-04-27 15:42:00 +0300

    refactor ContainerInitializer

--- src/test/java/by/bsu/wialontransport/base/containerinitializer/ContainerInitializer.java
+++ src/test/java/by/bsu/wialontransport/base/containerinitializer/ContainerInitializer.java
@@ -1,31 +1,64 @@
 package by.bsu.wialontransport.base.containerinitializer;
 
-import lombok.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
+import org.testcontainers.lifecycle.Startable;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Optional;
 
-import static java.util.stream.Collectors.toMap;
+import static org.testcontainers.utility.DockerImageName.parse;
 
-public abstract class ContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+@RequiredArgsConstructor
+public abstract class ContainerInitializer<C extends Startable> implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     @Override
-    public final void initialize(final ConfigurableApplicationContext context) {
-        TestPropertyValues.of(getPropertyValuesByKeys()).applyTo(context.getEnvironment());
+    public final void initialize(@SuppressWarnings("NullableProblems") final ConfigurableApplicationContext context) {
+        final C container = startContainer(context);
+        overrideAppProperties(container, context);
     }
 
-    protected abstract Stream<TestProperty> getProperties();
+    protected abstract String getImageName();
 
-    private Map<String, String> getPropertyValuesByKeys() {
-        return getProperties().collect(toMap(TestProperty::getKey, TestProperty::getValue));
+    protected abstract Optional<String> getOtherImageName();
+
+    protected abstract C createContainer(final DockerImageName imageName);
+
+    protected abstract void configure(final C container);
+
+    protected abstract Map<String, String> getPropertiesByKeys(final C container);
+
+    private C startContainer(final ConfigurableApplicationContext context) {
+        final DockerImageName imageName = parseImageName();
+        final C container = createContainer(imageName);
+        try {
+            configure(container);
+            container.start();
+            closeOnContextClosed(container, context);
+            return container;
+        } catch (final Throwable exception) {
+            container.close();
+            throw exception;
+        }
     }
 
-    @Value
-    protected static class TestProperty {
-        String key;
-        String value;
+    private DockerImageName parseImageName() {
+        final DockerImageName imageName = parse(getImageName());
+        return getOtherImageName()
+                .map(imageName::asCompatibleSubstituteFor)
+                .orElse(imageName);
+    }
+
+    private void closeOnContextClosed(final C container, final ConfigurableApplicationContext context) {
+        context.addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> container.close());
+    }
+
+    private void overrideAppProperties(final C container, final ConfigurableApplicationContext context) {
+        TestPropertyValues.of(getPropertiesByKeys(container)).applyTo(context.getEnvironment());
     }
 }
