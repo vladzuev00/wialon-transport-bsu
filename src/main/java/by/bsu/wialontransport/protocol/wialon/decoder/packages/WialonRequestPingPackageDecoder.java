c3914135a41df37e24ec739813822f7546e59c71
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-05-04 18:58:20 +0300

    refactor WialonRequestPingPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonRequestPingPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonRequestPingPackageDecoder.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import static by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage.PREFIX;
 
 @Component
-public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder<WialonRequestPingPackage> {
+public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder {
 
     public WialonRequestPingPackageDecoder() {
         super(PREFIX);
