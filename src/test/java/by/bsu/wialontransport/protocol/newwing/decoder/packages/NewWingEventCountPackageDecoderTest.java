86d0b03faf6cd93bb31ac4bbe2c4e14096f5e4e5
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-05-05 15:31:50 +0300

    refactor NewWingEventCountPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/NewWingEventCountPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/NewWingEventCountPackageDecoderTest.java
@@ -1,28 +1,33 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.newwing.model.packages.request.builder.NewWingEventCountPackageBuilder;
-//import io.netty.buffer.ByteBuf;
-//import org.junit.Test;
-//
-//import static org.mockito.Mockito.*;
-//
-//public final class NewWingEventCountPackageDecoderTest {
-//    private final NewWingEventCountPackageDecoder decoder = new NewWingEventCountPackageDecoder();
-//
-//    @Test
-//    public void bufferShouldBeDecodedUntilChecksum() {
-//        final ByteBuf givenBuffer = mock(ByteBuf.class);
-//        final NewWingEventCountPackageBuilder givenPackageBuilder = mock(NewWingEventCountPackageBuilder.class);
-//
-//        final short givenEventCount = 20;
-//        final short givenFrameEventCount = 5;
-//        when(givenBuffer.readShortLE())
-//                .thenReturn(givenEventCount)
-//                .thenReturn(givenFrameEventCount);
-//
-//        decoder.decodeUntilChecksum(givenBuffer, givenPackageBuilder);
-//
-//        verify(givenPackageBuilder, times(1)).setEventCount(eq(givenEventCount));
-//        verify(givenPackageBuilder, times(1)).setFrameEventCount(eq(givenFrameEventCount));
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder.packages;
+
+import by.bsu.wialontransport.protocol.newwing.decoder.packages.NewWingPackageDecoder.RequestFactory;
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingEventCountPackage;
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
+import io.netty.buffer.ByteBuf;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class NewWingEventCountPackageDecoderTest {
+    private final NewWingEventCountPackageDecoder decoder = new NewWingEventCountPackageDecoder();
+
+    @Test
+    public void bufferShouldBeDecodedUntilChecksum() {
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
+
+        final short givenEventCount = 20;
+        final short givenFrameEventCount = 5;
+        when(givenBuffer.readShortLE())
+                .thenReturn(givenEventCount)
+                .thenReturn(givenFrameEventCount);
+
+        final RequestFactory requestFactory = decoder.decodeUntilChecksum(givenBuffer);
+
+        final int givenChecksum = 53444546;
+        final NewWingRequestPackage actual = requestFactory.create(givenChecksum);
+        final var expected = new NewWingEventCountPackage(givenChecksum, givenEventCount, givenFrameEventCount);
+        assertEquals(expected, actual);
+    }
+}
