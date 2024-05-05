34c4ee3c949e90a7b56642863d2e32b41a59eff9
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-05-05 16:54:28 +0300

    refactor WialonPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonPackageDecoderTest.java
@@ -1,49 +1,44 @@
-//package by.bsu.wialontransport.protocol.wialon.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.wialon.model.packages.WialonPackage;
-//import lombok.Getter;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertSame;
-//import static org.mockito.Mockito.mock;
-//
-//public final class WialonPackageDecoderTest {
-//
-//    @Test
-//    public void sourceShouldBeDecoded() {
-//        final String givenPackagePrefix = "#TEST#";
-//        final WialonPackage givenDecodedPackage = mock(WialonPackage.class);
-//        final TestWialonPackageDecoder givenDecoder = new TestWialonPackageDecoder(
-//                givenPackagePrefix,
-//                givenDecodedPackage
-//        );
-//
-//        final String givenSource = "#TEST#message\r\n";
-//
-//        final WialonPackage actual = givenDecoder.decode(givenSource);
-//        assertSame(givenDecodedPackage, actual);
-//
-//        final String expectedCapturedMessage = "message";
-//        final String actualCapturedMessage = givenDecoder.getCapturedMessage();
-//        assertEquals(expectedCapturedMessage, actualCapturedMessage);
-//    }
-//
-//    private static final class TestWialonPackageDecoder extends WialonPackageDecoder<WialonPackage> {
-//        private final WialonPackage decodedPackage;
-//
-//        @Getter
-//        private String capturedMessage;
-//
-//        public TestWialonPackageDecoder(final String packagePrefix, final WialonPackage decodedPackage) {
-//            super(packagePrefix);
-//            this.decodedPackage = decodedPackage;
-//        }
-//
-//        @Override
-//        protected WialonPackage decodeMessage(final String message) {
-//            capturedMessage = message;
-//            return decodedPackage;
-//        }
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.decoder.packages;
+
+import by.bsu.wialontransport.protocol.wialon.model.packages.WialonPackage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class WialonPackageDecoderTest {
+
+    @Test
+    public void sourceShouldBeDecoded() {
+        final String givenPrefix = "#TEST#";
+        final WialonPackage givenDecodedPackage = mock(WialonPackage.class);
+        final TestWialonPackageDecoder givenDecoder = new TestWialonPackageDecoder(givenPrefix, givenDecodedPackage);
+
+        final String givenSource = "#TEST#message\r\n";
+
+        final WialonPackage actual = givenDecoder.decode(givenSource);
+        assertSame(givenDecodedPackage, actual);
+
+        final String actualCapturedMessage = givenDecoder.capturedMessage;
+        final String expectedCapturedMessage = "message";
+        assertEquals(expectedCapturedMessage, actualCapturedMessage);
+    }
+
+    private static final class TestWialonPackageDecoder extends WialonPackageDecoder {
+        private final WialonPackage decodedPackage;
+
+        private String capturedMessage;
+
+        public TestWialonPackageDecoder(final String packagePrefix, final WialonPackage decodedPackage) {
+            super(packagePrefix);
+            this.decodedPackage = decodedPackage;
+        }
+
+        @Override
+        protected WialonPackage decodeMessage(final String message) {
+            capturedMessage = message;
+            return decodedPackage;
+        }
+    }
+}
