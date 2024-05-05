a8a5f5e8bb8a01afe16040acd094a75ff5f09365
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-05-05 16:55:49 +0300

    refactor WialonRequestLoginPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonRequestLoginPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/packages/WialonRequestLoginPackageDecoderTest.java
@@ -13,10 +13,7 @@ public final class WialonRequestLoginPackageDecoderTest {
         final String givenMessage = "11111222223333344444;1111";
 
         final WialonRequestLoginPackage actual = decoder.decodeMessage(givenMessage);
-        final WialonRequestLoginPackage expected = new WialonRequestLoginPackage(
-                "11111222223333344444",
-                "1111"
-        );
+        final WialonRequestLoginPackage expected = new WialonRequestLoginPackage("11111222223333344444", "1111");
         assertEquals(expected, actual);
     }
 }
