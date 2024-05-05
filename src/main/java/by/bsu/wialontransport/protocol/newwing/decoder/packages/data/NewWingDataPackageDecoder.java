9db5d7bbff791ee73c7199436106cfe8024b7628
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-05-05 14:43:11 +0300

    refactor NewWingDataPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataPackageDecoder.java
@@ -3,39 +3,28 @@ package by.bsu.wialontransport.protocol.newwing.decoder.packages.data;
 import by.bsu.wialontransport.protocol.newwing.decoder.packages.NewWingPackageDecoder;
 import by.bsu.wialontransport.protocol.newwing.model.NewWingData;
 import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingDataPackage;
-import by.bsu.wialontransport.protocol.newwing.model.packages.request.builder.NewWingDataPackageBuilder;
 import io.netty.buffer.ByteBuf;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import static by.bsu.wialontransport.util.CollectionUtil.convertToList;
+import static org.apache.commons.collections4.IteratorUtils.toList;
 
 @Component
-public final class NewWingDataPackageDecoder
-        extends NewWingPackageDecoder<NewWingDataPackage, NewWingDataPackageBuilder> {
-    private static final String PACKAGE_PREFIX = "GPRSSI";
+public final class NewWingDataPackageDecoder extends NewWingPackageDecoder {
+    private static final String PREFIX = "GPRSSI";
 
     private final NewWingDataIteratorFactory dataIteratorFactory;
 
     public NewWingDataPackageDecoder(final NewWingDataIteratorFactory dataIteratorFactory) {
-        super(PACKAGE_PREFIX);
+        super(PREFIX);
         this.dataIteratorFactory = dataIteratorFactory;
     }
 
     @Override
-    protected NewWingDataPackageBuilder createPackageBuilder() {
-        return new NewWingDataPackageBuilder();
-    }
-
-    @Override
-    protected void decodeUntilChecksum(final ByteBuf buffer, final NewWingDataPackageBuilder packageBuilder) {
-        decodeData(buffer, packageBuilder);
-    }
-
-    private void decodeData(final ByteBuf buffer, final NewWingDataPackageBuilder packageBuilder) {
+    protected RequestFactory decodeUntilChecksum(final ByteBuf buffer) {
         final NewWingDataIterator iterator = dataIteratorFactory.create(buffer);
-        final List<NewWingData> data = convertToList(iterator);
-        packageBuilder.setData(data);
+        final List<NewWingData> data = toList(iterator);
+        return checksum -> new NewWingDataPackage(checksum, data);
     }
 }
