72abfa3810fc1e6b48b5db818b4489c2cbf62716
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-14 17:28:10 +0300

    refactor NewWingDataPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/data/NewWingDataPackageDecoderTest.java
@@ -3,8 +3,8 @@ package by.bsu.wialontransport.protocol.newwing.decoder.packages.data;
 import by.bsu.wialontransport.protocol.newwing.model.NewWingData;
 import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingDataPackage;
 import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
-import by.bsu.wialontransport.util.CollectionUtil;
 import io.netty.buffer.ByteBuf;
+import org.apache.commons.collections4.IteratorUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,14 +15,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.lang.reflect.Method;
 import java.util.List;
 
-import static by.bsu.wialontransport.util.CollectionUtil.collectToList;
+import static org.apache.commons.collections4.IteratorUtils.toList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class NewWingDataPackageDecoderTest {
-    private static final String REQUEST_FACTORY_CREATE_METHOD_NAME = "create";
+    private static final String PACKAGE_FACTORY_CREATE_METHOD_NAME = "create";
 
     @Mock
     private NewWingDataIteratorFactory mockedDataIteratorFactory;
@@ -37,7 +37,7 @@ public final class NewWingDataPackageDecoderTest {
     @Test
     public void bufferShouldBeDecodedUntilChecksum()
             throws Exception {
-        try (final MockedStatic<CollectionUtil> mockedCollectionUtil = mockStatic(CollectionUtil.class)) {
+        try (final MockedStatic<IteratorUtils> mockedIteratorUtil = mockStatic(IteratorUtils.class)) {
             final ByteBuf givenBuffer = mock(ByteBuf.class);
 
             final NewWingDataIterator givenIterator = mock(NewWingDataIterator.class);
@@ -48,12 +48,12 @@ public final class NewWingDataPackageDecoderTest {
                     createData(23, 11, 3),
                     createData(23, 11, 4)
             );
-            mockedCollectionUtil.when(() -> collectToList(same(givenIterator))).thenReturn(givenData);
+            mockedIteratorUtil.when(() -> toList(same(givenIterator))).thenReturn(givenData);
 
-            final Object requestFactory = decoder.decodeUntilChecksum(givenBuffer);
+            final Object packageFactory = decoder.decodeUntilChecksum(givenBuffer);
 
             final int givenChecksum = 53444546;
-            final NewWingRequestPackage actual = createRequest(requestFactory, givenChecksum);
+            final NewWingRequestPackage actual = createPackage(packageFactory, givenChecksum);
             final NewWingRequestPackage expected = new NewWingDataPackage(givenChecksum, givenData);
             assertEquals(expected, actual);
         }
@@ -69,9 +69,9 @@ public final class NewWingDataPackageDecoderTest {
     }
 
     @SuppressWarnings("SameParameterValue")
-    private NewWingRequestPackage createRequest(final Object factory, final int checksum)
+    private NewWingRequestPackage createPackage(final Object factory, final int checksum)
             throws Exception {
-        final Method method = factory.getClass().getMethod(REQUEST_FACTORY_CREATE_METHOD_NAME, int.class);
+        final Method method = factory.getClass().getMethod(PACKAGE_FACTORY_CREATE_METHOD_NAME, int.class);
         return (NewWingRequestPackage) method.invoke(factory, checksum);
     }
 }
