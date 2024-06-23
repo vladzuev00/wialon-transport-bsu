ad88f2d2fb45cdcb3c89e87c161c801de044d7bd
Vlad Zuev <vladzuev.00@mail.ru>
2024-06-23 12:59:40 +0300

    refactor PackageDecoderByFixedPrefix

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageDecoderByFixedPrefix.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PackageDecoderByFixedPrefix.java
@@ -5,11 +5,11 @@ import lombok.RequiredArgsConstructor;
 import java.util.Objects;
 
 @RequiredArgsConstructor
-public abstract class FixedPrefixPackageDecoder<PREFIX> implements PackageDecoder<PREFIX> {
+public abstract class PackageDecoderByFixedPrefix<PREFIX> extends PackageDecoderByPrefix<PREFIX> {
     private final PREFIX prefix;
 
     @Override
-    public final boolean isAbleToDecode(final PREFIX prefix) {
+    protected final boolean isAbleToDecode(final PREFIX prefix) {
         return Objects.equals(prefix, this.prefix);
     }
 }
