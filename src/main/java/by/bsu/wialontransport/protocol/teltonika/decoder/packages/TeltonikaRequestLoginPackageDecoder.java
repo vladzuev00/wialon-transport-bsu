7aa7b6628f4e7db9966e12161c597145020d2fec
Vlad Zuev <vladzuev.00@mail.ru>
2024-06-23 17:16:05 +0300

    refactor TeltonikaRequestLoginPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/teltonika/decoder/packages/TeltonikaRequestLoginPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/teltonika/decoder/packages/TeltonikaRequestLoginPackageDecoder.java
@@ -1,16 +1,34 @@
 package by.bsu.wialontransport.protocol.teltonika.decoder.packages;
 
-import by.bsu.wialontransport.protocol.core.decoder.packages.PackageBufferDecoder;
-import by.bsu.wialontransport.protocol.core.model.packages.Package;
+import by.bsu.wialontransport.protocol.core.decoder.packages.PackageDecoderByPrefix;
+import by.bsu.wialontransport.protocol.teltonika.model.packages.login.TeltonikaRequestLoginPackage;
 import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
 
-public class TeltonikaRequestLoginPackageDecoder extends PackageBufferDecoder<Short> {
-    public TeltonikaRequestLoginPackageDecoder(Short aShort) {
-        super(aShort);
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+@Component
+public final class TeltonikaRequestLoginPackageDecoder extends PackageDecoderByPrefix<Short> {
+    private static final int IMEI_START_BYTE_INDEX = 2;
+
+    @Override
+    public TeltonikaRequestLoginPackage decode(final ByteBuf buffer) {
+        final String imei = readImei(buffer);
+        return new TeltonikaRequestLoginPackage(imei);
     }
 
     @Override
-    public Package decode(ByteBuf byteBuf) {
-        return null;
+    protected Short getPrefix(final ByteBuf buffer) {
+        return buffer.getShort(0);
+    }
+
+    @Override
+    protected boolean isAbleToDecode(final Short prefix) {
+        return prefix.compareTo((short) 0) > 0;
+    }
+
+    private String readImei(final ByteBuf buffer) {
+        final short length = buffer.readShort();
+        return buffer.toString(IMEI_START_BYTE_INDEX, length, US_ASCII);
     }
 }
