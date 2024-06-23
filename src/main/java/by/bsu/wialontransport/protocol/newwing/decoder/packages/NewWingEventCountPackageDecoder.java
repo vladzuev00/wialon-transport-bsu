6a531988dbd270c71964686c16732cbabc6767d8
Vlad Zuev <vladzuev.00@mail.ru>
2024-06-23 18:51:31 +0300

    refactor NewWingEventCountPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/NewWingEventCountPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/packages/NewWingEventCountPackageDecoder.java
@@ -1,21 +1,21 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingEventCountPackage;
-//import io.netty.buffer.ByteBuf;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public final class NewWingEventCountPackageDecoder extends NewWingPackageDecoder {
-//    private static final String PREFIX = "GPRSGI";
-//
-//    public NewWingEventCountPackageDecoder() {
-//        super(PREFIX);
-//    }
-//
-//    @Override
-//    protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
-//        final short eventCount = buffer.readShortLE();
-//        final short frameEventCount = buffer.readShortLE();
-//        return checksum -> new NewWingEventCountPackage(checksum, eventCount, frameEventCount);
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder.packages;
+
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingEventCountPackage;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class NewWingEventCountPackageDecoder extends NewWingPackageDecoder {
+    private static final String PREFIX = "GPRSGI";
+
+    public NewWingEventCountPackageDecoder() {
+        super(PREFIX);
+    }
+
+    @Override
+    protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
+        final short eventCount = buffer.readShortLE();
+        final short frameEventCount = buffer.readShortLE();
+        return checksum -> new NewWingEventCountPackage(checksum, eventCount, frameEventCount);
+    }
+}
