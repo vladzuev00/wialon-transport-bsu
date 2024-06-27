e3832ecb1262e72778c9e894db720ff962a2ea5b
Vlad Zuev <vladzuev.00@mail.ru>
2024-06-27 23:31:09 +0300

    refactor PrefixiedPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PrefixiedPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/PrefixiedPackageDecoder.java
@@ -1,8 +1,9 @@
 package by.bsu.wialontransport.protocol.core.decoder.packages;
 
+import by.bsu.wialontransport.protocol.core.model.packages.Package;
 import io.netty.buffer.ByteBuf;
 
-public abstract class PrefixiedPackageDecoder<PREFIX> implements PackageDecoder {
+public abstract class PrefixiedPackageDecoder<PACKAGE extends Package, PREFIX> implements PackageDecoder<PACKAGE> {
 
     @Override
     public final boolean isAbleToDecode(final ByteBuf buffer) {
