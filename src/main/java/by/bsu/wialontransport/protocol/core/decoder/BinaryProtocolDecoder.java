7e8c377220be14366786cec348c9856cb3148628
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-03 16:50:07 +0300

    add BinaryProtocolDecoder

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/BinaryProtocolDecoder.java
@@ -0,0 +1,18 @@
+package by.bsu.wialontransport.protocol.core.decoder;
+
+import by.bsu.wialontransport.protocol.core.decoder.packages.PackageDecoder;
+import io.netty.buffer.ByteBuf;
+
+import java.util.List;
+
+public final class BinaryProtocolDecoder extends ProtocolDecoder<ByteBuf> {
+
+    public BinaryProtocolDecoder(final List<PackageDecoder<ByteBuf>> packageDecoders) {
+        super(packageDecoders);
+    }
+
+    @Override
+    protected ByteBuf createSource(final ByteBuf buffer) {
+        return buffer;
+    }
+}
