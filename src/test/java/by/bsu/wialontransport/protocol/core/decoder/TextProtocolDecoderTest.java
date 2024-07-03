826b1d89c97a82fc41533b896b0cfd88dafc826c
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-03 17:27:35 +0300

    add TextProtocolDecoderTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/core/decoder/TextProtocolDecoderTest.java
@@ -0,0 +1,21 @@
+package by.bsu.wialontransport.protocol.core.decoder;
+
+import io.netty.buffer.ByteBuf;
+import org.junit.Test;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+
+public final class TextProtocolDecoderTest {
+    private final TextProtocolDecoder decoder = new TextProtocolDecoder(emptyList());
+
+    @Test
+    public void sourceShouldBeCreated() {
+        final ByteBuf givenBuffer = wrappedBuffer(new byte[]{35, 80, 82, 69, 70, 73, 88, 35, 36, 37, 38});
+
+        final String actual = decoder.createSource(givenBuffer);
+        final String expected = "#PREFIX#$%&";
+        assertEquals(expected, actual);
+    }
+}
