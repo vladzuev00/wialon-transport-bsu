79920af0d82e1133764b59381e3310fb810245c3
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-03 22:39:22 +0300

    refactor FixStringPrefixedBinaryPackageDecoder

--- src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/FixStringPrefixedBinaryPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/decoder/packages/FixStringPrefixedBinaryPackageDecoder.java
@@ -4,14 +4,19 @@ import io.netty.buffer.ByteBuf;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
-public abstract class FixStringPrefixedBinaryPackageDecoder extends FixStringPrefixedPackageDecoder<ByteBuf> {
+public abstract class FixStringPrefixedBinaryPackageDecoder extends FixPrefixedBinaryPackageDecoder<String> {
 
     public FixStringPrefixedBinaryPackageDecoder(final String prefix) {
         super(prefix);
     }
 
     @Override
-    protected final String readPrefix(final ByteBuf buffer, final int length) {
-        return buffer.getCharSequence(0, length, US_ASCII).toString();
+    protected final void skipPrefix(final ByteBuf buffer) {
+        buffer.skipBytes(getPrefix().length());
+    }
+
+    @Override
+    protected final String readPrefix(final ByteBuf buffer) {
+        return buffer.getCharSequence(0, getPrefix().length(), US_ASCII).toString();
     }
 }
