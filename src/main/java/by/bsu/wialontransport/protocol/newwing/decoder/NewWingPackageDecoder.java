0e22795ea7f25263e939574cb78ddc67f3514743
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-06 11:17:20 +0300

    refactor NewWingPackageDecoder and NewWingPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingPackageDecoder.java
@@ -1,31 +1,26 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder;
-//
-//import by.bsu.wialontransport.protocol.core.decoder.packages.FixStringPrefixedBinaryPackageDecoder;
-//import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
-//import io.netty.buffer.ByteBuf;
-//
-//public abstract class NewWingPackageDecoder extends FixStringPrefixedBinaryPackageDecoder {
-//
-//    public NewWingPackageDecoder(final String prefix) {
-//        super(prefix);
-//    }
-//
-//    @Override
-//    public final NewWingRequestPackage decode(final ByteBuf buffer) {
-////        skipPrefix(buffer);
-//        final PackageFactory factory = decodeUntilChecksum(buffer);
-//        final int checksum = buffer.readIntLE();
-//        return factory.create(checksum);
-//    }
-//
-//    protected abstract PackageFactory decodeUntilChecksum(final ByteBuf buffer);
-//
-//    private void getPrefixByteCount(final ByteBuf buffer) {
-//        buffer.skipBytes(getPrefix().length());
-//    }
-//
-//    @FunctionalInterface
-//    protected interface PackageFactory {
-//        NewWingRequestPackage create(final int checksum);
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder;
+
+import by.bsu.wialontransport.protocol.core.decoder.packages.FixStringPrefixedBinaryPackageDecoder;
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
+import io.netty.buffer.ByteBuf;
+
+public abstract class NewWingPackageDecoder extends FixStringPrefixedBinaryPackageDecoder {
+
+    public NewWingPackageDecoder(final String prefix) {
+        super(prefix);
+    }
+
+    @Override
+    protected final NewWingRequestPackage decodeAfterSkipPrefix(final ByteBuf buffer) {
+        final PackageFactory factory = decodeUntilChecksum(buffer);
+        final int checksum = buffer.readIntLE();
+        return factory.create(checksum);
+    }
+
+    protected abstract PackageFactory decodeUntilChecksum(final ByteBuf buffer);
+
+    @FunctionalInterface
+    protected interface PackageFactory {
+        NewWingRequestPackage create(final int checksum);
+    }
+}
--- src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingPackageDecoderTest.java
@@ -1,47 +1,45 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.newwing.decoder.NewWingPackageDecoder;
-//import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
-//import io.netty.buffer.ByteBuf;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//public final class NewWingPackageDecoderTest {
-//    private static final String GIVEN_PREFIX = "#TEST#";
-//
-//    private final TestNewWingPackageDecoder decoder = new TestNewWingPackageDecoder(GIVEN_PREFIX);
-//
-//    @Test
-//    public void bufferShouldBeDecoded() {
-//        final ByteBuf givenBuffer = mock(ByteBuf.class);
-//
-//        final int givenChecksum = 12345;
-//        when(givenBuffer.readIntLE()).thenReturn(givenChecksum);
-//
-//        final NewWingRequestPackage actual = decoder.decode(givenBuffer);
-//        final NewWingRequestPackage expected = new TestPackage(givenChecksum);
-//        assertEquals(expected, actual);
-//    }
-//
-//    private static final class TestPackage extends NewWingRequestPackage {
-//
-//        public TestPackage(final int checksum) {
-//            super(checksum);
-//        }
-//    }
-//
-//    private static final class TestNewWingPackageDecoder extends NewWingPackageDecoder {
-//
-//        public TestNewWingPackageDecoder(final String prefix) {
-//            super(prefix);
-//        }
-//
-//        @Override
-//        protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
-//            return TestPackage::new;
-//        }
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder;
+
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingRequestPackage;
+import io.netty.buffer.ByteBuf;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class NewWingPackageDecoderTest {
+    private final TestNewWingPackageDecoder decoder = new TestNewWingPackageDecoder();
+
+    @Test
+    public void bufferShouldBeDecodedAfterSkipPrefix() {
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
+
+        final int givenChecksum = 12345;
+        when(givenBuffer.readIntLE()).thenReturn(givenChecksum);
+
+        final NewWingRequestPackage actual = decoder.decodeAfterSkipPrefix(givenBuffer);
+        final NewWingRequestPackage expected = new TestPackage(givenChecksum);
+        assertEquals(expected, actual);
+    }
+
+    private static final class TestPackage extends NewWingRequestPackage {
+
+        public TestPackage(final int checksum) {
+            super(checksum);
+        }
+    }
+
+    private static final class TestNewWingPackageDecoder extends NewWingPackageDecoder {
+        private static final String PREFIX = "PREFIX";
+
+        public TestNewWingPackageDecoder() {
+            super(PREFIX);
+        }
+
+        @Override
+        protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
+            return TestPackage::new;
+        }
+    }
+}
