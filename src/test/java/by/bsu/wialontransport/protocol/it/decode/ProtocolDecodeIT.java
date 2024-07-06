e35bf372e754885d5f46ed166cbdfd5c82ab291b
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-06 16:59:23 +0300

    refactor ProtocolDecodeIT

--- src/test/java/by/bsu/wialontransport/protocol/it/decode/ProtocolDecodeIT.java
+++ src/test/java/by/bsu/wialontransport/protocol/it/decode/ProtocolDecodeIT.java
@@ -2,10 +2,13 @@ package by.bsu.wialontransport.protocol.it.decode;
 
 import by.bsu.wialontransport.base.AbstractSpringBootTest;
 import by.bsu.wialontransport.protocol.core.decoder.ProtocolDecoder;
+import by.bsu.wialontransport.protocol.core.decoder.TextProtocolDecoder;
+import by.bsu.wialontransport.protocol.core.model.packages.Package;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.embedded.EmbeddedChannel;
 
 import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.junit.Assert.assertEquals;
 
 public abstract class ProtocolDecodeIT extends AbstractSpringBootTest {
@@ -16,5 +19,10 @@ public abstract class ProtocolDecodeIT extends AbstractSpringBootTest {
         givenChannel.writeInbound(givenBuffer);
         final Object actual = givenChannel.readInbound();
         assertEquals(expected, actual);
+    }
+
+    protected final void test(final TextProtocolDecoder givenDecoder, final String givenContent, final Package expected) {
+        final byte[] givenBytes = givenContent.getBytes(US_ASCII);
+        test(givenDecoder, givenBytes, expected);
     }
 }
