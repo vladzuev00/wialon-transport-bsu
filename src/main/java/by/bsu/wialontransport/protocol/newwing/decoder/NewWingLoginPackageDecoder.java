06e733f176620db3ce18f0470824d1c7c6c24f64
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-06 11:21:48 +0300

    refactor NewWingLoginPackageDecoder and NewWingLoginPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoder.java
@@ -1,25 +1,25 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder;
-//
-//import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingLoginPackage;
-//import io.netty.buffer.ByteBuf;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public final class NewWingLoginPackageDecoder extends NewWingPackageDecoder {
-//    private static final String PREFIX = "GPRSGC";
-//
-//    public NewWingLoginPackageDecoder() {
-//        super(PREFIX);
-//    }
-//
-//    @Override
-//    protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
-//        final String imei = readImei(buffer);
-//        return checksum -> new NewWingLoginPackage(checksum, imei);
-//    }
-//
-//    private String readImei(final ByteBuf buffer) {
-//        final short imei = buffer.readShortLE();
-//        return Short.toString(imei);
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder;
+
+import by.bsu.wialontransport.protocol.newwing.model.packages.request.NewWingLoginPackage;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class NewWingLoginPackageDecoder extends NewWingPackageDecoder {
+    private static final String PREFIX = "GPRSGC";
+
+    public NewWingLoginPackageDecoder() {
+        super(PREFIX);
+    }
+
+    @Override
+    protected PackageFactory decodeUntilChecksum(final ByteBuf buffer) {
+        final String imei = readImei(buffer);
+        return checksum -> new NewWingLoginPackage(checksum, imei);
+    }
+
+    private String readImei(final ByteBuf buffer) {
+        final short imei = buffer.readShortLE();
+        return Short.toString(imei);
+    }
+}
