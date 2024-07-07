634b3bded9cb6de52ee98e7a31545c3fd2bd9de3
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-07 11:06:31 +0300

    refactor FixStringPrefixedTextPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/core/decoder/packages/FixStringPrefixedTextPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/core/decoder/packages/FixStringPrefixedTextPackageDecoderTest.java
@@ -1,32 +1,41 @@
-//package by.bsu.wialontransport.protocol.core.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.core.model.packages.Package;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public final class FixStringPrefixedTextPackageDecoderTest {
-//    private final TestFixStringPrefixedTextPackageDecoder decoder = new TestFixStringPrefixedTextPackageDecoder();
-//
-//    @Test
-//    public void prefixShouldBeRead() {
-//        final String givenContent = "#PREFIX#message\r\n";
-//
-//        final String actual = decoder.readPrefix(givenContent);
-//        final String expected = "#PREFIX#";
-//        assertEquals(expected, actual);
-//    }
-//
-//    private static final class TestFixStringPrefixedTextPackageDecoder extends FixStringPrefixedTextPackageDecoder {
-//        private static final String PREFIX = "#PREFIX#";
-//
-//        public TestFixStringPrefixedTextPackageDecoder() {
-//            super(PREFIX);
-//        }
-//
-//        @Override
-//        public Package decode(final String content) {
-//            throw new UnsupportedOperationException();
-//        }
-//    }
-//}
+package by.bsu.wialontransport.protocol.core.decoder.packages;
+
+import by.bsu.wialontransport.protocol.core.model.packages.Package;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class FixStringPrefixedTextPackageDecoderTest {
+    private final TestFixStringPrefixedTextPackageDecoder decoder = new TestFixStringPrefixedTextPackageDecoder();
+
+    @Test
+    public void prefixShouldBeRead() {
+        final String givenContent = "#PREFIX#message\r\n";
+
+        final String actual = decoder.readPrefix(givenContent);
+        final String expected = "#PREFIX#";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prefixShouldBeRemoved() {
+        final String givenContent = "#PREFIX#message\r\n";
+
+        final String actual = decoder.removePrefix(givenContent);
+        final String expected = "message\r\n";
+        assertEquals(expected, actual);
+    }
+
+    private static final class TestFixStringPrefixedTextPackageDecoder extends FixStringPrefixedTextPackageDecoder {
+        private static final String PREFIX = "#PREFIX#";
+
+        public TestFixStringPrefixedTextPackageDecoder() {
+            super(PREFIX);
+        }
+
+        @Override
+        protected Package decodeWithoutPrefix(final String content) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
