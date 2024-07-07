ba88e3c850aefeeee6c015d77a0d8573cec4c61a
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-07 11:39:46 +0300

    refactor WialonRequestPingPackageDecoder and WialonRequestPingPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoder.java
@@ -1,19 +1,19 @@
-//package by.bsu.wialontransport.protocol.wialon.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage;
-//import org.springframework.stereotype.Component;
-//
-//import static by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage.PREFIX;
-//
-//@Component
-//public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder {
-//
-//    public WialonRequestPingPackageDecoder() {
-//        super(PREFIX);
-//    }
-//
-//    @Override
-//    protected WialonRequestPingPackage decodeMessage(final String message) {
-//        return new WialonRequestPingPackage();
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+import by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage;
+import org.springframework.stereotype.Component;
+
+import static by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage.PREFIX;
+
+@Component
+public final class WialonRequestPingPackageDecoder extends WialonPackageDecoder {
+
+    public WialonRequestPingPackageDecoder() {
+        super(PREFIX);
+    }
+
+    @Override
+    protected WialonRequestPingPackage decodeMessage(final String message) {
+        return new WialonRequestPingPackage();
+    }
+}
--- src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/decoder/WialonRequestPingPackageDecoderTest.java
@@ -1,18 +1,18 @@
-//package by.bsu.wialontransport.protocol.wialon.decoder.packages;
-//
-//import by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertNotNull;
-//
-//public final class WialonRequestPingPackageDecoderTest {
-//    private final WialonRequestPingPackageDecoder decoder = new WialonRequestPingPackageDecoder();
-//
-//    @Test
-//    public void messageShouldBeDecoded() {
-//        final String givenMessage = "";
-//
-//        final WialonRequestPingPackage actual = decoder.decodeMessage(givenMessage);
-//        assertNotNull(actual);
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.decoder;
+
+import by.bsu.wialontransport.protocol.wialon.model.packages.ping.WialonRequestPingPackage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public final class WialonRequestPingPackageDecoderTest {
+    private final WialonRequestPingPackageDecoder decoder = new WialonRequestPingPackageDecoder();
+
+    @Test
+    public void messageShouldBeDecoded() {
+        final String givenMessage = "";
+
+        final WialonRequestPingPackage actual = decoder.decodeMessage(givenMessage);
+        assertNotNull(actual);
+    }
+}
