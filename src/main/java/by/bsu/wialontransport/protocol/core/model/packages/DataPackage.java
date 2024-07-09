fd024fe4d89ce90c926074beda7468d5806816ad
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-10 01:08:55 +0300

    add DataPackage

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/model/packages/DataPackage.java
@@ -0,0 +1,9 @@
+package by.bsu.wialontransport.protocol.core.model.packages;
+
+import by.bsu.wialontransport.crud.dto.Data;
+
+import java.util.List;
+
+public interface DataPackage {
+    List<Data> getData();
+}
