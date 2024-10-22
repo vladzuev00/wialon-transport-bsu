0a7e3206d77f3004d91c792ae8c65fe73416f1b8
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-22 14:53:25 +0300

    refactor BinaryProtocolDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/core/decoder/BinaryProtocolDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/core/decoder/BinaryProtocolDecoderTest.java
@@ -1,10 +1,10 @@
 package by.bsu.wialontransport.protocol.core.decoder;
 
 import io.netty.buffer.ByteBuf;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static java.util.Collections.emptyList;
-import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.mock;
 
 public final class BinaryProtocolDecoderTest {
