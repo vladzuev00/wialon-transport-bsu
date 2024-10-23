ebabb5d5c00bc9b4c7c0bc9d65d53cdb5bd613d3
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-23 14:45:43 +0300

    add JT808HeartBeatPackageDecoder and JT808HeartBeatPackageDecoderTest

---
+++ src/main/java/by/bsu/wialontransport/protocol/jt808/decoder/JT808HeartBeatPackageDecoder.java
@@ -0,0 +1,19 @@
+package by.bsu.wialontransport.protocol.jt808.decoder;
+
+import by.bsu.wialontransport.protocol.jt808.model.JT808HeartBeatPackage;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class JT808HeartBeatPackageDecoder extends JT808PackageDecoder {
+    private static final byte[] REQUIRED_PREFIX = {126, 0, 2};
+
+    public JT808HeartBeatPackageDecoder() {
+        super(REQUIRED_PREFIX);
+    }
+
+    @Override
+    protected Object decodeInternal(final ByteBuf buffer, final String phoneNumber) {
+        return new JT808HeartBeatPackage();
+    }
+}
