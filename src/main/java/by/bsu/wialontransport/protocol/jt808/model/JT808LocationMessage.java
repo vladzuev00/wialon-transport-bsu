1b702cfa6f7cd317b50fc8407bc0c771915ec343
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-23 12:47:59 +0300

    refactor JT808LocationMessage

--- src/main/java/by/bsu/wialontransport/protocol/jt808/model/JT808LocationMessage.java
+++ src/main/java/by/bsu/wialontransport/protocol/jt808/model/JT808LocationMessage.java
@@ -1,10 +1,11 @@
 package by.bsu.wialontransport.protocol.jt808.model;
 
+import by.bsu.wialontransport.crud.dto.Location;
 import lombok.Value;
 
 import java.util.List;
 
 @Value
 public class JT808LocationMessage {
-    List<Location> location;
+    List<Location> locations;
 }
