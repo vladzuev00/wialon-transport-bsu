cfc474d923f11c7b3511ec28c18819ebfd3cc932
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-25 12:43:01 +0300

    refactor NewWingLoginPackageDecoder and NewWingLoginPackageDecoderTest

--- src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoder.java
+++ src/main/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoder.java
@@ -5,7 +5,6 @@ import by.bsu.wialontransport.protocol.newwing.model.request.NewWingLoginPackage
 import io.netty.buffer.ByteBuf;
 import org.springframework.stereotype.Component;
 
-//TODO: test
 @Component
 public final class NewWingLoginPackageDecoder extends PrefixedByStringBinaryPackageDecoder {
     private static final String REQUIRED_PREFIX = "GPRSGC";
--- src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingLoginPackageDecoderTest.java
@@ -1,31 +1,22 @@
-//package by.bsu.wialontransport.protocol.newwing.decoder;
-//
-//import by.bsu.wialontransport.protocol.newwing.model.request.NewWingLoginPackage;
-//import by.bsu.wialontransport.protocol.newwing.util.NewWingUtil;
-//import io.netty.buffer.ByteBuf;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.MockedStatic;
-//
-//import static by.bsu.wialontransport.protocol.newwing.util.NewWingUtil.decodeImei;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.same;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.mockStatic;
-//
-//public final class NewWingLoginPackageDecoderTest {
-//    private final NewWingLoginPackageDecoder decoder = new NewWingLoginPackageDecoder();
-//
-//    @Test
-//    public void bufferShouldBeDecodedInternally() {
-//        try (final MockedStatic<NewWingUtil> mockedUtil = mockStatic(NewWingUtil.class)) {
-//            final ByteBuf givenBuffer = mock(ByteBuf.class);
-//
-//            final short givenImei = "11112222333344445555";
-//            mockedUtil.when(() -> decodeImei(same(givenBuffer))).thenReturn(givenImei);
-//
-//            final NewWingLoginPackage actual = decoder.decodeInternal(givenBuffer);
-//            final NewWingLoginPackage expected = new NewWingLoginPackage(givenImei);
-//            assertEquals(expected, actual);
-//        }
-//    }
-//}
+package by.bsu.wialontransport.protocol.newwing.decoder;
+
+import by.bsu.wialontransport.protocol.newwing.model.request.NewWingLoginPackage;
+import io.netty.buffer.ByteBuf;
+import org.junit.jupiter.api.Test;
+
+import static io.netty.buffer.ByteBufUtil.decodeHexDump;
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class NewWingLoginPackageDecoderTest {
+    private final NewWingLoginPackageDecoder decoder = new NewWingLoginPackageDecoder();
+
+    @Test
+    public void bufferShouldBeDecodedInternally() {
+        final ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("c10b"));
+
+        final NewWingLoginPackage actual = decoder.decodeInternal(givenBuffer);
+        final NewWingLoginPackage expected = new NewWingLoginPackage((short) 3009);
+        assertEquals(expected, actual);
+    }
+}
