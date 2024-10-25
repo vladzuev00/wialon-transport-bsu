4676fce5508cf0048d02e7931916ca69aa1427a7
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-25 10:57:11 +0300

    refactor NewWingEventCountPackageDecoderTest

--- src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingEventCountPackageDecoderTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/newwing/decoder/NewWingEventCountPackageDecoderTest.java
@@ -3,18 +3,21 @@ package by.bsu.wialontransport.protocol.newwing.decoder;
 import by.bsu.wialontransport.protocol.newwing.model.request.NewWingEventCountPackage;
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 public final class NewWingEventCountPackageDecoderTest {
     private final NewWingEventCountPackageDecoder decoder = new NewWingEventCountPackageDecoder();
 
     @Test
     public void bufferShouldBeDecodedInternally() {
-        final ByteBuf givenBuffer = Mockito.mock(ByteBuf.class);
+        final ByteBuf givenBuffer = mock(ByteBuf.class);
 
         final NewWingEventCountPackage actual = decoder.decodeInternal(givenBuffer);
         assertNotNull(actual);
+
+        verifyNoInteractions(givenBuffer);
     }
 }
