673c312b2bfdebbd987c2fccac8b6477f0c4d7ec
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-28 17:47:01 +0300

    add WialonLocation

--- src/main/java/by/bsu/wialontransport/protocol/wialon/model/packages/WialonPackage.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/model/packages/WialonPackage.java
@@ -1,12 +1,11 @@
 package by.bsu.wialontransport.protocol.wialon.model.packages;
 
-import by.bsu.wialontransport.protocol.core.model.packages.Package;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @EqualsAndHashCode
 @ToString
-public abstract class WialonPackage implements Package {
+public abstract class WialonPackage {
     public static final String POSTFIX = "\r\n";
 
     public abstract String getPrefix();
