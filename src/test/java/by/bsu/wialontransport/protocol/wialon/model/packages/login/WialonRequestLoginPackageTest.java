0e83e166992e81e9e28019f3ca1b5eca89a90cdb
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-29 11:46:54 +0300

    refactor WialonRequestLoginPackageTest

--- src/test/java/by/bsu/wialontransport/protocol/wialon/model/packages/login/WialonRequestLoginPackageTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/model/packages/login/WialonRequestLoginPackageTest.java
@@ -8,11 +8,8 @@ import static org.junit.Assert.assertSame;
 public final class WialonRequestLoginPackageTest {
 
     @Test
-    public void prefixShouldBeFound() {
-        final WialonRequestLoginPackage givenPackage = new WialonRequestLoginPackage(
-                "11112222333344445555",
-                "111"
-        );
+    public void prefixShouldBeGot() {
+        final var givenPackage = new WialonRequestLoginPackage("11112222333344445555", "111");
 
         final String actual = givenPackage.getPrefix();
         assertSame(PREFIX, actual);
