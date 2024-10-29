fcd40b6745c3bd8dece88960e7ed971486a77c38
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-29 11:45:17 +0300

    refactor WialonResponsePingPackageTest

--- src/test/java/by/bsu/wialontransport/protocol/wialon/model/packages/ping/WialonResponsePingPackageTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/wialon/model/packages/ping/WialonResponsePingPackageTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertSame;
 public final class WialonResponsePingPackageTest {
 
     @Test
-    public void prefixShouldBeFound() {
+    public void prefixShouldBeGot() {
         final WialonResponsePingPackage givenPackage = new WialonResponsePingPackage();
 
         final String actual = givenPackage.getPrefix();
