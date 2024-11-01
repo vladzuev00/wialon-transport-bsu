18e7d1d65266695915ef925fd29738b1b46b5825
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-01 13:22:07 +0300

    add WialonRequestLocationPackageDecoder, WialonRequestSingleLocationPackageDecoder, WialonRequestBulkLocationPackageDecoder

---
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/decoder/location/WialonRequestSingleLocationPackageDecoder.java
@@ -0,0 +1,23 @@
+package by.bsu.wialontransport.protocol.wialon.decoder.location;
+
+import by.bsu.wialontransport.protocol.wialon.decoder.location.parser.WialonLocationParser;
+import by.bsu.wialontransport.protocol.wialon.model.WialonLocation;
+import by.bsu.wialontransport.protocol.wialon.model.packages.WialonPackage;
+import by.bsu.wialontransport.protocol.wialon.model.packages.location.request.WialonRequestSingleLocationPackage;
+import org.springframework.stereotype.Component;
+
+import static by.bsu.wialontransport.protocol.wialon.model.packages.location.request.WialonRequestSingleLocationPackage.PREFIX;
+
+@Component
+public final class WialonRequestSingleLocationPackageDecoder extends WialonRequestLocationPackageDecoder {
+
+    public WialonRequestSingleLocationPackageDecoder(final WialonLocationParser locationParser) {
+        super(PREFIX, locationParser);
+    }
+
+    @Override
+    protected WialonPackage decodeMessageInternal(final String message, final WialonLocationParser locationParser) {
+        final WialonLocation location = locationParser.parse(message);
+        return new WialonRequestSingleLocationPackage(location);
+    }
+}
