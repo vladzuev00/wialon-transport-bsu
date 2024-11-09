a42f7a3f550f9b182e1a8392f7ffdb5086377a72
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-09 18:03:43 +0300

    refactor ProtectedLoginPackageHandler, UnprotectedLoginPackageHandler

--- src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/login/ProtectedLoginPackageHandler.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/login/ProtectedLoginPackageHandler.java
@@ -1,42 +1,38 @@
-//package by.bsu.wialontransport.protocol.core.handler.packages.login;
-//
-//import by.bsu.wialontransport.crud.dto.Tracker;
-//import by.bsu.wialontransport.crud.service.DataService;
-//import by.bsu.wialontransport.crud.service.TrackerService;
-//import by.bsu.wialontransport.protocol.core.connectionmanager.ConnectionManager;
-//import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
-//import by.bsu.wialontransport.protocol.core.model.packages.Package;
-//import by.bsu.wialontransport.protocol.core.model.packages.login.ProtectedLoginPackage;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//import java.util.Optional;
-//
-//import static java.util.Optional.empty;
-//
-////TODO: refactor tests
-//public abstract class ProtectedLoginPackageHandler<PACKAGE extends ProtectedLoginPackage>
-//        extends LoginPackageHandler<PACKAGE> {
-//    private final BCryptPasswordEncoder passwordEncoder;
-//
-//    public ProtectedLoginPackageHandler(final Class<PACKAGE> handledPackageType,
-//                                        final ContextAttributeManager contextAttributeManager,
-//                                        final TrackerService trackerService,
-//                                        final ConnectionManager connectionManager,
-//                                        final DataService dataService,
-//                                        final BCryptPasswordEncoder passwordEncoder) {
-//        super(handledPackageType, contextAttributeManager, trackerService, connectionManager, dataService);
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    @Override
-//    protected final Optional<Package> loginCreatingResponseIfFailed(final Tracker tracker,
-//                                                                    final ProtectedLoginPackage request) {
-//        return isPasswordCorrect(tracker, request) ? empty() : Optional.of(createWrongPasswordResponse());
-//    }
-//
-//    protected abstract Package createWrongPasswordResponse();
-//
-//    private boolean isPasswordCorrect(final Tracker tracker, final ProtectedLoginPackage request) {
-//        return passwordEncoder.matches(request.getPassword(), tracker.getPassword());
-//    }
-//}
+package by.bsu.wialontransport.protocol.core.handler.packages.login;
+
+import by.bsu.wialontransport.crud.dto.Tracker;
+import by.bsu.wialontransport.crud.service.LocationService;
+import by.bsu.wialontransport.crud.service.TrackerService;
+import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
+import by.bsu.wialontransport.protocol.core.contextmanager.ChannelHandlerContextManager;
+import by.bsu.wialontransport.protocol.core.model.login.ProtectedLoginPackage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+
+public abstract class ProtectedLoginPackageHandler<REQUEST extends ProtectedLoginPackage> extends LoginPackageHandler<REQUEST> {
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public ProtectedLoginPackageHandler(final Class<REQUEST> requestType,
+                                        final ContextAttributeManager contextAttributeManager,
+                                        final TrackerService trackerService,
+                                        final ChannelHandlerContextManager contextManager,
+                                        final LocationService locationService,
+                                        final BCryptPasswordEncoder passwordEncoder) {
+        super(requestType, contextAttributeManager, trackerService, contextManager, locationService);
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected final Optional<Object> loginCreatingFailedResponse(final Tracker tracker, final REQUEST request) {
+        return isPasswordCorrect(tracker, request) ? empty() : Optional.of(createWrongPasswordResponse());
+    }
+
+    protected abstract Object createWrongPasswordResponse();
+
+    private boolean isPasswordCorrect(final Tracker tracker, final REQUEST request) {
+        return passwordEncoder.matches(request.getPassword(), tracker.getPassword());
+    }
+}
--- src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/login/UnprotectedLoginPackageHandler.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/login/UnprotectedLoginPackageHandler.java
@@ -1,29 +1,28 @@
-//package by.bsu.wialontransport.protocol.core.handler.packages.login;
-//
-//import by.bsu.wialontransport.crud.dto.Tracker;
-//import by.bsu.wialontransport.crud.service.DataService;
-//import by.bsu.wialontransport.crud.service.TrackerService;
-//import by.bsu.wialontransport.protocol.core.connectionmanager.ConnectionManager;
-//import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
-//import by.bsu.wialontransport.protocol.core.model.packages.Package;
-//import by.bsu.wialontransport.protocol.core.model.packages.login.LoginPackage;
-//
-//import java.util.Optional;
-//
-//import static java.util.Optional.empty;
-//
-////TODO: do the same as ProtectedLoginPackageHandler
-//public abstract class UnprotectedLoginPackageHandler extends LoginPackageHandler<LoginPackage> {
-//
-//    public UnprotectedLoginPackageHandler(final ContextAttributeManager contextAttributeManager,
-//                                          final TrackerService trackerService,
-//                                          final ConnectionManager connectionManager,
-//                                          final DataService dataService) {
-//        super(LoginPackage.class, contextAttributeManager, trackerService, connectionManager, dataService);
-//    }
-//
-//    @Override
-//    protected final Optional<Package> loginCreatingResponseIfFailed(final Tracker tracker, final LoginPackage request) {
-//        return empty();
-//    }
-//}
+package by.bsu.wialontransport.protocol.core.handler.packages.login;
+
+import by.bsu.wialontransport.crud.dto.Tracker;
+import by.bsu.wialontransport.crud.service.LocationService;
+import by.bsu.wialontransport.crud.service.TrackerService;
+import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
+import by.bsu.wialontransport.protocol.core.contextmanager.ChannelHandlerContextManager;
+import by.bsu.wialontransport.protocol.core.model.login.LoginPackage;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+
+public abstract class UnprotectedLoginPackageHandler<REQUEST extends LoginPackage> extends LoginPackageHandler<REQUEST> {
+
+    public UnprotectedLoginPackageHandler(final Class<REQUEST> requestType,
+                                          final ContextAttributeManager contextAttributeManager,
+                                          final TrackerService trackerService,
+                                          final ChannelHandlerContextManager contextManager,
+                                          final LocationService locationService) {
+        super(requestType, contextAttributeManager, trackerService, contextManager, locationService);
+    }
+
+    @Override
+    protected final Optional<Object> loginCreatingFailedResponse(final Tracker tracker, final REQUEST request) {
+        return empty();
+    }
+}
