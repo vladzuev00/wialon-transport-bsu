ff1dfa0365dd296aa647ee17e14cb967d4d5ff7d
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-11 00:45:24 +0300

    add LocationValidator

---
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/location/validator/LocationValidator.java
@@ -0,0 +1,17 @@
+package by.bsu.wialontransport.protocol.core.handler.packages.location.validator;
+
+import by.bsu.wialontransport.crud.dto.Location;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public final class LocationValidator {
+    private final List<LocationPropertyValidator<?>> propertyValidators;
+
+    public boolean isValid(final Location location) {
+        return propertyValidators.stream().allMatch(validator -> validator.isValid(location));
+    }
+}
