fb4fe07741c939114da79796cc85355b131201c7
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-10 15:15:33 +0300

    refactor ProtectedLoginPackageHandlerTest

--- src/test/java/by/bsu/wialontransport/protocol/core/handler/packages/login/ProtectedLoginPackageHandlerTest.java
+++ src/test/java/by/bsu/wialontransport/protocol/core/handler/packages/login/ProtectedLoginPackageHandlerTest.java
@@ -1,122 +1,91 @@
-//package by.bsu.wialontransport.protocol.core.handler.packages.login;
-//
-//import by.bsu.wialontransport.crud.dto.Tracker;
-//import by.bsu.wialontransport.protocol.core.model.packages.Package;
-//import by.bsu.wialontransport.protocol.core.model.packages.login.ProtectedLoginPackage;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//import java.util.Optional;
-//
-//import static org.junit.Assert.assertSame;
-//import static org.junit.Assert.assertTrue;
-//import static org.mockito.ArgumentMatchers.same;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public final class ProtectedLoginPackageHandlerTest {
-//    private static final Package GIVEN_WRONG_PASSWORD_RESPONSE = new Package() {
-//    };
-//
-//    @Mock
-//    private BCryptPasswordEncoder mockedPasswordEncoder;
-//
-//    private ProtectedLoginPackageHandler loginPackageHandler;
-//
-//    @Before
-//    public void initializeLoginPackageHandler() {
-//        loginPackageHandler = new TestProtectedLoginPackageHandler(
-//                mockedPasswordEncoder,
-//                GIVEN_WRONG_PASSWORD_RESPONSE
-//        );
-//    }
-//
-//    @Test
-//    public void loginShouldBeSuccessful() {
-//        final String givenTrackerPassword = "$2a$10$8y9hC00YePN";
-//        final Tracker givenTracker = createTracker(255L, givenTrackerPassword);
-//
-//        final String givenPackagePassword = "111";
-//        final ProtectedLoginPackage givenLoginPackage = createLoginPackage(givenPackagePassword);
-//
-//        when(mockedPasswordEncoder.matches(same(givenPackagePassword), same(givenTrackerPassword)))
-//                .thenReturn(true);
-//
-//        final Optional<Package> optionalActual = loginPackageHandler.loginCreatingResponseIfFailed(
-//                givenTracker,
-//                givenLoginPackage
-//        );
-//        assertTrue(optionalActual.isEmpty());
-//    }
-//
-//    @Test
-//    public void loginShouldBeFailed() {
-//        final String givenTrackerPassword = "$2a$10$8y9hC00YeP";
-//        final Tracker givenTracker = createTracker(256L, givenTrackerPassword);
-//
-//        final String givenPackagePassword = "1111";
-//        final ProtectedLoginPackage givenLoginPackage = createLoginPackage(givenPackagePassword);
-//
-//        when(mockedPasswordEncoder.matches(same(givenPackagePassword), same(givenTrackerPassword)))
-//                .thenReturn(false);
-//
-//        final Optional<Package> optionalActual = loginPackageHandler.loginCreatingResponseIfFailed(
-//                givenTracker,
-//                givenLoginPackage
-//        );
-//        assertTrue(optionalActual.isPresent());
-//        final Package actual = optionalActual.get();
-//        assertSame(GIVEN_WRONG_PASSWORD_RESPONSE, actual);
-//    }
-//
-//    private static Tracker createTracker(final Long id, final String password) {
-//        return Tracker.builder()
-//                .id(id)
-//                .password(password)
-//                .build();
-//    }
-//
-//    private static ProtectedLoginPackage createLoginPackage(final String password) {
-//        return new ProtectedLoginPackage() {
-//
-//            @Override
-//            public String getPassword() {
-//                return password;
-//            }
-//
-//            @Override
-//            public String getImei() {
-//                throw new UnsupportedOperationException();
-//            }
-//        };
-//    }
-//
-//    private static final class TestProtectedLoginPackageHandler extends ProtectedLoginPackageHandler {
-//        private final Package wrongPasswordResponse;
-//
-//        public TestProtectedLoginPackageHandler(final BCryptPasswordEncoder passwordEncoder,
-//                                                final Package wrongPasswordResponse) {
-//            super(null, null, null, null, passwordEncoder);
-//            this.wrongPasswordResponse = wrongPasswordResponse;
-//        }
-//
-//        @Override
-//        protected Package createNoSuchImeiResponse() {
-//            throw new UnsupportedOperationException();
-//        }
-//
-//        @Override
-//        protected Package createSuccessResponse() {
-//            throw new UnsupportedOperationException();
-//        }
-//
-//        @Override
-//        protected Package createWrongPasswordResponse() {
-//            return wrongPasswordResponse;
-//        }
-//    }
-//}
+package by.bsu.wialontransport.protocol.core.handler.packages.login;
+
+import by.bsu.wialontransport.crud.dto.Tracker;
+import by.bsu.wialontransport.protocol.core.model.login.ProtectedLoginPackage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public final class ProtectedLoginPackageHandlerTest {
+
+    @Mock
+    private BCryptPasswordEncoder mockedPasswordEncoder;
+
+    private TestProtectedLoginPackageHandler handler;
+
+    @BeforeEach
+    public void initializeHandler() {
+        handler = new TestProtectedLoginPackageHandler(mockedPasswordEncoder);
+    }
+
+    @Test
+    public void loginExistingTrackerShouldBeSuccess() {
+        final String givenTrackerPassword = "sdffsdsj324243sdsfsdf";
+        final Tracker givenTracker = Tracker.builder().password(givenTrackerPassword).build();
+        final String givenRequestPassword = "111";
+        final TestRequest givenRequest = new TestRequest(givenRequestPassword);
+
+        when(mockedPasswordEncoder.matches(same(givenRequestPassword), same(givenTrackerPassword))).thenReturn(true);
+
+        final Optional<Object> optionalActual = handler.loginCreatingFailedResponse(givenTracker, givenRequest);
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void loginExistingTrackerShouldBeFailed() {
+        final String givenTrackerPassword = "sdffsdsj324243sdsfsdf";
+        final Tracker givenTracker = Tracker.builder().password(givenTrackerPassword).build();
+        final String givenRequestPassword = "111";
+        final TestRequest givenRequest = new TestRequest(givenRequestPassword);
+
+        when(mockedPasswordEncoder.matches(same(givenRequestPassword), same(givenTrackerPassword))).thenReturn(false);
+
+        final Optional<Object> optionalActual = handler.loginCreatingFailedResponse(givenTracker, givenRequest);
+        assertTrue(optionalActual.isPresent());
+        final Object actual = optionalActual.get();
+        assertTrue(actual instanceof TestWrongPasswordResponse);
+    }
+
+    private static final class TestRequest extends ProtectedLoginPackage {
+
+        public TestRequest(final String password) {
+            super(null, password);
+        }
+    }
+
+    private static final class TestWrongPasswordResponse {
+
+    }
+
+    private static final class TestProtectedLoginPackageHandler extends ProtectedLoginPackageHandler<TestRequest> {
+
+        public TestProtectedLoginPackageHandler(final BCryptPasswordEncoder passwordEncoder) {
+            super(TestRequest.class, null, null, null, null, passwordEncoder);
+        }
+
+        @Override
+        protected Object createNoSuchImeiResponse() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected Object createSuccessResponse() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected TestWrongPasswordResponse createWrongPasswordResponse() {
+            return new TestWrongPasswordResponse();
+        }
+    }
+}
