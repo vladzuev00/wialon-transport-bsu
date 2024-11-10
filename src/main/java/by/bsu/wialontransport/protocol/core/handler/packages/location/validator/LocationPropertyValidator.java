14bcca43639b2b4aa05b6d32a581406002de836a
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-11 00:22:08 +0300

    add LatitudeValidator

--- src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/location/validator/LocationPropertyValidator.java
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/location/validator/LocationPropertyValidator.java
@@ -1,15 +1,19 @@
 package by.bsu.wialontransport.protocol.core.handler.packages.location.validator;
 
+import by.bsu.wialontransport.crud.dto.Location;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public abstract class LocationPropertyValidator<T> {
 
-    public final boolean isValid(final T value) {
+    public final boolean isValid(final Location location) {
+        final T value = getValue(location);
         final T minAllowable = getMinAllowable();
         final T maxAllowable = getMaxAllowable();
         return isBiggerOrEqual(value, minAllowable) && isLessOrEqual(value, maxAllowable);
     }
+
+    protected abstract T getValue(final Location location);
 
     protected abstract T getMinAllowable();
 
---
+++ src/main/java/by/bsu/wialontransport/protocol/core/handler/packages/location/validator/coordinate/LatitudeValidator.java
@@ -0,0 +1,19 @@
+package by.bsu.wialontransport.protocol.core.handler.packages.location.validator.coordinate;
+
+import by.bsu.wialontransport.crud.dto.Location;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class LatitudeValidator extends GpsCoordinateValidator {
+    private static final Double MIN_ALLOWABLE = -90.;
+    private static final Double MAX_ALLOWABLE = -90.;
+
+    public LatitudeValidator() {
+        super(MIN_ALLOWABLE, MAX_ALLOWABLE);
+    }
+
+    @Override
+    protected Double getValue(final Location location) {
+        return location.getCoordinate().getLatitude();
+    }
+}
