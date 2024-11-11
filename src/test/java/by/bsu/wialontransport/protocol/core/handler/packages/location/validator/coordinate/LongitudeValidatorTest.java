ff29094e78f64ce61c26e4295e494bac9894a466
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-11-11 13:17:11 +0300

    add LongitudeValidatorTest

---
+++ src/test/java/by/bsu/wialontransport/protocol/core/handler/packages/location/validator/coordinate/LongitudeValidatorTest.java
@@ -0,0 +1,22 @@
+package by.bsu.wialontransport.protocol.core.handler.packages.location.validator.coordinate;
+
+import by.bsu.wialontransport.crud.dto.Location;
+import by.bsu.wialontransport.model.GpsCoordinate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class LongitudeValidatorTest {
+    private final LongitudeValidator validator = new LongitudeValidator();
+
+    @Test
+    public void valueShouldBeGot() {
+        final double givenLongitude = 5.5;
+        final Location givenLocation = Location.builder()
+                .coordinate(GpsCoordinate.builder().longitude(givenLongitude).build())
+                .build();
+
+        final Double actual = validator.getValue(givenLocation);
+        assertEquals(givenLongitude, actual);
+    }
+}
