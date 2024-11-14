021068bc3ab35df0f0b80be37e9baf2130e33bf6
Vlad Zuev <vladzuev.00@mail.ru>
2024-11-14 22:36:59 +0300

    refactor WialonRequestLoginPackageHandler

--- src/main/java/by/bsu/wialontransport/protocol/wialon/handler/WialonRequestLoginPackageHandler.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/handler/WialonRequestLoginPackageHandler.java
@@ -1,48 +1,51 @@
-//package by.bsu.wialontransport.protocol.wialon.handler.packages;
-//
-//import by.bsu.wialontransport.crud.service.DataService;
-//import by.bsu.wialontransport.crud.service.TrackerService;
-//import by.bsu.wialontransport.protocol.core.connectionmanager.ConnectionManager;
-//import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
-//import by.bsu.wialontransport.protocol.core.handler.packages.login.ProtectedLoginPackageHandler;
-//import by.bsu.wialontransport.protocol.core.model.packages.Package;
-//import by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonRequestLoginPackage;
-//import by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonResponseLoginPackage;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import static by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonResponseLoginPackage.Status.*;
-//
-//@Component
-//public final class WialonRequestLoginPackageHandler extends ProtectedLoginPackageHandler<WialonRequestLoginPackage> {
-//
-//    public WialonRequestLoginPackageHandler(final ContextAttributeManager contextAttributeManager,
-//                                            final TrackerService trackerService,
-//                                            final ConnectionManager connectionManager,
-//                                            final DataService dataService,
-//                                            final BCryptPasswordEncoder passwordEncoder) {
-//        super(
-//                WialonRequestLoginPackage.class,
-//                contextAttributeManager,
-//                trackerService,
-//                connectionManager,
-//                dataService,
-//                passwordEncoder
-//        );
-//    }
-//
-//    @Override
-//    protected Package createNoSuchImeiResponse() {
-//        return new WialonResponseLoginPackage(CONNECTION_FAILURE);
-//    }
-//
-//    @Override
-//    protected Package createSuccessResponse() {
-//        return new WialonResponseLoginPackage(SUCCESS_AUTHORIZATION);
-//    }
-//
-//    @Override
-//    protected Package createWrongPasswordResponse() {
-//        return new WialonResponseLoginPackage(ERROR_CHECK_PASSWORD);
-//    }
-//}
+package by.bsu.wialontransport.protocol.wialon.handler;
+
+import by.bsu.wialontransport.crud.service.LocationService;
+import by.bsu.wialontransport.crud.service.TrackerService;
+import by.bsu.wialontransport.protocol.core.contextattributemanager.ContextAttributeManager;
+import by.bsu.wialontransport.protocol.core.contextmanager.ChannelHandlerContextManager;
+import by.bsu.wialontransport.protocol.core.handler.packages.login.ProtectedLoginPackageHandler;
+import by.bsu.wialontransport.protocol.core.handler.packages.login.factory.TrackerImeiFactory;
+import by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonRequestLoginPackage;
+import by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonResponseLoginPackage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import static by.bsu.wialontransport.protocol.wialon.model.packages.login.WialonResponseLoginPackage.Status.*;
+
+//TODO: test
+@Component
+public final class WialonRequestLoginPackageHandler extends ProtectedLoginPackageHandler<WialonRequestLoginPackage> {
+
+    public WialonRequestLoginPackageHandler(final TrackerImeiFactory imeiFactory,
+                                            final ContextAttributeManager contextAttributeManager,
+                                            final TrackerService trackerService,
+                                            final ChannelHandlerContextManager contextManager,
+                                            final LocationService locationService,
+                                            final BCryptPasswordEncoder passwordEncoder) {
+        super(
+                WialonRequestLoginPackage.class,
+                imeiFactory,
+                contextAttributeManager,
+                trackerService,
+                contextManager,
+                locationService,
+                passwordEncoder
+        );
+    }
+
+    @Override
+    protected WialonResponseLoginPackage createNoSuchImeiResponse() {
+        return new WialonResponseLoginPackage(CONNECTION_FAILURE);
+    }
+
+    @Override
+    protected WialonResponseLoginPackage createSuccessResponse() {
+        return new WialonResponseLoginPackage(SUCCESS_AUTHORIZATION);
+    }
+
+    @Override
+    protected WialonResponseLoginPackage createWrongPasswordResponse() {
+        return new WialonResponseLoginPackage(PASSWORD_ERROR);
+    }
+}
