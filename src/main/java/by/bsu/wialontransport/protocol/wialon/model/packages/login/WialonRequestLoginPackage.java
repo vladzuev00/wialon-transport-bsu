97833d460e88a14f4dfb640e90ab848c17fb1059
Vlad Zuev <vladzuev.00@mail.ru>
2024-11-14 22:27:31 +0300

    refactor WialonRequestLoginPackage

--- src/main/java/by/bsu/wialontransport/protocol/wialon/model/packages/login/WialonRequestLoginPackage.java
+++ src/main/java/by/bsu/wialontransport/protocol/wialon/model/packages/login/WialonRequestLoginPackage.java
@@ -1,20 +1,14 @@
 package by.bsu.wialontransport.protocol.wialon.model.packages.login;
 
+import by.bsu.wialontransport.protocol.core.model.login.ProtectedLoginPackage;
 import by.bsu.wialontransport.protocol.wialon.model.packages.WialonPackage;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
-@RequiredArgsConstructor
-@Getter
-@EqualsAndHashCode
-@ToString
-public final class WialonRequestLoginPackage implements WialonPackage {
+public final class WialonRequestLoginPackage extends ProtectedLoginPackage implements WialonPackage {
     public static final String PREFIX = "#L#";
 
-    private final String imei;
-    private final String password;
+    public WialonRequestLoginPackage(final String imei, final String password) {
+        super(imei, password);
+    }
 
     @Override
     public String getPrefix() {
