bd9a60be3ca303581bc2126a122c4f1cd71fa1c8
Vlad <v.zuev@besmart.by>
2025-02-12 15:30:38 +0300

    refactor ProtocolDecodingIT

--- src/test/java/by/bsu/wialontransport/it/protocol/decoding/ProtocolDecodingIT.java
+++ src/test/java/by/bsu/wialontransport/it/protocol/decoding/ProtocolDecodingIT.java
@@ -1,23 +1,20 @@
 package by.bsu.wialontransport.it.protocol.decoding;
 
-import by.bsu.wialontransport.base.AbstractSpringBootTest;
+import by.bsu.wialontransport.base.AbstractSpringBootIT;
 import by.bsu.wialontransport.protocol.core.decoder.ProtocolDecoder;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.embedded.EmbeddedChannel;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.stream.Stream;
 
 import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @RequiredArgsConstructor
-@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-public abstract class ProtocolDecodingIT extends AbstractSpringBootTest {
+public abstract class ProtocolDecodingIT extends AbstractSpringBootIT {
     private final ProtocolDecoder<?> decoder;
 
     @Test
