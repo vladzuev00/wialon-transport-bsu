6a9c66c55f451a5ef6f394484acb173329f14485
Vlad <v.zuev@besmart.by>
2025-02-18 14:06:14 +0300

    refactor City

--- src/main/java/by/vladzuev/locationreceiver/crud/dto/City.java
+++ src/main/java/by/vladzuev/locationreceiver/crud/dto/City.java
@@ -3,27 +3,11 @@ package by.vladzuev.locationreceiver.crud.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
-import org.locationtech.jts.geom.Geometry;
 
 @Value
-@AllArgsConstructor
 @Builder
+@AllArgsConstructor
 public class City implements Dto<Long> {
     Long id;
     Address address;
-    SearchingCitiesProcess searchingCitiesProcess;
-
-    public Geometry findGeometry() {
-        return address.getGeometry();
-    }
-
-    public static City copyWithAddressAndProcess(final City source,
-                                                 final Address address,
-                                                 final SearchingCitiesProcess process) {
-        return new City(source.getId(), address, process);
-    }
-
-    public static City createWithAddress(final Address address) {
-        return new City(null, address, null);
-    }
 }
