3b6784947e9da045427bce0f6dc3d470020ecec2
Vlad <v.zuev@besmart.by>
2025-02-24 09:50:43 +0300

    add AddressCrudMapper

---
+++ src/main/java/by/vladzuev/locationreceiver/crud/mapper/AddressCrudMapper.java
@@ -0,0 +1,8 @@
+package by.vladzuev.locationreceiver.crud.mapper;
+
+import by.vladzuev.locationreceiver.crud.dto.Address;
+import by.vladzuev.locationreceiver.crud.entity.AddressEntity;
+
+public interface AddressCrudMapper extends CrudMapper<Address, AddressEntity> {
+
+}
