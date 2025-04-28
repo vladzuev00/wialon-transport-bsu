e335278d09f48d3ff621ddddd568ca116c3a9190
Vlad Zuev <vladzuev.00@mail.ru>
2025-04-28 13:38:47 +0300

    add JT808RegistrationPackageHandler and JT808RegistrationPackageHandlerTest

---
+++ src/main/java/by/vladzuev/locationreceiver/protocol/jt808/handler/JT808RegistrationPackageHandler.java
@@ -0,0 +1,40 @@
+package by.vladzuev.locationreceiver.protocol.jt808.handler;
+
+import by.vladzuev.locationreceiver.crud.service.LocationService;
+import by.vladzuev.locationreceiver.crud.service.TrackerService;
+import by.vladzuev.locationreceiver.protocol.core.handler.packages.login.UnprotectedLoginPackageHandler;
+import by.vladzuev.locationreceiver.protocol.core.handler.packages.login.factory.TrackerImeiFactory;
+import by.vladzuev.locationreceiver.protocol.core.manager.ContextAttributeManager;
+import by.vladzuev.locationreceiver.protocol.core.manager.ContextManager;
+import by.vladzuev.locationreceiver.protocol.jt808.model.JT808RegistrationPackage;
+import by.vladzuev.locationreceiver.protocol.jt808.model.JT808ResponsePackage;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class JT808RegistrationPackageHandler extends UnprotectedLoginPackageHandler<JT808RegistrationPackage> {
+
+    public JT808RegistrationPackageHandler(TrackerImeiFactory imeiFactory,
+                                           ContextAttributeManager contextAttributeManager,
+                                           TrackerService trackerService,
+                                           ContextManager contextManager,
+                                           LocationService locationService) {
+        super(
+                JT808RegistrationPackage.class,
+                imeiFactory,
+                contextAttributeManager,
+                trackerService,
+                contextManager,
+                locationService
+        );
+    }
+
+    @Override
+    protected JT808ResponsePackage createNoSuchImeiResponse() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected JT808ResponsePackage createSuccessResponse() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/by/vladzuev/locationreceiver/protocol/jt808/handler/JT808RegistrationPackageHandlerTest.java
@@ -0,0 +1,25 @@
+package by.vladzuev.locationreceiver.protocol.jt808.handler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class JT808RegistrationPackageHandlerTest {
+    private final JT808RegistrationPackageHandler handler = new JT808RegistrationPackageHandler(
+            null,
+            null,
+            null,
+            null,
+            null
+    );
+
+    @Test
+    public void noSuchImeiResponseShouldBeCreated() {
+        assertThrows(UnsupportedOperationException.class, handler::createNoSuchImeiResponse);
+    }
+
+    @Test
+    public void successResponseShouldBeCreated() {
+        assertThrows(UnsupportedOperationException.class, handler::createSuccessResponse);
+    }
+}
