5018f170d2434e564f6adfaf19c226a78748287b
Vlad <v.zuev@besmart.by>
2025-04-30 11:43:28 +0300

    add LoginSuccessHolder

---
+++ src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/holder/LoginSuccessHolder.java
@@ -0,0 +1,12 @@
+package by.vladzuev.locationreceiver.protocol.teltonika.holder;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+public final class LoginSuccessHolder {
+    private volatile boolean success;
+}
