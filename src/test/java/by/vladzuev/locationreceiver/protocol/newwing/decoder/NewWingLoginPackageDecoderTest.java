9dba09e1250342670f9dfd1557a8cb5af886887e
Vlad <v.zuev@besmart.by>
2025-05-02 15:06:14 +0300

    refactor

--- src/test/java/by/vladzuev/locationreceiver/protocol/newwing/decoder/NewWingLoginPackageDecoderTest.java
+++ src/test/java/by/vladzuev/locationreceiver/protocol/newwing/decoder/NewWingLoginPackageDecoderTest.java
@@ -1,6 +1,7 @@
 package by.vladzuev.locationreceiver.protocol.newwing.decoder;
 
 import by.vladzuev.locationreceiver.protocol.newwing.model.request.NewWingLoginPackage;
+import by.vladzuev.locationreceiver.protocol.newwing.util.NewWingUtil;
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
