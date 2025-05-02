1762ae5aba3b1ac761c796174eae6f4b3dc05c5c
Vlad <v.zuev@besmart.by>
2025-05-02 17:26:38 +0300

    refactor TeltonikaPackageDecoder

--- src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/decoder/TeltonikaPackageDecoder.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/decoder/TeltonikaPackageDecoder.java
@@ -1,9 +1,19 @@
 package by.vladzuev.locationreceiver.protocol.teltonika.decoder;
 
 import by.vladzuev.locationreceiver.protocol.core.decoder.packages.PackageDecoder;
+import by.vladzuev.locationreceiver.protocol.teltonika.holder.LoginSuccessHolder;
 import io.netty.buffer.ByteBuf;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public abstract class TeltonikaPackageDecoder implements PackageDecoder<ByteBuf> {
+    private final LoginSuccessHolder loginSuccessHolder;
+    private final boolean requiredLoginSuccess;
+
+    @Override
+    public final boolean isAbleDecode(final ByteBuf buffer) {
+        return loginSuccessHolder.isSuccess() == requiredLoginSuccess;
+    }
 
     @Override
     public final Object decode(final ByteBuf buffer) {
