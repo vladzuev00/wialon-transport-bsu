444791539cda6b19cb539f956e84cacd2164bd7b
Vlad <v.zuev@besmart.by>
2025-05-02 11:31:11 +0300

    refactor

--- src/main/java/by/vladzuev/locationreceiver/util/StreamUtil.java
+++ src/main/java/by/vladzuev/locationreceiver/util/StreamUtil.java
@@ -13,12 +13,6 @@ import static java.util.stream.StreamSupport.stream;
 @UtilityClass
 public final class StreamUtil {
 
-    public static <T> Stream<T> concat(final Stream<? extends T>... streams) {
-        return streams != null
-                ? Stream.of(streams).flatMap(identity())
-                : empty();
-    }
-
     public static boolean isEmpty(final Stream<?> stream) {
         return stream.findAny().isEmpty();
     }
