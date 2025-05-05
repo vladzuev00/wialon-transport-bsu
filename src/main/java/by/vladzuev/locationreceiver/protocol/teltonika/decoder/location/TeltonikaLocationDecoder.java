406b17cb9c8e5cad0c0b616b75f9c21a581da67c
Vlad <v.zuev@besmart.by>
2025-05-05 11:04:55 +0300

    refactor TeltonikaLocationDecoder

--- src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/decoder/location/TeltonikaLocationDecoder.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/decoder/location/TeltonikaLocationDecoder.java
@@ -11,17 +11,23 @@ import static java.time.ZoneOffset.UTC;
 
 @Component
 public final class TeltonikaLocationDecoder {
+    private static final double GPS_COORDINATE_DELIMITER = 10000000.0;
+    private static final int IO_PREFIX_BYTE_COUNT = 2;
+    private static final int IO_N1_VALUE_BYTE_COUNT = 1;
+    private static final int IO_N2_VALUE_BYTE_COUNT = 2;
+    private static final int IO_N4_VALUE_BYTE_COUNT = 4;
+    private static final int IO_N8_VALUE_BYTE_COUNT = 8;
 
     public TeltonikaLocation decode(final ByteBuf buffer) {
         final LocalDateTime dateTime = readDateTime(buffer);
         skipPriority(buffer);
-        final double longitude = buffer.readInt() / 10000000.0;
-        final double latitude = buffer.readInt() / 10000000.0;
+        final double longitude = readGpsCoordinate(buffer);
+        final double latitude = readGpsCoordinate(buffer);
         final short altitude = buffer.readShort();
         final short angle = buffer.readShort();
         final byte satelliteCount = buffer.readByte();
         final short speed = buffer.readShort();
-        //TODO io
+        skipAllIO(buffer);
         return new TeltonikaLocation(dateTime, latitude, longitude, altitude, angle, satelliteCount, speed);
     }
 
@@ -30,7 +36,25 @@ public final class TeltonikaLocationDecoder {
         return ofEpochSecond(epochSecond, 0, UTC);
     }
 
+    private double readGpsCoordinate(final ByteBuf buffer) {
+        return buffer.readInt() / GPS_COORDINATE_DELIMITER;
+    }
+
     private void skipPriority(final ByteBuf buffer) {
         buffer.skipBytes(Byte.BYTES);
+    }
+
+    private void skipAllIO(final ByteBuf buffer) {
+        buffer.skipBytes(IO_PREFIX_BYTE_COUNT);
+        skipIO(buffer, IO_N1_VALUE_BYTE_COUNT);
+        skipIO(buffer, IO_N2_VALUE_BYTE_COUNT);
+        skipIO(buffer, IO_N4_VALUE_BYTE_COUNT);
+        skipIO(buffer, IO_N8_VALUE_BYTE_COUNT);
+    }
+
+    private void skipIO(final ByteBuf buffer, final int valueByteCount) {
+        final int count = buffer.readByte();
+        buffer.skipBytes(count);
+        buffer.skipBytes(count * valueByteCount);
     }
 }
