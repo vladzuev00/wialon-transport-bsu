630188ae92ec9cf7b67d5c1fa897767d0d978f09
Vlad <v.zuev@besmart.by>
2025-05-05 14:04:52 +0300

    add TeltonikaLocationPackageEncoder

--- src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/encoder/TeltonikaLocationPackageEncoder.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/teltonika/encoder/TeltonikaLocationPackageEncoder.java
@@ -1,4 +1,18 @@
 package by.vladzuev.locationreceiver.protocol.teltonika.encoder;
 
-public class TeltonikaLocationPackageEncoder {
+import by.vladzuev.locationreceiver.protocol.core.encoder.packages.PackageEncoder;
+import by.vladzuev.locationreceiver.protocol.teltonika.model.location.TeltonikaResponseLocationPackage;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class TeltonikaLocationPackageEncoder extends PackageEncoder<TeltonikaResponseLocationPackage> {
+
+    public TeltonikaLocationPackageEncoder() {
+        super(TeltonikaResponseLocationPackage.class);
+    }
+
+    @Override
+    protected byte[] encodeInternal(final TeltonikaResponseLocationPackage response) {
+        return new byte[]{(byte) response.getLocationCount()};
+    }
 }
