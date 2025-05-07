3e3a1365c7e399c36a8640c963d9362146455adb
Vlad <v.zuev@besmart.by>
2025-05-07 13:09:07 +0300

    correct ApelRequestExtendedLoginPackageDecoderTest

--- src/test/java/by/vladzuev/locationreceiver/protocol/apel/decoder/ApelRequestExtendedLoginPackageDecoderTest.java
+++ src/test/java/by/vladzuev/locationreceiver/protocol/apel/decoder/ApelRequestExtendedLoginPackageDecoderTest.java
@@ -1,15 +1,23 @@
 package by.vladzuev.locationreceiver.protocol.apel.decoder;
 
+import by.vladzuev.locationreceiver.protocol.apel.model.login.ApelRequestExtendedLoginPackage;
+import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.Test;
+
+import static io.netty.buffer.ByteBufUtil.decodeHexDump;
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class ApelRequestExtendedLoginPackageDecoderTest {
     private final ApelRequestExtendedLoginPackageDecoder decoder = new ApelRequestExtendedLoginPackageDecoder();
 
     @Test
     public void packageShouldBeDecodedStartingFromBody() {
-        throw new UnsupportedOperationException();
-//        final ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("0c00"));
-//
-//        System.out.println(givenBuffer.readShortLE());
+        final ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("f12a00000f003235303032363533343135313036340f0033353638393530333632373938313101002000000000"));
+
+        final ApelRequestExtendedLoginPackage actual = decoder.decodeStartingFromBody(givenBuffer);
+        final ApelRequestExtendedLoginPackage expected = new ApelRequestExtendedLoginPackage("356895036279811", " ");
+        assertEquals(expected, actual);
+        assertEquals(Integer.BYTES, givenBuffer.readableBytes());
     }
 }
