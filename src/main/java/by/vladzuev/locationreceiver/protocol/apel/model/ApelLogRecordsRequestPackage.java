680980d372202f72a5518ee69974365d4e972de7
Vlad <v.zuev@besmart.by>
2025-05-08 14:38:40 +0300

    refactor ApelLogRecordsRequestPackage

--- src/main/java/by/vladzuev/locationreceiver/protocol/apel/model/ApelLogRecordsRequestPackage.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/model/ApelLogRecordsRequestPackage.java
@@ -1,8 +1,15 @@
 package by.vladzuev.locationreceiver.protocol.apel.model;
 
+import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelCurrentLocationResponsePackage;
+import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelCurrentStateRequestPackage;
 import lombok.Value;
+
+import java.util.List;
 
 @Value
 public class ApelLogRecordsRequestPackage {
-
+    List<ApelSensorStateResponsePackage> sensorStatePackages;
+    List<InputOutputStateResponsePackage> inputOutputStatePackages;
+    List<ApelCurrentLocationResponsePackage> currentLocationPackages;
+    List<ApelCurrentStateRequestPackage> currentStatePackages;
 }
