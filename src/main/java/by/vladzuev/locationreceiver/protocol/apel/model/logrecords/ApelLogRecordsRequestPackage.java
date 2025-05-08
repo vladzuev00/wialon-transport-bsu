41d736a209817423374dc0c00c2edbe9c565664f
Vlad <v.zuev@besmart.by>
2025-05-08 13:15:58 +0300

    refactor

--- src/main/java/by/vladzuev/locationreceiver/protocol/apel/model/logrecords/ApelLogRecordsRequestPackage.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/model/logrecords/ApelLogRecordsRequestPackage.java
@@ -1,11 +1,8 @@
 package by.vladzuev.locationreceiver.protocol.apel.model.logrecords;
 
-import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelLocation;
 import lombok.Value;
-
-import java.util.List;
 
 @Value
 public class ApelLogRecordsRequestPackage {
-    List<ApelLocation> locations;
+
 }
