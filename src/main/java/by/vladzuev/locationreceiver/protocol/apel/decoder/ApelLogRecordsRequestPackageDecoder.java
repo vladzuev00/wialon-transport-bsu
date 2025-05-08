b888dd94536c4aaf970fb20d80f7fd6af19f9270
Vlad <v.zuev@besmart.by>
2025-05-08 13:03:41 +0300

    add ApelLogRecordsRequestPackageDecoder

---
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/ApelLogRecordsRequestPackageDecoder.java
@@ -0,0 +1,32 @@
+package by.vladzuev.locationreceiver.protocol.apel.decoder;
+
+import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelLocation;
+import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelLogRecordsRequestPackage;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+
+@Component
+public final class ApelLogRecordsRequestPackageDecoder extends ApelPackageDecoder {
+    private static final Integer PREFIX = 131;
+
+    public ApelLogRecordsRequestPackageDecoder() {
+        super(PREFIX);
+    }
+
+    @Override
+    protected ApelLogRecordsRequestPackage decodeStartingFromBody(final ByteBuf buffer) {
+        final int locationCount = buffer.readUnsignedShortLE();
+        return range(0, locationCount)
+                .mapToObj(i -> readLocation(buffer))
+                .collect(collectingAndThen(toList(), ApelLogRecordsRequestPackage::new));
+    }
+
+    private ApelLocation readLocation(final ByteBuf buffer) {
+        buffer.skipBytes(Integer.BYTES);
+        throw new RuntimeException();
+    }
+}
