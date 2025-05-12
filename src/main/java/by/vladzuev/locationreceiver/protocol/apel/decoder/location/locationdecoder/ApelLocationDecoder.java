8d960c1b11cda2ad8b25bd0d05339c2de3ba4b74
Vlad <v.zuev@besmart.by>
2025-05-12 15:45:04 +0300

    add ApelLocationDecoder

--- src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/location/locationdecoder/ApelLocationDecoder.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/location/locationdecoder/ApelLocationDecoder.java
@@ -5,7 +5,24 @@ import io.netty.buffer.ByteBuf;
 
 public abstract class ApelLocationDecoder {
 
-    public ApelLocation decode(final ByteBuf buffer) {
-        throw new RuntimeException();
+    public final ApelLocation decode(final ByteBuf buffer) {
+        final int epochSeconds = buffer.readIntLE();
+        final int latitude = buffer.readIntLE();
+        final int longitude = buffer.readIntLE();
+        final short speed = readSpeed(buffer);
+        final Double hdop = readHdop(buffer);
+        final short course = buffer.readShortLE();
+        final short altitude = buffer.readShortLE();
+        final Byte satelliteCount = readSatelliteCount(buffer);
+        final double[] analogInputs = readAnalogInputs(buffer);
+        return new ApelLocation(epochSeconds, latitude, longitude, speed, hdop, course, altitude, satelliteCount, analogInputs);
     }
+
+    protected abstract short readSpeed(final ByteBuf buffer);
+
+    protected abstract Double readHdop(final ByteBuf buffer);
+
+    protected abstract Byte readSatelliteCount(final ByteBuf buffer);
+
+    protected abstract double[] readAnalogInputs(final ByteBuf buffer);
 }
