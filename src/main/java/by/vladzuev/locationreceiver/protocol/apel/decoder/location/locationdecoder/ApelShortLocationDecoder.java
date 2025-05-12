9a0f1c41757ced9c76e39e2d9abf06f20cef8857
Vlad <v.zuev@besmart.by>
2025-05-12 16:01:45 +0300

    add ApelShortLocationDecoder

---
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/location/locationdecoder/ApelShortLocationDecoder.java
@@ -0,0 +1,28 @@
+package by.vladzuev.locationreceiver.protocol.apel.decoder.location.locationdecoder;
+
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class ApelShortLocationDecoder extends ApelLocationDecoder {
+
+    @Override
+    protected short readSpeed(final ByteBuf buffer) {
+        return buffer.readShortLE();
+    }
+
+    @Override
+    protected Double readHdop(final ByteBuf buffer) {
+        return null;
+    }
+
+    @Override
+    protected Byte readSatelliteCount(final ByteBuf buffer) {
+        return null;
+    }
+
+    @Override
+    protected double[] readAnalogInputs(final ByteBuf buffer) {
+        return new double[0];
+    }
+}
