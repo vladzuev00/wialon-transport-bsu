e9425f6ee9a79fc5d61a7121f96f736217fbf509
Vlad <v.zuev@besmart.by>
2025-05-13 18:56:12 +0300

    refactor

--- src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/ApelLogRecordsRequestPackageDecoder.java
+++ src/main/java/by/vladzuev/locationreceiver/protocol/apel/decoder/ApelLogRecordsRequestPackageDecoder.java
@@ -1,9 +1,12 @@
 package by.vladzuev.locationreceiver.protocol.apel.decoder;
 
-import by.vladzuev.locationreceiver.protocol.apel.model.location.ApelLocation;
 import by.vladzuev.locationreceiver.protocol.apel.model.ApelLogRecordsRequestPackage;
 import io.netty.buffer.ByteBuf;
 import org.springframework.stereotype.Component;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
 @Component
 public final class ApelLogRecordsRequestPackageDecoder extends ApelPackageDecoder {
@@ -15,15 +18,9 @@ public final class ApelLogRecordsRequestPackageDecoder extends ApelPackageDecode
 
     @Override
     protected ApelLogRecordsRequestPackage decodeStartingFromBody(final ByteBuf buffer) {
-        throw new RuntimeException();
 //        final int locationCount = buffer.readUnsignedShortLE();
 //        return range(0, locationCount)
 //                .mapToObj(i -> readLocation(buffer))
 //                .collect(collectingAndThen(toList(), ApelLogRecordsRequestPackage::new));
-    }
-
-    private ApelLocation readLocation(final ByteBuf buffer) {
-        buffer.skipBytes(Integer.BYTES);
-        throw new RuntimeException();
     }
 }
